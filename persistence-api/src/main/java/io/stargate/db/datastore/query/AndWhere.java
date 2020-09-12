a27c4f7acda676abb853a139420aeff46584296b
csplinter <chris.splinter@datastax.com>
2020-09-11 19:51:09 -0500

    incorporate final feedback from Legal

--- persistence-api/src/main/java/io/stargate/db/datastore/query/AndWhere.java
+++ persistence-api/src/main/java/io/stargate/db/datastore/query/AndWhere.java
@@ -1,5 +1,5 @@
 /*
- * Copyright The Stargate Authors
+ * Copyright DataStax, Inc. and/or The Stargate Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
