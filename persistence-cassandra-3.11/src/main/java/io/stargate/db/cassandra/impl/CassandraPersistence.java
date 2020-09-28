0fbd090955e1a17138e7b9bb7fdadda0e8e7ddd6
Michael Penick <michael.penick@datastax.com>
2020-09-28 10:49:15 -0400

    Simplify from code review

--- persistence-cassandra-3.11/src/main/java/io/stargate/db/cassandra/impl/CassandraPersistence.java
+++ persistence-cassandra-3.11/src/main/java/io/stargate/db/cassandra/impl/CassandraPersistence.java
@@ -101,10 +101,7 @@ public class CassandraPersistence
    * live token owner doesn't become live within MigrationManager.MIGRATION_DELAY_IN_MS.
    */
   private static final int STARTUP_DELAY_MS =
-      Integer.parseInt(
-          System.getProperty(
-              "stargate.startup_delay_ms",
-              Integer.toString(3 * MigrationManager.MIGRATION_DELAY_IN_MS)));
+      Integer.getInteger("stargate.startup_delay_ms", 3 * MigrationManager.MIGRATION_DELAY_IN_MS);
 
   private CassandraDaemon daemon;
   private Authenticator authenticator;
