e8fada27f41dc75e0e03ca8d410bea34d3b846a1
tomekl007 <tomasz.lelek@datastax.com>
2020-09-28 14:52:15 +0200

    synchronize HealthCheckerActivator

--- health-checker/src/main/java/io/stargate/health/HealthCheckerActivator.java
+++ health-checker/src/main/java/io/stargate/health/HealthCheckerActivator.java
@@ -2,10 +2,12 @@ package io.stargate.health;
 
 import io.stargate.health.metrics.api.Metrics;
 import io.stargate.health.metrics.impl.MetricsImpl;
+import net.jcip.annotations.GuardedBy;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceEvent;
 import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,16 +16,20 @@ public class HealthCheckerActivator implements BundleActivator, ServiceListener 
 
   private BundleContext context;
 
+  @GuardedBy("this")
+  private WebImpl web = new WebImpl();
+
   @Override
-  public void start(BundleContext context) {
+  public synchronized void start(BundleContext context) {
     this.context = context;
     log.info("Starting healthchecker....");
 
     Metrics metrics = new MetricsImpl();
     context.registerService(Metrics.class, metrics, null);
+    web.setContext(context);
+    web.setMetrics(metrics);
 
     try {
-      WebImpl web = new WebImpl(this.context, metrics);
       web.start();
       log.info("Started healthchecker....");
     } catch (Exception e) {
@@ -32,10 +38,10 @@ public class HealthCheckerActivator implements BundleActivator, ServiceListener 
   }
 
   @Override
-  public void stop(BundleContext context) {}
+  public synchronized void stop(BundleContext context) {}
 
   @Override
-  public void serviceChanged(ServiceEvent serviceEvent) {
+  public synchronized void serviceChanged(ServiceEvent serviceEvent) {
     int type = serviceEvent.getType();
     String[] objectClass = (String[]) serviceEvent.getServiceReference().getProperty("objectClass");
     switch (type) {
--- health-checker/src/main/java/io/stargate/health/WebImpl.java
+++ health-checker/src/main/java/io/stargate/health/WebImpl.java
@@ -5,10 +5,23 @@ import org.osgi.framework.BundleContext;
 
 public class WebImpl {
   private BundleContext context;
-  private final Metrics metrics;
+  private Metrics metrics;
 
-  public WebImpl(BundleContext context, Metrics metrics) {
+  public WebImpl() {}
+
+  public BundleContext getContext() {
+    return context;
+  }
+
+  public void setContext(BundleContext context) {
     this.context = context;
+  }
+
+  public Metrics getMetrics() {
+    return metrics;
+  }
+
+  public void setMetrics(Metrics metrics) {
     this.metrics = metrics;
   }
 
