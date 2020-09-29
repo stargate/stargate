b9292978322936fe2e0412b78d5c255d4562fa75
tomekl007 <tomasz.lelek@datastax.com>
2020-09-29 12:16:10 +0200

    move: ``` List<CellValue> getPartitionKeys(); List<CellValue> getClusteringKeys(); ``` to MutationEvent

--- persistence-api/src/main/java/org/apache/cassandra/stargate/db/MutationEvent.java
+++ persistence-api/src/main/java/org/apache/cassandra/stargate/db/MutationEvent.java
@@ -1,5 +1,6 @@
 package org.apache.cassandra.stargate.db;
 
+import java.util.List;
 import org.apache.cassandra.stargate.schema.TableMetadata;
 
 /** Represents a change made in the database. */
@@ -9,4 +10,8 @@ public interface MutationEvent {
 
   /** Timestamp of the change. */
   long getTimestamp();
+  /** Gets the partitions keys of this event */
+  List<CellValue> getPartitionKeys();
+  /** Gets the clustering keys of this event */
+  List<CellValue> getClusteringKeys();
 }
--- persistence-api/src/main/java/org/apache/cassandra/stargate/db/RowUpdateEvent.java
+++ persistence-api/src/main/java/org/apache/cassandra/stargate/db/RowUpdateEvent.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 /** Represents an update affecting a single row. */
 public interface RowUpdateEvent extends MutationEvent {
-  List<CellValue> getPartitionKeys();
-
-  List<CellValue> getClusteringKeys();
 
   List<Cell> getCells();
 }
