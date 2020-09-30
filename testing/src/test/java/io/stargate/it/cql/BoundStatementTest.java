967987b0701af67d69af86ce43763f8293d944f5
olim7t <omichallat+github@gmail.com>
2020-09-30 11:54:11 -0700

    More BoundStatement tests

--- testing/src/test/java/io/stargate/it/cql/BoundStatementTest.java
+++ testing/src/test/java/io/stargate/it/cql/BoundStatementTest.java
@@ -1,17 +1,22 @@
 package io.stargate.it.cql;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import com.datastax.oss.driver.api.core.cql.BoundStatement;
+import com.datastax.oss.driver.api.core.cql.ExecutionInfo;
 import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import com.datastax.oss.driver.api.core.cql.QueryTrace;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.core.data.ByteUtils;
 import com.datastax.oss.driver.api.core.servererrors.InvalidQueryException;
+import com.datastax.oss.driver.api.core.servererrors.ProtocolError;
 import io.stargate.it.storage.ClusterConnectionInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Note: all tests related to unset values assume that the protocol version is always 4 or higher.
@@ -107,5 +112,61 @@ public class BoundStatementTest extends JavaDriverTestBase {
 
     Row row = session.execute("SELECT v0 FROM test2 WHERE k=?", KEY).one();
     assertThat(row.getInt("v0")).isEqualTo(VALUE);
+  }
+
+  @Test
+  public void should_use_page_size_on_statement() {
+    PreparedStatement prepared =
+        session.prepare(
+            SimpleStatement.newInstance("SELECT v FROM test WHERE k=?").setPageSize(20));
+    BoundStatement statement = prepared.bind(KEY);
+    ResultSet resultSet = session.execute(statement);
+    assertThat(resultSet.getAvailableWithoutFetching()).isEqualTo(20);
+
+    statement = statement.copy(resultSet.getExecutionInfo().getPagingState());
+    resultSet = session.execute(statement);
+
+    assertThat(resultSet.iterator().next().getInt("v")).isEqualTo(20);
+  }
+
+  @Test
+  @Disabled("C* 3.11 returns a ProtocolError, Stargate returns a ServerError")
+  public void should_throw_when_using_corrupt_paging_state() {
+    PreparedStatement prepared = session.prepare("SELECT v FROM test WHERE k=?");
+    BoundStatement statement =
+        prepared
+            .boundStatementBuilder(KEY)
+            .setPagingState(ByteUtils.fromHexString("0x1234"))
+            .build();
+    assertThatThrownBy(() -> session.execute(statement)).isInstanceOf(ProtocolError.class);
+  }
+
+  @Test
+  @Disabled("Looks like the query timestamp is not propagated correctly")
+  public void should_use_query_timestamp() {
+    long timestamp = 10; // whatever
+    session.execute(
+        SimpleStatement.builder("INSERT INTO test2 (k, v) values ('test', 1)")
+            .setQueryTimestamp(timestamp)
+            .build());
+
+    Row row = session.execute("SELECT writetime(v) FROM test2 WHERE k = 'test'").one();
+    assertThat(row).isNotNull();
+    assertThat(row.getLong(0)).isEqualTo(timestamp);
+  }
+
+  @Test
+  public void should_use_tracing() {
+    PreparedStatement prepared = session.prepare("SELECT v FROM test WHERE k=?");
+    BoundStatement statement = prepared.bind(KEY);
+
+    ExecutionInfo executionInfo = session.execute(statement).getExecutionInfo();
+    assertThat(executionInfo.getTracingId()).isNull();
+
+    executionInfo = session.execute(statement.setTracing(true)).getExecutionInfo();
+    assertThat(executionInfo.getTracingId()).isNotNull();
+    QueryTrace queryTrace = executionInfo.getQueryTrace();
+    assertThat(queryTrace).isNotNull();
+    assertThat(queryTrace.getEvents()).isNotEmpty();
   }
 }
