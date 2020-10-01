aa510a3167ca326ea69cc6e1b6c295748fac262f
Olivier Michallat <olim7t@users.noreply.github.com>
2020-10-01 08:39:34 -0700

    Add basic request metrics for GraphQL (#158)

--- graphqlapi/src/main/java/io/stargate/graphql/WebImpl.java
+++ graphqlapi/src/main/java/io/stargate/graphql/WebImpl.java
@@ -15,13 +15,14 @@
  */
 package io.stargate.graphql;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.servlet.InstrumentedFilter;
 import graphql.kickstart.servlet.CustomGraphQLServlet;
 import graphql.kickstart.servlet.SchemaGraphQLServlet;
 import io.stargate.auth.AuthenticationService;
+import io.stargate.core.metrics.api.Metrics;
 import io.stargate.db.Persistence;
 import java.util.EnumSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.DispatcherType;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
@@ -31,11 +32,11 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
 
 public class WebImpl {
-  private Server server;
-  private Persistence persistence;
-  private AuthenticationService authenticationService;
 
-  public void start() throws Exception {
+  private final Server server;
+
+  public WebImpl(
+      Persistence<?, ?, ?> persistence, Metrics metrics, AuthenticationService authentication) {
     server = new Server();
 
     ServerConnector connector = new ServerConnector(server);
@@ -46,60 +47,47 @@ public class WebImpl {
     ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
     context.setContextPath("/");
     ServletHolder servletHolder =
-        new ServletHolder(new CustomGraphQLServlet(persistence, authenticationService));
+        new ServletHolder(new CustomGraphQLServlet(persistence, authentication));
     context.addServlet(servletHolder, "/graphql/*");
-    ServletHolder schema =
-        new ServletHolder(new SchemaGraphQLServlet(persistence, authenticationService));
+    ServletHolder schema = new ServletHolder(new SchemaGraphQLServlet(persistence, authentication));
     context.addServlet(schema, "/graphql-schema");
 
     ServletHolder playground = new ServletHolder(new PlaygroundServlet());
     context.addServlet(playground, "/playground");
 
-    FilterHolder filter = new FilterHolder();
-    filter.setInitParameter(
-        CrossOriginFilter.ALLOWED_METHODS_PARAM, "POST,GET,OPTIONS,PUT,DELETE,PATCH");
-    filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
-    filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
-    filter.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "*");
-    filter.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
-    filter.setInitParameter(CrossOriginFilter.EXPOSED_HEADERS_PARAM, "Date");
-    CrossOriginFilter corsFilter = new CrossOriginFilter();
-    filter.setFilter(corsFilter);
+    EnumSet<DispatcherType> allDispatcherTypes = EnumSet.allOf(DispatcherType.class);
 
-    context.addFilter(
-        org.eclipse.jetty.servlets.CrossOriginFilter.class,
-        "/*",
-        EnumSet.allOf(DispatcherType.class));
+    FilterHolder corsFilter = new FilterHolder();
+    corsFilter.setInitParameter(
+        CrossOriginFilter.ALLOWED_METHODS_PARAM, "POST,GET,OPTIONS,PUT,DELETE,PATCH");
+    corsFilter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+    corsFilter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+    corsFilter.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "*");
+    corsFilter.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+    corsFilter.setInitParameter(CrossOriginFilter.EXPOSED_HEADERS_PARAM, "Date");
+    corsFilter.setFilter(new CrossOriginFilter());
+    context.addFilter(corsFilter, "/*", allDispatcherTypes);
+
+    context
+        .addFilter(InstrumentedFilter.class, "/graphql/*", allDispatcherTypes)
+        .setInitParameter("name-prefix", "io.stargate.GraphQL");
+    context
+        .addFilter(InstrumentedFilter.class, "/graphql-schema/*", allDispatcherTypes)
+        .setInitParameter("name-prefix", "io.stargate.GraphQLSchema");
+
+    MetricRegistry metricRegistry = metrics.getRegistry("graphql");
+    context.setAttribute(InstrumentedFilter.REGISTRY_ATTRIBUTE, metricRegistry);
 
     server.setHandler(context);
+  }
 
-    try {
-      server.start();
-      server.dump(System.err);
-    } catch (Exception ex) {
-      Logger.getLogger(WebImpl.class.getName()).log(Level.SEVERE, null, ex);
-    }
+  public void start() throws Exception {
+    server.start();
   }
 
   public void stop() throws Exception {
     if (server != null) {
       server.stop();
     }
-  }
-
-  public void setPersistence(Persistence persistence) {
-    this.persistence = persistence;
-  }
-
-  public Persistence getPersistence() {
-    return persistence;
-  }
-
-  public AuthenticationService getAuthenticationService() {
-    return authenticationService;
-  }
-
-  public void setAuthenticationService(AuthenticationService authenticationService) {
-    this.authenticationService = authenticationService;
   }
 }
