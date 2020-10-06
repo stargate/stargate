5492ea883567dac64e754ceaacf007f5c2379818
Olivier Michallat <olim7t@users.noreply.github.com>
2020-10-06 08:58:47 -0700

    Add more CQL tests (#169)
    Co-authored-by: Bret McGuire <bret.mcguire@datastax.com>
    Co-authored-by: Jorge Bay Gondra <jorgebaygondra@gmail.com>

---
+++ testing/src/test/java/io/stargate/it/cql/WarningsTest.java
@@ -0,0 +1,27 @@
+package io.stargate.it.cql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import io.stargate.it.storage.ClusterConnectionInfo;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WarningsTest extends JavaDriverTestBase {
+
+  public WarningsTest(ClusterConnectionInfo backend) {
+    super(backend);
+  }
+
+  @Test
+  @DisplayName("Should surface query warnings")
+  public void warningsTest() {
+    // Avoid using a system table
+    session.execute("CREATE TABLE test_warnings (k0 text, k1 int, v int, PRIMARY KEY (k0, k1))");
+    ResultSet resultSet = session.execute("SELECT count(*) FROM test_warnings");
+    List<String> warnings = resultSet.getExecutionInfo().getWarnings();
+
+    assertThat(warnings).containsExactly("Aggregation query used without partition key");
+  }
+}
