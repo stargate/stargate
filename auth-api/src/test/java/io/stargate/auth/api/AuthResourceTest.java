5b81d1a13412c46b79d5f590ff923a84f7bd6c06
Frank Rosner <frank@fam-rosner.de>
2020-10-11 16:33:58 +0200

    test creating tokens from credentials

--- auth-api/src/test/java/io/stargate/auth/api/AuthResourceTest.java
+++ auth-api/src/test/java/io/stargate/auth/api/AuthResourceTest.java
@@ -5,6 +5,7 @@ import io.dropwizard.testing.junit5.ResourceExtension;
 import io.stargate.auth.AuthenticationService;
 import io.stargate.auth.UnauthorizedException;
 import io.stargate.auth.model.AuthTokenResponse;
+import io.stargate.auth.model.Credentials;
 import io.stargate.auth.model.Secret;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -94,6 +95,73 @@ class AuthResourceTest {
         Response response = resource.target("/v1/auth/token/generate")
                 .request()
                 .post(Entity.entity(secret, MediaType.APPLICATION_JSON));
+
+        assertThat(response.getStatusInfo().getStatusCode()).isEqualTo(400);
+    }
+
+    @Test
+    void createTokenFromCredentialsSuccess() throws UnauthorizedException {
+        Credentials credentials = new Credentials("username", "password");
+        when(authService.createToken("username", "password")).thenReturn("token");
+
+        AuthTokenResponse token = resource.target("/v1/auth")
+                .request()
+                .post(Entity.entity(credentials, MediaType.APPLICATION_JSON), AuthTokenResponse.class);
+
+        assertThat(token.getAuthToken()).isEqualTo("token");
+    }
+
+    @Test
+    void createTokenFromCredentialsUnauthorized() throws UnauthorizedException {
+        Credentials credentials = new Credentials("username", "password");
+        when(authService.createToken("username", "password")).thenThrow(UnauthorizedException.class);
+
+        Response response = resource.target("/v1/auth")
+                .request()
+                .post(Entity.entity(credentials, MediaType.APPLICATION_JSON));
+
+        assertThat(response.getStatusInfo().getStatusCode()).isEqualTo(401);
+    }
+
+    @Test
+    void createTokenFromCredentialsInternalServerError() throws UnauthorizedException {
+        Credentials credentials = new Credentials("username", "password");
+        when(authService.createToken("username", "password")).thenThrow(RuntimeException.class);
+
+        Response response = resource.target("/v1/auth")
+                .request()
+                .post(Entity.entity(credentials, MediaType.APPLICATION_JSON));
+
+        assertThat(response.getStatusInfo().getStatusCode()).isEqualTo(500);
+    }
+
+    @Test
+    void createTokenFromCredentialsNoPayload() {
+        Response response = resource.target("/v1/auth")
+                .request()
+                .post(null);
+
+        assertThat(response.getStatusInfo().getStatusCode()).isEqualTo(400);
+    }
+
+    @Test
+    void createTokenFromCredentialsEmptyUsername() {
+        Credentials credentials = new Credentials("", "password");
+
+        Response response = resource.target("/v1/auth")
+                .request()
+                .post(Entity.entity(credentials, MediaType.APPLICATION_JSON));
+
+        assertThat(response.getStatusInfo().getStatusCode()).isEqualTo(400);
+    }
+
+    @Test
+    void createTokenFromCredentialsEmptyPassword() {
+        Credentials credentials = new Credentials("username", "");
+
+        Response response = resource.target("/v1/auth")
+                .request()
+                .post(Entity.entity(credentials, MediaType.APPLICATION_JSON));
 
         assertThat(response.getStatusInfo().getStatusCode()).isEqualTo(400);
     }
