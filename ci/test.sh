72ada4af4bb6e5733cf8e9a73e41365060d17486
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2020-10-12 15:58:45 -0500

    Test out google code build

---
+++ ci/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get update && apt-get install openjdk-8-jdk git python2.7 -y
+java -version
+
+git clone --branch master --single-branch https://github.com/riptano/ccm.git
+pushd ccm || exit
+sudo python setup.py install
+popd
+
+
+CCM_CLUSTER_START_TIMEOUT_OVERRIDE=600 ./mvnw -B verify --file pom.xml -P it-cassandra-3.11 -P it-cassandra-4.0
