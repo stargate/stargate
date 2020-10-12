b8510ff2d3edff970ba4509f80a9a8b6c18eb946
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2020-10-12 17:37:20 -0500

    Rearrange test setup

--- ci/test.sh
+++ ci/test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-adduser --disabled-password --gecos "" ubuntu
-su - ubuntu
 
-apt-get update && apt-get install openjdk-8-jdk git python2.7 python-setuptools python-six python-yaml -y
+apt-get update && apt-get install openjdk-8-jdk git python2.7 python-setuptools python-six python-yaml sudo -y
 java -version
 
 ln -sf /usr/bin/python2.7 /usr/bin/python
@@ -12,9 +10,13 @@ export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
 
 git clone --branch master --single-branch https://github.com/riptano/ccm.git
 pushd ccm || exit
-python setup.py install
+sudo python setup.py install
 popd
 
+adduser --disabled-password --gecos "" ubuntu
+su - ubuntu
+
+export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
 
 CCM_CLUSTER_START_TIMEOUT_OVERRIDE=600 ./mvnw -B verify --file pom.xml \
 -P it-cassandra-3.11 \
