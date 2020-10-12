9a3a0c5fc95b602ac3d887509bf523f5dd212dc0
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2020-10-12 16:29:18 -0500

    Few more python dependencies for ccm

--- ci/test.sh
+++ ci/test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-apt-get update && apt-get install openjdk-8-jdk git python2.7 -y
+apt-get update && apt-get install openjdk-8-jdk git python2.7 python-setuptools python-six python-yaml -y
 java -version
+
+ln -sf /usr/bin/python2.7 /usr/bin/python
 
 git clone --branch master --single-branch https://github.com/riptano/ccm.git
 pushd ccm || exit
-python2.7 setup.py install
+python setup.py install
 popd
 
 
-CCM_CLUSTER_START_TIMEOUT_OVERRIDE=600 ./mvnw -B verify --file pom.xml -P it-cassandra-3.11 -P it-cassandra-4.0
+CCM_CLUSTER_START_TIMEOUT_OVERRIDE=600 ./mvnw -B verify --file pom.xml \
+-P it-cassandra-3.11 \
+-P it-cassandra-4.0 \
+-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
