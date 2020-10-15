bc2d016f6da67d72b46d9fd3e9c8a96dde809b6f
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2020-10-15 13:35:08 -0500

    Prepull test image and add timestamps to script

--- ci/test.sh
+++ ci/test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+echoinfo() { echo "[$(date -Is)] - $@" 1>&2; }
+
+echoinfo "Starting test"
 # These directories are used by other steps so make sure the non-root user has access
 chown -R ubuntu:ubuntu /workspace/
 chown -R ubuntu:ubuntu /cache/
 
 # Need to switch users since we can't pass the right flag to allow running Cassandra as root
 sudo -i -u ubuntu bash << EOF
+echoinfo() { echo "[$(date -Is)] - $@" 1>&2; }
 export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
 export PATH=$PATH:$JAVA_HOME/bin
 export MAVEN_OPTS="-Dmaven.repo.local=/cache/.m2"
@@ -15,6 +19,7 @@ cd /workspace
 # create temp directories to avoid issues with concurrent execution
 cp -R . /tmp/$PERSISTENCE_BACKEND
 cd /tmp/$PERSISTENCE_BACKEND
+echoinfo "Copied directory"
 
 PROFILE=""
 case "$PERSISTENCE_BACKEND" in
@@ -23,11 +28,13 @@ case "$PERSISTENCE_BACKEND" in
   "dse-6.8")        PROFILE="dse,it-dse-6.8,!it-cassandra-4.0,!it-cassandra-3.11" ;;
 esac
 
-echo "Using backend $PERSISTENCE_BACKEND"
+echoinfo "Using backend $PERSISTENCE_BACKEND"
 
 export CCM_CLUSTER_START_TIMEOUT_OVERRIDE=600
 mvn -B failsafe:integration-test --file pom.xml -P "${PROFILE}" \
 -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+
+echoinfo "Test complete
 EOF
 
 
