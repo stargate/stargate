e1bb28a0c489235973c7c65cc4a8c8bb536ba237
tomekl007 <tomasz.lelek@datastax.com>
2020-10-15 11:04:38 +0200

    all settings should have `cdc.kafka` prefix

--- producer-kafka/src/main/java/io/stargate/producer/kafka/configuration/ConfigLoader.java
+++ producer-kafka/src/main/java/io/stargate/producer/kafka/configuration/ConfigLoader.java
@@ -18,7 +18,7 @@ package io.stargate.producer.kafka.configuration;
 import java.util.Map;
 
 public interface ConfigLoader {
-  String CDC_TOPIC_PREFIX_NAME = "cdc.topic.prefix-name";
+  String CDC_TOPIC_PREFIX_NAME = "cdc.kafka.topic.prefix-name";
   String CDC_KAFKA_PRODUCER_SETTING_PREFIX = "cdc.kafka.producer";
   String SCHEMA_REGISTRY_URL_SETTING_NAME = "schema.registry.url";
   String METRICS_ENABLED_SETTING_NAME = "cdc.kafka.metrics.enabled";
