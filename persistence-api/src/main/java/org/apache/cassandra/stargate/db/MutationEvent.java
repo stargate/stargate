bf848d24b7dde84b8849c84d9cdcef8401296715
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2020-10-29 10:56:51 +0100

    CDC schema information: Use classes in io.stargate.db.schema

--- persistence-api/src/main/java/org/apache/cassandra/stargate/db/MutationEvent.java
+++ persistence-api/src/main/java/org/apache/cassandra/stargate/db/MutationEvent.java
@@ -1,17 +1,19 @@
 package org.apache.cassandra.stargate.db;
 
+import io.stargate.db.schema.Table;
 import java.util.List;
-import org.apache.cassandra.stargate.schema.TableMetadata;
 
 /** Represents a change made in the database. */
 public interface MutationEvent {
   /** Gets the table metadata at the time of the event. */
-  TableMetadata getTable();
+  Table getTable();
 
   /** Timestamp of the change. */
   long getTimestamp();
+
   /** Gets the partitions keys of this event */
   List<CellValue> getPartitionKeys();
+
   /** Gets the clustering keys of this event */
   List<CellValue> getClusteringKeys();
 }
