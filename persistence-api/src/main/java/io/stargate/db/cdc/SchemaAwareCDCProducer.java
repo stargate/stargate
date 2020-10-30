d181ba4882cc206ba812e5f9f5b6b4e803e94110
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2020-10-30 10:07:04 +0100

    CDC schema information: Use classes in io.stargate.db.schema (#349)

--- persistence-api/src/main/java/io/stargate/db/cdc/SchemaAwareCDCProducer.java
+++ persistence-api/src/main/java/io/stargate/db/cdc/SchemaAwareCDCProducer.java
@@ -16,12 +16,11 @@
 package io.stargate.db.cdc;
 
 import com.datastax.oss.driver.shaded.guava.common.annotations.VisibleForTesting;
-import java.util.UUID;
+import io.stargate.db.schema.Table;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import org.apache.cassandra.stargate.db.MutationEvent;
-import org.apache.cassandra.stargate.schema.TableMetadata;
 
 /**
  * Represents a schema-aware CDC async producer.
@@ -37,14 +36,14 @@ import org.apache.cassandra.stargate.schema.TableMetadata;
  * </ul>
  */
 public abstract class SchemaAwareCDCProducer implements CDCProducer {
-  private final ConcurrentHashMap<UUID, TableSchemaManager> tableSchemaManager =
+  private final ConcurrentHashMap<Table, TableSchemaManager> tableSchemaManager =
       new ConcurrentHashMap<>();
 
   @Override
   public abstract CompletableFuture<Void> init();
 
   /** Asynchronously publishes the schema of the table. */
-  protected abstract CompletableFuture<Void> createTableSchemaAsync(TableMetadata table);
+  protected abstract CompletableFuture<Void> createTableSchemaAsync(Table table);
 
   /**
    * Asynchronously publishes the partition update.
@@ -61,7 +60,7 @@ public abstract class SchemaAwareCDCProducer implements CDCProducer {
   public CompletableFuture<Void> publish(MutationEvent mutation) {
     TableSchemaManager schemaManager =
         tableSchemaManager.computeIfAbsent(
-            mutation.getTable().getId(), k -> new TableSchemaManager(this::createTableSchemaAsync));
+            mutation.getTable(), k -> new TableSchemaManager(this::createTableSchemaAsync));
 
     return schemaManager
         .ensureCreated(mutation.getTable())
@@ -73,11 +72,11 @@ public abstract class SchemaAwareCDCProducer implements CDCProducer {
   /** Contains logic to handle different table schema versions. */
   @VisibleForTesting
   static class TableSchemaManager {
-    private final Function<TableMetadata, CompletableFuture<Void>> createHandler;
-    private final ConcurrentHashMap<TableIdentityKey, CompletableFuture<Void>> creators =
+    private final Function<Table, CompletableFuture<Void>> createHandler;
+    private final ConcurrentHashMap<Table, CompletableFuture<Void>> creators =
         new ConcurrentHashMap<>();
 
-    TableSchemaManager(Function<TableMetadata, CompletableFuture<Void>> createHandler) {
+    TableSchemaManager(Function<Table, CompletableFuture<Void>> createHandler) {
       this.createHandler = createHandler;
     }
 
@@ -90,38 +89,13 @@ public abstract class SchemaAwareCDCProducer implements CDCProducer {
      *
      * @return The UUID of the schema version that caused a changed in the table.
      */
-    CompletableFuture<Void> ensureCreated(TableMetadata table) {
-      TableIdentityKey identity = new TableIdentityKey(table);
-
+    CompletableFuture<Void> ensureCreated(Table table) {
       // Try to reuse the table schema for a given table hash code
-      return creators.computeIfAbsent(identity, k2 -> create(table));
+      return creators.computeIfAbsent(table, k2 -> create(table));
     }
 
-    private CompletableFuture<Void> create(TableMetadata table) {
+    private CompletableFuture<Void> create(Table table) {
       return this.createHandler.apply(table);
-    }
-
-    /** A hash map key that uses identity checks, valid for immutable objects. */
-    private static class TableIdentityKey {
-      private final Object identity;
-
-      TableIdentityKey(TableMetadata table) {
-        identity = table.getIdentity();
-      }
-
-      @Override
-      public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TableIdentityKey that = (TableIdentityKey) o;
-        // Use reference equality
-        return identity == that.identity;
-      }
-
-      @Override
-      public int hashCode() {
-        return System.identityHashCode(identity);
-      }
     }
   }
 }
