f35b8288894bbb83ad6d6156660e4b186197db79
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2020-11-11 11:03:28 -0600

    Set default authID in activator

--- auth-table-based-service/src/main/java/io/stargate/auth/table/AuthTableBasedServiceActivator.java
+++ auth-table-based-service/src/main/java/io/stargate/auth/table/AuthTableBasedServiceActivator.java
@@ -56,7 +56,8 @@ public class AuthTableBasedServiceActivator implements BundleActivator, ServiceL
 
   @Override
   public synchronized void start(BundleContext context) {
-    if (AUTH_TABLE_IDENTIFIER.equals(System.getProperty("stargate.auth_id"))) {
+    if (AUTH_TABLE_IDENTIFIER
+        .equals(System.getProperty("stargate.auth_id", AUTH_TABLE_IDENTIFIER))) {
       this.context = context;
       log.info("Starting authnTableBasedService and authzTableBasedService....");
 
