601be387a7138a5e0eef85bebcb8062dfcbfda07
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2020-11-12 00:49:51 -0600

    Fix formatting

--- testing/src/test/java/io/stargate/it/http/RestApiJWTAuthTest.java
+++ testing/src/test/java/io/stargate/it/http/RestApiJWTAuthTest.java
@@ -54,12 +54,12 @@ import org.testcontainers.containers.wait.strategy.Wait;
 @ExtendWith(CqlSessionExtension.class)
 @CqlSessionSpec(
     initQueries = {
-        "CREATE ROLE IF NOT EXISTS 'web_user' WITH PASSWORD = 'web_user' AND LOGIN = TRUE",
-        "CREATE KEYSPACE IF NOT EXISTS store WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':'1'}",
-        "CREATE TABLE IF NOT EXISTS store.shopping_cart (userid text, item_count int, last_update_timestamp timestamp, PRIMARY KEY (userid, last_update_timestamp));",
-        "INSERT INTO store.shopping_cart (userid, item_count, last_update_timestamp) VALUES ('9876', 2, toTimeStamp(toDate(now())))",
-        "INSERT INTO store.shopping_cart (userid, item_count, last_update_timestamp) VALUES ('1234', 5, toTimeStamp(toDate(now())))",
-        "GRANT MODIFY, SELECT ON TABLE store.shopping_cart TO web_user",
+      "CREATE ROLE IF NOT EXISTS 'web_user' WITH PASSWORD = 'web_user' AND LOGIN = TRUE",
+      "CREATE KEYSPACE IF NOT EXISTS store WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':'1'}",
+      "CREATE TABLE IF NOT EXISTS store.shopping_cart (userid text, item_count int, last_update_timestamp timestamp, PRIMARY KEY (userid, last_update_timestamp));",
+      "INSERT INTO store.shopping_cart (userid, item_count, last_update_timestamp) VALUES ('9876', 2, toTimeStamp(toDate(now())))",
+      "INSERT INTO store.shopping_cart (userid, item_count, last_update_timestamp) VALUES ('1234', 5, toTimeStamp(toDate(now())))",
+      "GRANT MODIFY, SELECT ON TABLE store.shopping_cart TO web_user",
     })
 public class RestApiJWTAuthTest extends BaseOsgiIntegrationTest {
 
