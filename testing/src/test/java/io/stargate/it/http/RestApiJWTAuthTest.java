4d770cbd1e4e5be87fe82b939c98ac4da84f2d9c
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2020-11-12 00:55:45 -0600

    Split grants for C*

--- testing/src/test/java/io/stargate/it/http/RestApiJWTAuthTest.java
+++ testing/src/test/java/io/stargate/it/http/RestApiJWTAuthTest.java
@@ -59,14 +59,15 @@ import org.testcontainers.containers.wait.strategy.Wait;
       "CREATE TABLE IF NOT EXISTS store.shopping_cart (userid text, item_count int, last_update_timestamp timestamp, PRIMARY KEY (userid, last_update_timestamp));",
       "INSERT INTO store.shopping_cart (userid, item_count, last_update_timestamp) VALUES ('9876', 2, toTimeStamp(toDate(now())))",
       "INSERT INTO store.shopping_cart (userid, item_count, last_update_timestamp) VALUES ('1234', 5, toTimeStamp(toDate(now())))",
-      "GRANT MODIFY, SELECT ON TABLE store.shopping_cart TO web_user",
+      "GRANT MODIFY ON TABLE store.shopping_cart TO web_user",
+      "GRANT SELECT ON TABLE store.shopping_cart TO web_user",
     })
 public class RestApiJWTAuthTest extends BaseOsgiIntegrationTest {
 
   private static final Logger logger = LoggerFactory.getLogger(RestApiJWTAuthTest.class);
 
-  private String keyspaceName = "store";
-  private String tableName = "shopping_cart";
+  private final String keyspaceName = "store";
+  private final String tableName = "shopping_cart";
 
   private static String authToken;
   private String host;
