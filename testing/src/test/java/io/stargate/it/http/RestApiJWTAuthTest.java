fe90d46057bfd2969221a7f7cc19494f069d8761
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2020-11-12 00:45:03 -0600

    Add permission to role for RestApiJWTAuthTest

--- testing/src/test/java/io/stargate/it/http/RestApiJWTAuthTest.java
+++ testing/src/test/java/io/stargate/it/http/RestApiJWTAuthTest.java
@@ -59,6 +59,7 @@ import org.testcontainers.containers.wait.strategy.Wait;
       "CREATE TABLE IF NOT EXISTS store.shopping_cart (userid text, item_count int, last_update_timestamp timestamp, PRIMARY KEY (userid, last_update_timestamp));",
       "INSERT INTO store.shopping_cart (userid, item_count, last_update_timestamp) VALUES ('9876', 2, toTimeStamp(toDate(now())))",
       "INSERT INTO store.shopping_cart (userid, item_count, last_update_timestamp) VALUES ('1234', 5, toTimeStamp(toDate(now())))",
+      "GRANT INSERT, UPDATE, DELETE, SELECT ON TABLE store.shopping_cart TO web_user",
     })
 public class RestApiJWTAuthTest extends BaseOsgiIntegrationTest {
 
@@ -116,7 +117,7 @@ public class RestApiJWTAuthTest extends BaseOsgiIntegrationTest {
         "http://"
             + keycloakContainer.getContainerIpAddress()
             + ":"
-            + +keycloakContainer.getMappedPort(keycloakPort);
+            + keycloakContainer.getMappedPort(keycloakPort);
 
     setupKeycloakUsers();
   }
