e9673045427f4d2910a13a4b1ea75788e701d5c0
Tomasz Lelek <tomasz.lelek@datastax.com>
2020-11-13 11:49:36 +0100

    Register producer extension based on config (#399)

--- producer-kafka/src/test/java/io/stargate/producer/kafka/KafkaProducerActivatorTest.java
+++ producer-kafka/src/test/java/io/stargate/producer/kafka/KafkaProducerActivatorTest.java
@@ -15,6 +15,9 @@
  */
 package io.stargate.producer.kafka;
 
+import static io.stargate.producer.kafka.KafkaProducerActivator.ENABLED_SETTING_NAME;
+import static io.stargate.producer.kafka.configuration.DefaultConfigLoader.CONFIG_STORE_MODULE_NAME;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
@@ -24,10 +27,18 @@ import static org.mockito.Mockito.when;
 
 import com.codahale.metrics.MetricRegistry;
 import io.stargate.config.store.api.ConfigStore;
+import io.stargate.config.store.api.ConfigWithOverrides;
+import io.stargate.config.store.yaml.ConfigStoreYaml;
 import io.stargate.core.metrics.api.Metrics;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Hashtable;
+import java.util.Objects;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Filter;
 import org.osgi.framework.InvalidSyntaxException;
@@ -36,16 +47,18 @@ import org.osgi.framework.ServiceReference;
 class KafkaProducerActivatorTest {
 
   @Test
-  public void shouldStartIfBothServicesAreRegistered() throws InvalidSyntaxException {
+  public void shouldStartIfBothServicesAreRegisteredAndEnabledInConfigStore()
+      throws InvalidSyntaxException {
     // given
     BundleContext bundleContext = mock(BundleContext.class);
     KafkaProducerActivator activator = new KafkaProducerActivator();
     mockFilterForBothServices(bundleContext);
     activator.start(bundleContext);
+    ConfigStore configStore = mockConfigStore("true");
 
     // when
     activator.tracker.startIfAllRegistered(mock(ServiceReference.class), mockMetrics());
-    activator.tracker.startIfAllRegistered(mock(ServiceReference.class), mock(ConfigStore.class));
+    activator.tracker.startIfAllRegistered(mock(ServiceReference.class), configStore);
 
     // then should not register service
     verify(bundleContext, times(1))
@@ -54,6 +67,28 @@ class KafkaProducerActivatorTest {
             any(KafkaCDCProducer.class),
             eq(new Hashtable<>()));
     Assertions.assertThat(activator.started).isTrue();
+  }
+
+  @Test
+  public void shouldNotRegisterIfBothServicesAreRegisteredButDisabledInConfigStore()
+      throws InvalidSyntaxException {
+    // given
+    BundleContext bundleContext = mock(BundleContext.class);
+    KafkaProducerActivator activator = new KafkaProducerActivator();
+    mockFilterForBothServices(bundleContext);
+    activator.start(bundleContext);
+    ConfigStore configStore = mockConfigStore("false");
+
+    // when
+    activator.tracker.startIfAllRegistered(mock(ServiceReference.class), mockMetrics());
+    activator.tracker.startIfAllRegistered(mock(ServiceReference.class), configStore);
+
+    // then should not register service
+    verify(bundleContext, times(0))
+        .registerService(
+            eq(KafkaCDCProducer.class.getName()),
+            any(KafkaCDCProducer.class),
+            eq(new Hashtable<>()));
   }
 
   @Test
@@ -94,6 +129,87 @@ class KafkaProducerActivatorTest {
             any(KafkaCDCProducer.class),
             eq(new Hashtable<>()));
     Assertions.assertThat(activator.started).isFalse();
+  }
+
+  @Test
+  public void shouldReturnThatServiceIsEnabled() {
+    // given
+    ConfigStore configStore = new ConfigStoreYaml(getProducerEnabledPath(), new MetricRegistry());
+
+    // when
+    boolean serviceEnabled = new KafkaProducerActivator().isServiceEnabled(configStore);
+
+    // then
+    assertThat(serviceEnabled).isTrue();
+  }
+
+  @Test
+  public void shouldReturnThatServiceIsDisabledByPickingSystemProperty() {
+    // given
+    ConfigStore configStore = new ConfigStoreYaml(getProducerEnabledPath(), new MetricRegistry());
+    try {
+      System.setProperty("cdc.kafka.enabled", "false");
+      // when
+      boolean serviceEnabled = new KafkaProducerActivator().isServiceEnabled(configStore);
+
+      // then
+      assertThat(serviceEnabled).isFalse();
+    } finally {
+      System.clearProperty("cdc.kafka.enabled");
+    }
+  }
+
+  @ParameterizedTest
+  @MethodSource("activatorDisabledPaths")
+  public void shouldReturnThatServiceIsDisabled(Path configPath) {
+    // given
+    ConfigStore configStore = new ConfigStoreYaml(configPath, new MetricRegistry());
+
+    // when
+    boolean serviceEnabled = new KafkaProducerActivator().isServiceEnabled(configStore);
+
+    // then
+    assertThat(serviceEnabled).isFalse();
+  }
+
+  public static Stream<Path> activatorDisabledPaths() {
+    return Stream.of(
+        getProducerDisabledPath(), getProducerEnabledNotSetPath(), Paths.get("non_existing"));
+  }
+
+  private static Path getProducerEnabledPath() {
+    return Paths.get(
+        Objects.requireNonNull(
+                KafkaProducerActivatorTest.class
+                    .getClassLoader()
+                    .getResource("stargate-config-enabled.yaml"))
+            .getPath());
+  }
+
+  private static Path getProducerDisabledPath() {
+    return Paths.get(
+        Objects.requireNonNull(
+                KafkaProducerActivatorTest.class
+                    .getClassLoader()
+                    .getResource("stargate-config-disabled.yaml"))
+            .getPath());
+  }
+
+  private static Path getProducerEnabledNotSetPath() {
+    return Paths.get(
+        Objects.requireNonNull(
+                KafkaProducerActivatorTest.class
+                    .getClassLoader()
+                    .getResource("stargate-config.yaml"))
+            .getPath());
+  }
+
+  private ConfigStore mockConfigStore(String settingValue) {
+    ConfigStore configStore = mock(ConfigStore.class);
+    ConfigWithOverrides configWithOverrides = mock(ConfigWithOverrides.class);
+    when(configStore.getConfigForModule(CONFIG_STORE_MODULE_NAME)).thenReturn(configWithOverrides);
+    when(configWithOverrides.getWithOverrides(ENABLED_SETTING_NAME)).thenReturn(settingValue);
+    return configStore;
   }
 
   private Metrics mockMetrics() {
