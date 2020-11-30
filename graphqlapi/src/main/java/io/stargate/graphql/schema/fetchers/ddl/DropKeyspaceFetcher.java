017092a220ec4f6edb3785f7f5ae5e913284848a
Olivier Michallat <olim7t@users.noreply.github.com>
2020-11-30 08:43:48 -0800

    GraphQL: Add dropKeyspace mutation (fixes #410) (#416)

---
+++ graphqlapi/src/main/java/io/stargate/graphql/schema/fetchers/ddl/DropKeyspaceFetcher.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright The Stargate Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.stargate.graphql.schema.fetchers.ddl;
+
+import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.querybuilder.SchemaBuilder;
+import com.datastax.oss.driver.api.querybuilder.schema.Drop;
+import graphql.schema.DataFetchingEnvironment;
+import io.stargate.auth.AuthenticationService;
+import io.stargate.db.Persistence;
+
+public class DropKeyspaceFetcher extends DdlQueryFetcher {
+
+  public DropKeyspaceFetcher(Persistence persistence, AuthenticationService authenticationService) {
+    super(persistence, authenticationService);
+  }
+
+  @Override
+  public String getQuery(DataFetchingEnvironment dataFetchingEnvironment) {
+    Drop query =
+        SchemaBuilder.dropKeyspace(
+            CqlIdentifier.fromInternal(dataFetchingEnvironment.getArgument("name")));
+    boolean ifExists = dataFetchingEnvironment.getArgumentOrDefault("ifExists", Boolean.FALSE);
+    if (ifExists) {
+      query = query.ifExists();
+    }
+    return query.asCql();
+  }
+}
