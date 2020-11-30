1778157677cb973c53e1d0743636f81f3f13e75f
tomekl007 <tomasz.lelek@datastax.com>
2020-11-30 13:53:49 +0100

    allow registration of N services

--- core/src/main/java/io/stargate/core/activator/BaseActivator.java
+++ core/src/main/java/io/stargate/core/activator/BaseActivator.java
@@ -17,10 +17,7 @@ package io.stargate.core.activator;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.errorprone.annotations.concurrent.GuardedBy;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import javax.annotation.Nullable;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -39,8 +36,6 @@ public abstract class BaseActivator implements BundleActivator {
 
   protected BundleContext context;
 
-  private Optional<Class<?>> targetServiceClass;
-
   public boolean started;
 
   @VisibleForTesting
@@ -49,22 +44,9 @@ public abstract class BaseActivator implements BundleActivator {
 
   @Nullable private ServiceRegistration<?> targetServiceRegistration;
 
-  /**
-   * @param activatorName - The name used when logging the progress of registration.
-   * @param targetServiceClass - This class will be used when registering the service. If null, then
-   *     the registration will not happen.
-   */
-  public BaseActivator(String activatorName, @Nullable Class<?> targetServiceClass) {
-    this.activatorName = activatorName;
-    this.targetServiceClass = Optional.ofNullable(targetServiceClass);
-  }
-
-  /**
-   * Convenience method for activators that does not register any service see docs for {@link
-   * this#BaseActivator(String, Class)}.
-   */
+  /** @param activatorName - The name used when logging the progress of registration. */
   public BaseActivator(String activatorName) {
-    this(activatorName, null);
+    this.activatorName = activatorName;
   }
 
   /**
@@ -72,9 +54,9 @@ public abstract class BaseActivator implements BundleActivator {
    * notification of all services passed as {@link this#dependencies()}. It will wait for a
    * notification denoting that service was registered using {@link
    * Tracker#addingService(ServiceReference)}. If all services are present, it will call the
-   * user-provided {@link this#createService()} and register it in the OSGi using {@link
+   * user-provided {@link this#createServices()} and register it in the OSGi using {@link
    * BundleContext#registerService(Class, Object, java.util.Dictionary)} if {@code
-   * targetServiceClass.isPresent()}. If it is not present, the {@link this#createService()} is
+   * targetServiceClass.isPresent()}. If it is not present, the {@link this#createServices()} is
    * called but there will bo no registration in the OSGi.
    */
   @Override
@@ -102,9 +84,8 @@ public abstract class BaseActivator implements BundleActivator {
 
   /**
    * It is calling the user-provided {@link this#stopService()} if the service was started
-   * successfully. If there {@link this#targetServiceClass} is present, and there was an OSGi
-   * service registration it will deregister service callint {@link
-   * BundleContext#ungetService(ServiceReference)}.
+   * successfully. If and there was an OSGi service registration it will deregister service calling
+   * {@link BundleContext#ungetService(ServiceReference)}.
    */
   @Override
   public synchronized void stop(BundleContext context) throws Exception {
@@ -130,14 +111,16 @@ public abstract class BaseActivator implements BundleActivator {
       return;
     }
     started = true;
-    ServiceAndProperties service = createService();
-    if (service != null && targetServiceClass.isPresent()) {
-      logger.info("Registering {} as {}", activatorName, targetServiceClass.get().getName());
-      this.targetServiceRegistration =
-          context.registerService(
-              targetServiceClass.get().getName(), service.service, service.properties);
+    List<ServiceAndProperties> services = createServices();
+    for (ServiceAndProperties service : services) {
+      if (service != null) {
+        logger.info("Registering {} as {}", activatorName, service.targetServiceClass.getName());
+        this.targetServiceRegistration =
+            context.registerService(
+                service.targetServiceClass.getName(), service.service, service.properties);
+      }
+      logger.info("Started {}", activatorName);
     }
-    logger.info("Started {}", activatorName);
   }
 
   public class Tracker extends ServiceTracker<Object, Object> {
@@ -149,7 +132,7 @@ public abstract class BaseActivator implements BundleActivator {
     /**
      * It will try to match all {@link this#dependencies()} with a ServiceReference notification.
      * After the notification is handled, it checks if all services in dependencies() are not null.
-     * If they are not, the client's provided {@link this#createService()} is called, and the
+     * If they are not, the client's provided {@link this#createServices()} is called, and the
      * service is registered. It will not register the service if it was already registered.
      */
     @Override
@@ -178,6 +161,17 @@ public abstract class BaseActivator implements BundleActivator {
   }
 
   /**
+   * Clients should override this method to create multiple Services that may be be registered in
+   * the OSGi container.
+   *
+   * @return list of services that has the service for OSGi registration and the properties that
+   *     will be passed.
+   */
+  protected List<ServiceAndProperties> createServices() {
+    return Collections.singletonList(createService());
+  };
+
+  /**
    * Clients should override this method to create the Service that may be be registered in the OSGi
    * container. The dependent services will contain all services registered by the {@link
    * this#dependencies()}.
@@ -185,8 +179,9 @@ public abstract class BaseActivator implements BundleActivator {
    * @return ServiceAndProperties that has the service for OSGi registration and the properties that
    *     will be passed or null if there is no registration service required.
    */
-  @Nullable
-  protected abstract ServiceAndProperties createService();
+  protected ServiceAndProperties createService() {
+    return null;
+  };
 
   /**
    * It will be called when the OSGi calls {@link this#stop(BundleContext)} and only if service was
@@ -197,22 +192,25 @@ public abstract class BaseActivator implements BundleActivator {
   /**
    * @return List of dependent services that this component relies on. It provides the
    *     happens-before meaning that all dependent services must be present before the {@link
-   *     this#createService()} is called.
+   *     this#createServices()} is called.
    */
   protected abstract List<ServicePointer<?>> dependencies();
 
   public static class ServiceAndProperties {
     private final Object service;
+    private final Class<?> targetServiceClass;
 
     private final Hashtable<String, String> properties;
 
-    public ServiceAndProperties(Object service, Hashtable<String, String> properties) {
+    public ServiceAndProperties(
+        Object service, Class<?> targetServiceClass, Hashtable<String, String> properties) {
       this.service = service;
+      this.targetServiceClass = targetServiceClass;
       this.properties = properties;
     }
 
-    public ServiceAndProperties(Object service) {
-      this(service, new Hashtable<>());
+    public ServiceAndProperties(Object service, Class<?> targetServiceClass) {
+      this(service, targetServiceClass, new Hashtable<>());
     }
   }
 
