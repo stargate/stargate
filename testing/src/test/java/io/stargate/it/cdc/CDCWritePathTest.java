1e7e7e7e11c1b50beebecda1c7aeb11118a7b102
tomekl007 <tomasz.lelek@datastax.com>
2020-12-07 17:10:53 +0100

    add a test for creating cdc_events table

--- testing/src/test/java/io/stargate/it/cdc/CDCWritePathTest.java
+++ testing/src/test/java/io/stargate/it/cdc/CDCWritePathTest.java
@@ -15,13 +15,18 @@
  */
 package io.stargate.it.cdc;
 
+import static io.stargate.db.cdc.datastore.CDCQueryBuilder.DEFAULT_CDC_EVENTS_TABLE;
 import static io.stargate.db.cdc.datastore.CDCQueryBuilder.DEFAULT_CDC_KEYSPACE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.oss.driver.api.core.metadata.schema.ColumnMetadata;
+import com.datastax.oss.driver.api.core.metadata.schema.TableMetadata;
+import com.datastax.oss.driver.api.core.type.DataTypes;
 import io.stargate.it.BaseOsgiIntegrationTest;
 import io.stargate.it.storage.StargateConnectionInfo;
 import java.net.InetSocketAddress;
@@ -72,5 +77,26 @@ public class CDCWritePathTest extends BaseOsgiIntegrationTest {
     Map<String, String> replication = row.getMap("replication", String.class, String.class);
     assertThat(replication.get("replication_factor")).isEqualTo("1");
     assertThat(replication.get("class")).isEqualTo("org.apache.cassandra.locator.SimpleStrategy");
+  }
+
+  @Test
+  public void shouldHaveCreatedCDCEventsTable() {
+    // when
+    TableMetadata tableMetadata =
+        session
+            .getMetadata()
+            .getKeyspace(DEFAULT_CDC_KEYSPACE)
+            .flatMap(keyspace -> keyspace.getTable(DEFAULT_CDC_EVENTS_TABLE))
+            .get();
+
+    // then
+    Map<CqlIdentifier, ColumnMetadata> columns = tableMetadata.getColumns();
+    assertThat(columns.get(CqlIdentifier.fromCql("shard")).getType()).isEqualTo(DataTypes.INT);
+    assertThat(columns.get(CqlIdentifier.fromCql("event_id")).getType())
+        .isEqualTo(DataTypes.TIMEUUID);
+    assertThat(columns.get(CqlIdentifier.fromCql("delivered_on_stream")).getType())
+        .isEqualTo(DataTypes.setOf(DataTypes.UUID));
+    assertThat(columns.get(CqlIdentifier.fromCql("payload")).getType()).isEqualTo(DataTypes.BLOB);
+    assertThat(columns.get(CqlIdentifier.fromCql("version")).getType()).isEqualTo(DataTypes.INT);
   }
 }
