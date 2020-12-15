1fa8620525ff34a3875cf4280f641f88fdb2bedd
Dmitri Bourlatchkov <40603221+dimas-b@users.noreply.github.com>
2020-12-15 18:59:36 -0500

    Re-include Docs API unit tests (fixes #527) (#531)
    * Convert Docs API tests to JUnit 5
    
    * Remove dependency on PowerMock as it does not appear to be usable under JUnit 5
    
    * Exclude transitive dependencies on JUnit4 to make sure tests do not use it accidentally

--- restapi/src/test/java/io/stargate/web/docsapi/dao/DocumentDBTest.java
+++ restapi/src/test/java/io/stargate/web/docsapi/dao/DocumentDBTest.java
@@ -1,6 +1,8 @@
 package io.stargate.web.docsapi.dao;
 
 import static io.stargate.db.query.TypedValue.javaValues;
+import static io.stargate.db.schema.Column.Kind.Clustering;
+import static io.stargate.db.schema.Column.Kind.PartitionKey;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,7 +25,9 @@ import io.stargate.db.datastore.ResultSet;
 import io.stargate.db.query.BoundQuery;
 import io.stargate.db.query.Query;
 import io.stargate.db.query.TypedValue.Codec;
+import io.stargate.db.schema.Column.Type;
 import io.stargate.db.schema.Schema;
+import io.stargate.db.schema.SchemaBuilder.SchemaBuilder__5;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -32,17 +36,37 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.UnaryOperator;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class DocumentDBTest {
+  private static final Schema schema = buildSchema();
+
   private DocumentDB documentDB;
   private TestDataStore ds;
   private static final ObjectMapper mapper = new ObjectMapper();
 
-  @Before
+  private static Schema buildSchema() {
+    SchemaBuilder__5 schemaBuilder =
+        Schema.build()
+            .keyspace("keyspace")
+            .table("table")
+            .column("key", Type.Text, PartitionKey)
+            .column("leaf", Type.Text)
+            .column("text_value", Type.Text)
+            .column("dbl_value", Type.Double)
+            .column("bool_value", Type.Boolean);
+
+    for (int i = 0; i < DocumentDB.MAX_DEPTH; i++) {
+      schemaBuilder = schemaBuilder.column("p" + i, Type.Text, Clustering);
+    }
+
+    return schemaBuilder.build();
+  }
+
+  @BeforeEach
   public void setup() throws UnauthorizedException {
-    ds = new TestDataStore();
+    ds = new TestDataStore(schema);
     AuthorizationService authorizationService = mock(AuthorizationService.class);
     doNothing().when(authorizationService).authorizeDataRead(anyString(), anyString(), anyString());
     documentDB = new DocumentDB(ds, "foo", authorizationService);
@@ -68,12 +92,22 @@ public class DocumentDBTest {
 
   @Test
   public void getInsertStatement() {
-    BoundQuery query =
-        documentDB.getInsertStatement(
-            "keyspace", "table", 1, new Object[DocumentDB.allColumns().size()]);
+    Object[] values = new Object[DocumentDB.allColumns().size()];
+    int idx = 0;
+    values[idx++] = "key";
+    for (int i = 0; i < DocumentDB.MAX_DEPTH; i++) {
+      values[idx++] = "value" + i;
+    }
+    values[idx++] = "leaf";
+    values[idx++] = "text";
+    values[idx++] = 1.2d; // dbl_value
+    values[idx++] = true; // bool_value
+    assertThat(idx).isEqualTo(values.length);
+
+    BoundQuery query = documentDB.getInsertStatement("keyspace", "table", 1, values);
     assertThat(query.queryString())
         .isEqualTo(
-            "INSERT INTO keyspace.table(key, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50, p51, p52, p53, p54, p55, p56, p57, p58, p59, p60, p61, p62, p63, leaf, text_value, dbl_value, bool_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) USING TIMESTAMP ?");
+            "INSERT INTO \"keyspace\".\"table\" (key, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50, p51, p52, p53, p54, p55, p56, p57, p58, p59, p60, p61, p62, p63, leaf, text_value, dbl_value, bool_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) USING TIMESTAMP ?");
   }
 
   @Test
@@ -84,7 +118,7 @@ public class DocumentDBTest {
     assertThat(query.queryString())
         .isEqualTo(
             "DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 = ? AND p2 = ?");
-    assertThat(javaValues(query.values())).isEqualTo(asList(1, "k", "a", "b", "c"));
+    assertThat(javaValues(query.values())).isEqualTo(asList(1L, "k", "a", "b", "c"));
   }
 
   @Test
@@ -94,9 +128,9 @@ public class DocumentDBTest {
             "keyspace", "table", "k", 1, ImmutableList.of("a", "b", "c", "d"));
     assertThat(query.queryString())
         .isEqualTo(
-            "DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 = ? AND p2 = ? AND p3 = ? AND p4 >= ? AND p4 <= ? ");
+            "DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 = ? AND p2 = ? AND p3 = ? AND p4 >= ? AND p4 <= ?");
     assertThat(javaValues(query.values()))
-        .isEqualTo(asList(1, "k", "a", "b", "c", "d", "[000000]", "[999999]"));
+        .isEqualTo(asList(1L, "k", "a", "b", "c", "d", "[000000]", "[999999]"));
   }
 
   @Test
@@ -111,9 +145,9 @@ public class DocumentDBTest {
             ImmutableList.of("a", "few", "keys"));
     assertThat(query.queryString())
         .isEqualTo(
-            "DELETE FROM keyspace.table USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 = ? AND p2 = ? AND p3 = ? AND p4 = ? AND p5 IN ?");
+            "DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 = ? AND p2 = ? AND p3 = ? AND p4 = ? AND p5 IN ?");
     assertThat(javaValues(query.values()))
-        .isEqualTo(asList(1, "k", "a", "b", "c", "d", "e", ImmutableList.of("a", "few", "keys")));
+        .isEqualTo(asList(1L, "k", "a", "b", "c", "d", "e", ImmutableList.of("a", "few", "keys")));
   }
 
   @Test
@@ -122,14 +156,14 @@ public class DocumentDBTest {
         documentDB.getExactPathDeleteStatement(
             "keyspace", "table", "key", 1, ImmutableList.of("a", "b", "c", "d", "e"));
     StringBuilder expected =
-        new StringBuilder("DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ?  WHERE key = ?");
+        new StringBuilder("DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ? WHERE key = ?");
     for (int i = 0; i < 64; i++) {
       expected.append(" AND p").append(i).append(" = ?");
     }
     assertThat(query.queryString()).isEqualTo(expected.toString());
 
     List<Object> expectedValues = new ArrayList<>();
-    expectedValues.add(1);
+    expectedValues.add(1L);
     expectedValues.add("key");
     expectedValues.addAll(ImmutableList.of("a", "b", "c", "d", "e"));
     for (int i = 5; i < 64; i++) {
@@ -156,12 +190,14 @@ public class DocumentDBTest {
 
   @Test
   public void deleteThenInsertBatch() throws UnauthorizedException {
-    ds = new TestDataStore();
+    ds = new TestDataStore(schema);
     AuthorizationService authorizationService = mock(AuthorizationService.class);
     doNothing().when(authorizationService).authorizeDataRead(anyString(), anyString(), anyString());
     documentDB = new DocumentDB(ds, "foo", authorizationService);
     List<String> path = ImmutableList.of("a", "b", "c");
     Map<String, Object> map = documentDB.newBindMap(path);
+    map.put("key", "key");
+    map.put("leaf", "c");
     map.put("bool_value", true);
     map.put("dbl_value", null);
     map.put("text_value", null);
@@ -173,24 +209,26 @@ public class DocumentDBTest {
 
     assertThat(generatedQueries.get(0).queryString())
         .isEqualTo(
-            "DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ? WHERE key = :key AND p0 = :p0 AND p1 = :p1 AND p2 = :p2");
+            "DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 = ? AND p2 = ?");
     assertThat(javaValues(generatedQueries.get(0).values())).isEqualTo(makeValues(0L, "key", path));
 
     assertThat(generatedQueries.get(1).queryString())
         .isEqualTo(
-            "INSERT INTO \"keyspace\".\"table\" (key, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50, p51, p52, p53, p54, p55, p56, p57, p58, p59, p60, p61, p62, p63, leaf, text_value, dbl_value, bool_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) USING TIMESTAMP ?");
+            "INSERT INTO \"keyspace\".\"table\" (key, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50, p51, p52, p53, p54, p55, p56, p57, p58, p59, p60, p61, p62, p63, leaf, text_value, dbl_value, bool_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) USING TIMESTAMP ?");
     assertThat(javaValues(generatedQueries.get(1).values())).isEqualTo(makeValues(values, 1L));
   }
 
   @Test
   public void deletePatchedPathsThenInsertBatch() throws UnauthorizedException {
-    ds = new TestDataStore();
+    ds = new TestDataStore(schema);
     AuthorizationService authorizationService = mock(AuthorizationService.class);
     doNothing().when(authorizationService).authorizeDataRead(anyString(), anyString(), anyString());
     documentDB = new DocumentDB(ds, "foo", authorizationService);
     List<String> path = ImmutableList.of("a", "b", "c");
     List<String> patchedKeys = ImmutableList.of("eric");
     Map<String, Object> map = documentDB.newBindMap(path);
+    map.put("key", "key");
+    map.put("leaf", "c");
     map.put("bool_value", null);
     map.put("dbl_value", 3.0);
     map.put("text_value", null);
@@ -208,7 +246,7 @@ public class DocumentDBTest {
 
     assertThat(generatedQueries.get(1).queryString())
         .isEqualTo(
-            "DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ?  WHERE key = ? AND p0 = ? AND p1 = ? AND p2 = ? AND p3 = ? AND p4 = ? AND p5 = ? AND p6 = ? AND p7 = ? AND p8 = ? AND p9 = ? AND p10 = ? AND p11 = ? AND p12 = ? AND p13 = ? AND p14 = ? AND p15 = ? AND p16 = ? AND p17 = ? AND p18 = ? AND p19 = ? AND p20 = ? AND p21 = ? AND p22 = ? AND p23 = ? AND p24 = ? AND p25 = ? AND p26 = ? AND p27 = ? AND p28 = ? AND p29 = ? AND p30 = ? AND p31 = ? AND p32 = ? AND p33 = ? AND p34 = ? AND p35 = ? AND p36 = ? AND p37 = ? AND p38 = ? AND p39 = ? AND p40 = ? AND p41 = ? AND p42 = ? AND p43 = ? AND p44 = ? AND p45 = ? AND p46 = ? AND p47 = ? AND p48 = ? AND p49 = ? AND p50 = ? AND p51 = ? AND p52 = ? AND p53 = ? AND p54 = ? AND p55 = ? AND p56 = ? AND p57 = ? AND p58 = ? AND p59 = ? AND p60 = ? AND p61 = ? AND p62 = ? AND p63 = ?");
+            "DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 = ? AND p2 = ? AND p3 = ? AND p4 = ? AND p5 = ? AND p6 = ? AND p7 = ? AND p8 = ? AND p9 = ? AND p10 = ? AND p11 = ? AND p12 = ? AND p13 = ? AND p14 = ? AND p15 = ? AND p16 = ? AND p17 = ? AND p18 = ? AND p19 = ? AND p20 = ? AND p21 = ? AND p22 = ? AND p23 = ? AND p24 = ? AND p25 = ? AND p26 = ? AND p27 = ? AND p28 = ? AND p29 = ? AND p30 = ? AND p31 = ? AND p32 = ? AND p33 = ? AND p34 = ? AND p35 = ? AND p36 = ? AND p37 = ? AND p38 = ? AND p39 = ? AND p40 = ? AND p41 = ? AND p42 = ? AND p43 = ? AND p44 = ? AND p45 = ? AND p46 = ? AND p47 = ? AND p48 = ? AND p49 = ? AND p50 = ? AND p51 = ? AND p52 = ? AND p53 = ? AND p54 = ? AND p55 = ? AND p56 = ? AND p57 = ? AND p58 = ? AND p59 = ? AND p60 = ? AND p61 = ? AND p62 = ? AND p63 = ?");
     String[] emptyStrings = new String[61];
     Arrays.fill(emptyStrings, "");
     assertThat(javaValues(generatedQueries.get(1).values()))
@@ -229,7 +267,7 @@ public class DocumentDBTest {
 
   @Test
   public void delete() throws UnauthorizedException {
-    ds = new TestDataStore();
+    ds = new TestDataStore(schema);
     AuthorizationService authorizationService = mock(AuthorizationService.class);
     doNothing().when(authorizationService).authorizeDataRead(anyString(), anyString(), anyString());
     documentDB = new DocumentDB(ds, "foo", authorizationService);
@@ -255,7 +293,7 @@ public class DocumentDBTest {
 
   @Test
   public void deleteDeadLeaves() throws UnauthorizedException {
-    ds = new TestDataStore();
+    ds = new TestDataStore(schema);
     AuthorizationService authorizationService = mock(AuthorizationService.class);
     doNothing().when(authorizationService).authorizeDataRead(anyString(), anyString(), anyString());
     documentDB = new DocumentDB(ds, "foo", authorizationService);
@@ -278,19 +316,24 @@ public class DocumentDBTest {
 
     assertThat(generatedQueries.get(0).queryString())
         .isEqualTo(
-            "DELETE FROM keyspace.table USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 IN (?)");
+            "DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 IN ?");
     assertThat(javaValues(generatedQueries.get(0).values()))
         .isEqualTo(asList(1L, "key", "a", singletonList("b")));
 
     assertThat(generatedQueries.get(1).queryString())
         .isEqualTo(
-            "DELETE FROM keyspace.table USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 >= ? AND p1 <= ?");
+            "DELETE FROM \"keyspace\".\"table\" USING TIMESTAMP ? WHERE key = ? AND p0 = ? AND p1 >= ? AND p1 <= ?");
     assertThat(javaValues(generatedQueries.get(1).values()))
         .isEqualTo(makeValues(1L, "key", "b", "[000000]", "[999999]"));
   }
 
   private static class TestDataStore implements DataStore {
     private final List<BoundQuery> recentQueries = new ArrayList<>();
+    private final Schema schema;
+
+    public TestDataStore(Schema schema) {
+      this.schema = schema;
+    }
 
     @Override
     public Codec valueCodec() {
@@ -320,7 +363,7 @@ public class DocumentDBTest {
 
     @Override
     public Schema schema() {
-      return null;
+      return schema;
     }
 
     @Override
