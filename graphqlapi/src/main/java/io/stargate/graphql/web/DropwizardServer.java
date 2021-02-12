409505033858b69d2edd1677aab80ffd61765ac0
Tomasz Lelek <tomasz.lelek@datastax.com>
2021-02-12 10:18:14 +0100

    Add a REST endpoint to download the definition of the Stargate CQL directives (#667)

--- graphqlapi/src/main/java/io/stargate/graphql/web/DropwizardServer.java
+++ graphqlapi/src/main/java/io/stargate/graphql/web/DropwizardServer.java
@@ -36,6 +36,7 @@ import io.stargate.graphql.web.resources.cqlfirst.GraphqlCache;
 import io.stargate.graphql.web.resources.cqlfirst.GraphqlDdlResource;
 import io.stargate.graphql.web.resources.cqlfirst.GraphqlDmlResource;
 import io.stargate.graphql.web.resources.schemafirst.AdminResource;
+import io.stargate.graphql.web.resources.schemafirst.FilesResource;
 import io.stargate.graphql.web.resources.schemafirst.NamespaceResource;
 import io.stargate.graphql.web.resources.schemafirst.SchemaFirstCache;
 import io.stargate.metrics.jersey.ResourceMetricsEventListener;
@@ -146,6 +147,7 @@ public class DropwizardServer extends Application<Configuration> {
     // GraphQL-first API:
     environment.jersey().register(AdminResource.class);
     environment.jersey().register(NamespaceResource.class);
+    environment.jersey().register(FilesResource.class);
 
     enableCors(environment);
 
