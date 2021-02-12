6669479631e32ed20bd03dca158ae0af1c449664
Dmitri Bourlatchkov <40603221+dimas-b@users.noreply.github.com>
2021-02-12 01:39:25 -0500

    Make update_changelog.sh runnable on Linux (#671)
    * `tail -r` is apparently not supported on Linux (at least not everywhere)
    * Hopefully `wc -l` is more standard

--- update_changelog.sh
+++ update_changelog.sh
@@ -9,16 +9,14 @@ CHANGELOG_FILE=CHANGELOG.md
 previous_version=$(head -5 $CHANGELOG_FILE | grep "##" | awk -F']' '{print $1}' | cut -c 5-)
 
 if [[ -z ${GITHUB_TOKEN-} ]]; then
-  echoerr "**WARNING** GITHUB_TOKEN is not currently set"
+  echo "**WARNING** GITHUB_TOKEN is not currently set" >&2
   exit 1
 fi
 
-
 # Remove the header so we can append the additions
-tail -n +2 "$CHANGELOG_FILE" > "$CHANGELOG_FILE.tmp" && mv "$CHANGELOG_FILE.tmp" "$CHANGELOG_FILE"
+tail -n +3 "$CHANGELOG_FILE" > "$CHANGELOG_FILE.tmp" && mv "$CHANGELOG_FILE.tmp" "$CHANGELOG_FILE"
 
 docker run -it --rm -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator -u stargate -p stargate -t $GITHUB_TOKEN --since-tag $previous_version -b $CHANGELOG_FILE
 
-
 # Remove the additional footer added
-tail -r "$CHANGELOG_FILE" | tail -n +4 | tail -r > "$CHANGELOG_FILE.tmp" && mv "$CHANGELOG_FILE.tmp" "$CHANGELOG_FILE"
+head -n $(( $(wc -l < $CHANGELOG_FILE) - 3 )) $CHANGELOG_FILE > "$CHANGELOG_FILE.tmp" && mv "$CHANGELOG_FILE.tmp" "$CHANGELOG_FILE"
