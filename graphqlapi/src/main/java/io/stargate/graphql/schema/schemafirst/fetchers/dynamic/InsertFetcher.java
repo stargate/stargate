1aced7e4a95c1cda4c86db1dc0b67ff85262e072
olim7t <omichallat+github@gmail.com>
2021-02-24 10:27:46 -0800

    Implement conditional inserts

--- graphqlapi/src/main/java/io/stargate/graphql/schema/schemafirst/fetchers/dynamic/InsertFetcher.java
+++ graphqlapi/src/main/java/io/stargate/graphql/schema/schemafirst/fetchers/dynamic/InsertFetcher.java
@@ -16,6 +16,7 @@
 package io.stargate.graphql.schema.schemafirst.fetchers.dynamic;
 
 import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.DataFetchingFieldSelectionSet;
 import io.stargate.auth.AuthenticationService;
 import io.stargate.auth.AuthenticationSubject;
 import io.stargate.auth.AuthorizationService;
@@ -25,6 +26,8 @@ import io.stargate.auth.TypedKeyValue;
 import io.stargate.auth.UnauthorizedException;
 import io.stargate.db.datastore.DataStore;
 import io.stargate.db.datastore.DataStoreFactory;
+import io.stargate.db.datastore.ResultSet;
+import io.stargate.db.datastore.Row;
 import io.stargate.db.query.BoundDMLQuery;
 import io.stargate.db.query.builder.AbstractBound;
 import io.stargate.db.query.builder.ValueModifier;
@@ -34,10 +37,13 @@ import io.stargate.graphql.schema.schemafirst.processor.EntityMappingModel;
 import io.stargate.graphql.schema.schemafirst.processor.FieldMappingModel;
 import io.stargate.graphql.schema.schemafirst.processor.InsertMappingModel;
 import io.stargate.graphql.schema.schemafirst.processor.MappingModel;
+import io.stargate.graphql.schema.schemafirst.processor.ResponseMappingModel;
+import io.stargate.graphql.schema.schemafirst.processor.ResponseMappingModel.TechnicalField;
 import io.stargate.graphql.schema.schemafirst.util.TypeHelper;
 import io.stargate.graphql.schema.schemafirst.util.Uuids;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -62,8 +68,10 @@ public class InsertFetcher extends DynamicFetcher<Map<String, Object>> {
       DataStore dataStore,
       AuthenticationSubject authenticationSubject)
       throws UnauthorizedException {
+    DataFetchingFieldSelectionSet selectionSet = environment.getSelectionSet();
 
     EntityMappingModel entityModel = model.getEntity();
+    boolean isLwt = model.ifNotExists();
     Keyspace keyspace = dataStore.schema().keyspace(entityModel.getKeyspaceName());
     Map<String, Object> input = environment.getArgument(model.getEntityArgumentName());
     Map<String, Object> response = new LinkedHashMap<>();
@@ -86,9 +94,9 @@ public class InsertFetcher extends DynamicFetcher<Map<String, Object>> {
         continue;
       }
       setters.add(ValueModifier.set(column.getCqlName(), cqlValue));
-      if (environment.getSelectionSet().contains(graphqlName)) {
-        response.put(graphqlName, graphqlValue);
-      }
+      // Echo the values back to the response now. We might override that later if the query turned
+      // out to be a failed LWT.
+      writeEntityField(graphqlName, graphqlValue, selectionSet, response);
     }
 
     AbstractBound<?> query =
@@ -96,6 +104,7 @@ public class InsertFetcher extends DynamicFetcher<Map<String, Object>> {
             .queryBuilder()
             .insertInto(entityModel.getKeyspaceName(), entityModel.getCqlName())
             .value(setters)
+            .ifNotExists(isLwt)
             .build()
             .bind();
 
@@ -107,8 +116,32 @@ public class InsertFetcher extends DynamicFetcher<Map<String, Object>> {
         Scope.MODIFY,
         SourceAPI.GRAPHQL);
 
-    executeUnchecked(query, dataStore);
+    ResultSet resultSet = executeUnchecked(query, dataStore);
 
+    boolean applied;
+    if (isLwt) {
+      Row row = resultSet.one();
+      applied = row.getBoolean("[applied]");
+      if (!applied) {
+        // The row contains the existing data, write it in the response.
+        for (FieldMappingModel field : model.getEntity().getAllColumns()) {
+          if (row.columns().stream().noneMatch(c -> c.name().equals(field.getCqlName()))) {
+            continue;
+          }
+          Object cqlValue = row.getObject(field.getCqlName());
+          writeEntityField(
+              field.getGraphqlName(),
+              toGraphqlValue(cqlValue, field.getCqlType(), field.getGraphqlType()),
+              selectionSet,
+              response);
+        }
+      }
+    } else {
+      applied = true;
+    }
+    if (selectionSet.contains(TechnicalField.APPLIED.getGraphqlName())) {
+      response.put(TechnicalField.APPLIED.getGraphqlName(), applied);
+    }
     return response;
   }
 
@@ -120,5 +153,40 @@ public class InsertFetcher extends DynamicFetcher<Map<String, Object>> {
       return Uuids.timeBased();
     }
     throw new AssertionError("This shouldn't get called for CQL type " + cqlType);
+  }
+
+  /** Writes an entity field in the response. */
+  private void writeEntityField(
+      String fieldName,
+      Object value,
+      DataFetchingFieldSelectionSet selectionSet,
+      Map<String, Object> responseMap) {
+
+    // Determine if we need an additional level of nesting. This happens if the mutation returns a
+    // payload type that contains the entity.
+    String rootPath = null;
+    if (model.getResponsePayloadType().isPresent()) {
+      ResponseMappingModel payloadType = model.getResponsePayloadType().get();
+      if (!payloadType.getEntityField().isPresent()) {
+        // This can happen if the payload only contains "technical" fields, like `applied`. In that
+        // case we never need to write any field.
+        return;
+      }
+      rootPath = payloadType.getEntityField().get().getName();
+    }
+
+    // Check if the GraphQL query asked for that field.
+    String selectionPattern = (rootPath == null) ? fieldName : rootPath + '/' + fieldName;
+    if (!selectionSet.contains(selectionPattern)) {
+      return;
+    }
+
+    @SuppressWarnings("unchecked")
+    Map<String, Object> targetMap =
+        (rootPath == null)
+            ? responseMap
+            : (Map<String, Object>)
+                responseMap.computeIfAbsent(rootPath, __ -> new HashMap<String, Object>());
+    targetMap.put(fieldName, value);
   }
 }
