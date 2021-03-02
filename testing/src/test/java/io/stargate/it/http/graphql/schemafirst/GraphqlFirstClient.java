53967c4651f213b160cbfe2c6e45433d4297f930
olim7t <omichallat+github@gmail.com>
2021-03-01 17:52:50 -0800

    Rename test package

--- testing/src/test/java/io/stargate/it/http/graphql/schemafirst/GraphqlFirstClient.java
+++ testing/src/test/java/io/stargate/it/http/graphql/schemafirst/GraphqlFirstClient.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.stargate.it.http.graphql.graphfirst;
+package io.stargate.it.http.graphql.schemafirst;
 
 import com.jayway.jsonpath.JsonPath;
 import io.stargate.it.http.RestUtils;
