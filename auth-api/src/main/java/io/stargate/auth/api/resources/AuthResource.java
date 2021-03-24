144565c141fc716a57a7be01edfa3d0f7640e4de
Doug Wettlaufer <45750136+dougwettlaufer@users.noreply.github.com>
2021-03-24 13:50:03 -0500

    Add Cache-Control header to auth-api responses - Fixes #762 (#767)
    * Add Cache-Control header to auth-api responses
    
    * Add padding to max-age for returned auth tokens
    
    * Use CacheControl object instead of creating each time
    
    * Add comment for tokenMaxAge

--- auth-api/src/main/java/io/stargate/auth/api/resources/AuthResource.java
+++ auth-api/src/main/java/io/stargate/auth/api/resources/AuthResource.java
@@ -35,6 +35,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -52,8 +53,13 @@ public class AuthResource {
 
   private static final Logger logger = LoggerFactory.getLogger(AuthResource.class);
 
+  // The token's Time To Live in seconds
+  private static final int tokenMaxAge =
+      Integer.parseInt(System.getProperty("stargate.auth_tokenttl", "1800"));
   private final AuthenticationService authService;
   private final boolean shouldEnableUsernameToken;
+  private final CacheControl cacheControlNoStore = cacheControlNoStore();
+  private final CacheControl cacheControlWithMaxAge = cacheControlWithAge();
 
   @Inject
   public AuthResource(AuthenticationService authService) {
@@ -84,18 +90,21 @@ public class AuthResource {
     if (secret == null) {
       return Response.status(Response.Status.BAD_REQUEST)
           .entity(new Error("Must provide a body to the request"))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
     if (secret.getKey() == null || secret.getKey().equals("")) {
       return Response.status(Response.Status.BAD_REQUEST)
           .entity(new Error("Must provide key in request"))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
     if (secret.getSecret() == null || secret.getSecret().equals("")) {
       return Response.status(Response.Status.BAD_REQUEST)
           .entity(new Error("Must provide secret in request"))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
@@ -105,16 +114,19 @@ public class AuthResource {
     } catch (UnauthorizedException e) {
       return Response.status(Response.Status.UNAUTHORIZED)
           .entity(new Error("Failed to create token: " + e.getMessage()))
+          .cacheControl(cacheControlNoStore)
           .build();
     } catch (Exception e) {
       logger.error("Failed to create token", e);
       return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
           .entity(new Error("Failed to create token: " + e.getMessage()))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
     return Response.status(Response.Status.CREATED)
         .entity(new AuthTokenResponse().authToken(token))
+        .cacheControl(cacheControlWithMaxAge)
         .build();
   }
 
@@ -138,18 +150,21 @@ public class AuthResource {
     if (credentials == null) {
       return Response.status(Response.Status.BAD_REQUEST)
           .entity(new Error("Must provide a body to the request"))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
     if (credentials.getUsername() == null || credentials.getUsername().equals("")) {
       return Response.status(Response.Status.BAD_REQUEST)
           .entity(new Error("Must provide username in request"))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
     if (credentials.getPassword() == null || credentials.getPassword().equals("")) {
       return Response.status(Response.Status.BAD_REQUEST)
           .entity(new Error("Must provide password in request"))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
@@ -161,16 +176,19 @@ public class AuthResource {
     } catch (UnauthorizedException e) {
       return Response.status(Response.Status.UNAUTHORIZED)
           .entity(new Error("Failed to create token: " + e.getMessage()))
+          .cacheControl(cacheControlNoStore)
           .build();
     } catch (Exception e) {
       logger.error("Failed to create token", e);
       return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
           .entity(new Error("Failed to create token: " + e.getMessage()))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
     return Response.status(Response.Status.CREATED)
         .entity(new AuthTokenResponse().authToken(token))
+        .cacheControl(cacheControlWithMaxAge)
         .build();
   }
 
@@ -211,18 +229,21 @@ public class AuthResource {
     if (!shouldEnableUsernameToken) {
       return Response.status(Response.Status.BAD_REQUEST)
           .entity(new Error("Generating a token for a username is not allowed"))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
     if (usernameCredentials == null) {
       return Response.status(Response.Status.BAD_REQUEST)
           .entity(new Error("Must provide a body to the request"))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
     if (usernameCredentials.getUsername() == null || usernameCredentials.getUsername().equals("")) {
       return Response.status(Response.Status.BAD_REQUEST)
           .entity(new Error("Must provide username in request"))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
@@ -232,16 +253,53 @@ public class AuthResource {
     } catch (UnauthorizedException e) {
       return Response.status(Response.Status.UNAUTHORIZED)
           .entity(new Error("Failed to create token: " + e.getMessage()))
+          .cacheControl(cacheControlNoStore)
           .build();
     } catch (Exception e) {
       logger.error("Failed to create token", e);
       return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
           .entity(new Error("Failed to create token: " + e.getMessage()))
+          .cacheControl(cacheControlNoStore)
           .build();
     }
 
     return Response.status(Response.Status.CREATED)
         .entity(new AuthTokenResponse().authToken(token))
+        .cacheControl(cacheControlWithMaxAge)
         .build();
+  }
+
+  /**
+   * Used to create a Cache-Control header containing the proper values for the token's max-age and
+   * s-maxage as defined by {@code stargate.auth_tokenttl} minus some time to avoid expiration. If
+   * the property is not set then the default value of 1790 will be used.
+   *
+   * @return A {@link CacheControl} object which is converted into a Cache-Control header with
+   *     proper directives.
+   */
+  private CacheControl cacheControlWithAge() {
+    // For a large enough TTL it's safe to knock off 10 seconds but for a smaller number reducing by
+    // that much will negatively impact caching so instead subtract only 1 second.
+    int maxAge = tokenMaxAge <= 100 ? tokenMaxAge - 1 : tokenMaxAge - 10;
+    CacheControl cacheControl = new CacheControl();
+    cacheControl.setMaxAge(maxAge);
+    cacheControl.setSMaxAge(maxAge);
+
+    return cacheControl;
+  }
+
+  /**
+   * Used to create a Cache-Control header that will indicate the response should not be cached.
+   * This is intended to be used in the event of a failure to authenticate.
+   *
+   * @return A {@link CacheControl} object denoting the response should not be cached.
+   */
+  private CacheControl cacheControlNoStore() {
+    CacheControl cacheControl = new CacheControl();
+    cacheControl.setMaxAge(0);
+    cacheControl.setSMaxAge(0);
+    cacheControl.setNoStore(true);
+
+    return cacheControl;
   }
 }
