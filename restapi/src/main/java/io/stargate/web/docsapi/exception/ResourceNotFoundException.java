53a67148c0068716be06347cca4b8996a6a8973c
Ivan Senic <senic.ivan@gmail.com>
2021-03-26 09:34:12 +0100

    fixes #775: better status codes for missing tables in document api (#785)

---
+++ restapi/src/main/java/io/stargate/web/docsapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright The Stargate Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.stargate.web.docsapi.exception;
+
+/** Thrown when API can not find a requested resource. */
+public class ResourceNotFoundException extends RuntimeException {
+  public ResourceNotFoundException(String msg) {
+    super(msg);
+  }
+
+  public ResourceNotFoundException(String msg, Throwable cause) {
+    super(msg, cause);
+  }
+}
