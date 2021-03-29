f658478b85f9172244f6c20581d01588f30d658d
Olivier Michallat <olim7t@users.noreply.github.com>
2021-03-29 09:39:22 -0700

    Improve comments in DirectedGraph

--- graphqlapi/src/main/java/io/stargate/graphql/schema/schemafirst/util/DirectedGraph.java
+++ graphqlapi/src/main/java/io/stargate/graphql/schema/schemafirst/util/DirectedGraph.java
@@ -29,22 +29,29 @@ import java.util.Map;
 import java.util.Queue;
 import net.jcip.annotations.NotThreadSafe;
 
-/** A basic directed graph implementation to perform topological sorts. */
+/**
+ * A basic directed graph implementation to perform topological sorts.
+ *
+ * <p>This class is <b>not</b> a general-purpose graph implementation: it is only intended for
+ * clients that will build a graph, sort it, and then immediately throw it away. In particular, once
+ * {@link #topologicalSort()} has been invoked, it is not possible to add vertices or sort again.
+ */
 @NotThreadSafe
 public class DirectedGraph<VertexT> {
 
-  // We need to keep track of the predecessor count. For simplicity, use a map to store it
-  // alongside the vertices.
-  private final Map<VertexT, Integer> vertices;
+  /** How many predecessors each vertex has. */
+  private final Map<VertexT, Integer> predecessorCounts;
+  /** The list successors of each vertex. */
   private final Multimap<VertexT, VertexT> adjacencyList;
+
   private boolean wasSorted;
 
   public DirectedGraph(Collection<VertexT> vertices) {
-    this.vertices = Maps.newLinkedHashMapWithExpectedSize(vertices.size());
+    this.predecessorCounts = Maps.newLinkedHashMapWithExpectedSize(vertices.size());
     this.adjacencyList = LinkedHashMultimap.create();
 
     for (VertexT vertex : vertices) {
-      this.vertices.put(vertex, 0);
+      this.predecessorCounts.put(vertex, 0);
     }
   }
 
@@ -55,50 +62,72 @@ public class DirectedGraph<VertexT> {
   }
 
   /**
-   * this assumes that {@code from} and {@code to} were part of the vertices passed to the
-   * constructor
+   * @throws IllegalStateException if {@link #topologicalSort()} was invoked before
+   * @throws IllegalArgumentException if {@code from} or {@code to} is not part of the vertices
+   *     passed to the constructor
    */
   public void addEdge(VertexT from, VertexT to) {
-    Preconditions.checkArgument(vertices.containsKey(from) && vertices.containsKey(to));
+    Preconditions.checkState(!wasSorted);
+    Preconditions.checkArgument(
+        predecessorCounts.containsKey(from) && predecessorCounts.containsKey(to));
     adjacencyList.put(from, to);
-    vertices.put(to, vertices.get(to) + 1);
+    predecessorCounts.put(to, predecessorCounts.get(to) + 1);
   }
 
-  /** one-time use only, calling this multiple times on the same graph won't work */
+  /**
+   * Performs a topological sort of the graph: for every directed edge {@code from->to}, {@code
+   * from} will come before {@code to} in the resulting list.
+   *
+   * <p>Implementation note: this is based on <a
+   * href="https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm">Kahn's algorithm</a>,
+   * the running time is linear in the number of nodes plus the number of edges.
+   *
+   * @throws IllegalStateException if this method was already invoked on this graph before
+   * @throws IllegalArgumentException if the graph has a cycle
+   */
   public List<VertexT> topologicalSort() {
     Preconditions.checkState(!wasSorted);
     wasSorted = true;
 
-    Queue<VertexT> queue = new ArrayDeque<>();
+    Queue<VertexT> startNodes = new ArrayDeque<>();
+    List<VertexT> result = Lists.newArrayList();
 
-    for (Map.Entry<VertexT, Integer> entry : vertices.entrySet()) {
+    // Start with all the nodes that have no predecessors
+    for (Map.Entry<VertexT, Integer> entry : predecessorCounts.entrySet()) {
       if (entry.getValue() == 0) {
-        queue.add(entry.getKey());
+        startNodes.add(entry.getKey());
       }
     }
 
-    List<VertexT> result = Lists.newArrayList();
-    while (!queue.isEmpty()) {
-      VertexT vertex = queue.remove();
+    // For each start node:
+    // - move it to the result
+    // - for each of its successors, decrement the predecessor count, and possibly promote to a
+    //   start node
+    while (!startNodes.isEmpty()) {
+      VertexT vertex = startNodes.remove();
       result.add(vertex);
       for (VertexT successor : adjacencyList.get(vertex)) {
         if (decrementAndGetCount(successor) == 0) {
-          queue.add(successor);
+          startNodes.add(successor);
         }
       }
     }
 
-    if (result.size() != vertices.size()) {
+    // If we've moved every node to the result, we have a solution, otherwise there is a cycle
+    if (result.size() != predecessorCounts.size()) {
       throw new IllegalArgumentException("failed to perform topological sort, graph has a cycle");
     }
 
     return result;
   }
 
+  @SuppressWarnings("")
   private int decrementAndGetCount(VertexT vertex) {
-    Integer count = vertices.get(vertex);
-    count = count - 1;
-    vertices.put(vertex, count);
-    return count;
+    return predecessorCounts.compute(
+        vertex,
+        (__, count) -> {
+          assert count != null;
+          return count - 1;
+        });
   }
 }
