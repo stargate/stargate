a717a5732c0658c13c87c6219400ae4687c599b9
Michael Penick <michael.penick@datastax.com>
2021-04-28 16:09:43 -0400

    Cleanup

--- restapi/src/main/java/io/stargate/web/docsapi/service/query/filter/operation/ValueFilterOperation.java
+++ restapi/src/main/java/io/stargate/web/docsapi/service/query/filter/operation/ValueFilterOperation.java
@@ -1,5 +1,24 @@
+/*
+ * Copyright The Stargate Authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.stargate.web.docsapi.service.query.filter.operation;
 
+/**
+ * Predicate that can compare values against {@link String}, {@link Double}, {@link Boolean}
+ * database values.
+ */
 public interface ValueFilterOperation extends BaseFilterOperation {
 
   /**
@@ -8,7 +27,7 @@ public interface ValueFilterOperation extends BaseFilterOperation {
    * @param filterValue Filter value, can be <code>null</code>
    * @param dbValue DB value, can be <code>null</code>
    * @return <code>true</code> if the filter value matches the predicate, otherwise <code>false
-   *     </code>
+   * </code>
    */
   boolean test(String filterValue, String dbValue);
 
@@ -29,7 +48,7 @@ public interface ValueFilterOperation extends BaseFilterOperation {
    * @param filterValue Filter value, can be <code>null</code>
    * @param dbValue DB value, can be <code>null</code>
    * @return <code>true</code> if the filter value matches the predicate, otherwise <code>false
-   *     </code>
+   * </code>
    */
   boolean test(Number filterValue, Double dbValue);
 
@@ -50,7 +69,7 @@ public interface ValueFilterOperation extends BaseFilterOperation {
    * @param filterValue Filter value, can be <code>null</code>
    * @param dbValue DB value, can be <code>null</code>
    * @return <code>true</code> if the filter value matches the predicate, otherwise <code>false
-   *     </code>
+   * </code>
    */
   boolean test(Boolean filterValue, Boolean dbValue);
 
@@ -63,5 +82,5 @@ public interface ValueFilterOperation extends BaseFilterOperation {
    */
   default void validateBooleanFilterInput(Boolean filterValue) {
     // default impl empty
-  };
+  }
 }
