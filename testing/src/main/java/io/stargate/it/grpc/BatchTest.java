9aa24d715d149ec335692de7aeb766b6291ecfbf
Michael Penick <michael.penick@datastax.com>
2021-05-06 14:21:49 -0400

    Batch integration tests

--- testing/src/main/java/io/stargate/it/grpc/BatchTest.java
+++ testing/src/main/java/io/stargate/it/grpc/BatchTest.java
@@ -1,14 +1,20 @@
 package io.stargate.it.grpc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.StringValue;
 import io.stargate.it.driver.CqlSessionExtension;
 import io.stargate.it.driver.CqlSessionSpec;
 import io.stargate.it.driver.TestKeyspace;
 import io.stargate.proto.QueryOuterClass.Batch;
 import io.stargate.proto.QueryOuterClass.BatchParameters;
-import io.stargate.proto.QueryOuterClass.BatchQuery;
+import io.stargate.proto.QueryOuterClass.Result;
+import io.stargate.proto.QueryOuterClass.ResultSet;
 import io.stargate.proto.StargateGrpc.StargateBlockingStub;
+import java.util.Arrays;
+import java.util.HashSet;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -20,22 +26,37 @@ import org.junit.jupiter.api.extension.ExtendWith;
 public class BatchTest extends GrpcIntegrationTest {
 
   @Test
-  public void simpleBatch(@TestKeyspace CqlIdentifier keyspace) {
-    StargateBlockingStub tokenStub = stub.withCallCredentials(new StargateBearerToken(authToken));
+  public void simpleBatch(@TestKeyspace CqlIdentifier keyspace)
+      throws InvalidProtocolBufferException {
+    StargateBlockingStub stub = this.stub.withCallCredentials(new StargateBearerToken(authToken));
 
-    tokenStub.executeBatch(
-        Batch.newBuilder()
-            .addQueries(query("INSERT INTO test (k, v) VALUES ('a', 1)"))
-            .addQueries(query("INSERT INTO test (k, v) VALUES ('b', 2)"))
-            .addQueries(query("INSERT INTO test (k, v) VALUES ('c', 3)"))
-            .setParameters(
-                BatchParameters.newBuilder()
-                    .setKeyspace(StringValue.of(keyspace.toString()))
-                    .build())
-            .build());
-  }
+    StringValue keyspaceValue = StringValue.of(keyspace.toString());
 
-  private static BatchQuery query(String cql) {
-    return BatchQuery.newBuilder().setCql(cql).build();
+    Result result =
+        stub.executeBatch(
+            Batch.newBuilder()
+                .addQueries(cqlBatchQuery("INSERT INTO test (k, v) VALUES ('a', 1)"))
+                .addQueries(
+                    cqlBatchQuery(
+                        "INSERT INTO test (k, v) VALUES (?, ?)", stringValue("b"), intValue(2)))
+                .addQueries(
+                    cqlBatchQuery(
+                        "INSERT INTO test (k, v) VALUES (?, ?)", stringValue("c"), intValue(3)))
+                .setParameters(BatchParameters.newBuilder().setKeyspace(keyspaceValue).build())
+                .build());
+    assertThat(result).isNotNull();
+
+    result =
+        stub.executeQuery(
+            cqlQuery("SELECT * FROM test", cqlQueryParameters().setKeyspace(keyspaceValue)));
+    assertThat(result.hasPayload()).isTrue();
+    ResultSet rs = result.getPayload().getValue().unpack(ResultSet.class);
+    assertThat(new HashSet<>(rs.getRowsList()))
+        .isEqualTo(
+            new HashSet<>(
+                Arrays.asList(
+                    cqlRow(stringValue("a"), intValue(1)),
+                    cqlRow(stringValue("b"), intValue(2)),
+                    cqlRow(stringValue("c"), intValue(3)))));
   }
 }
--- testing/src/main/java/io/stargate/it/grpc/QueryTest.java
+++ testing/src/main/java/io/stargate/it/grpc/QueryTest.java
@@ -2,45 +2,56 @@ package io.stargate.it.grpc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.google.protobuf.Any;
+import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.google.protobuf.InvalidProtocolBufferException;
-import io.stargate.proto.QueryOuterClass.Payload;
-import io.stargate.proto.QueryOuterClass.Payload.Type;
-import io.stargate.proto.QueryOuterClass.Query;
-import io.stargate.proto.QueryOuterClass.QueryParameters;
+import com.google.protobuf.StringValue;
+import io.stargate.it.driver.CqlSessionExtension;
+import io.stargate.it.driver.CqlSessionSpec;
+import io.stargate.it.driver.TestKeyspace;
 import io.stargate.proto.QueryOuterClass.Result;
 import io.stargate.proto.QueryOuterClass.ResultSet;
-import io.stargate.proto.QueryOuterClass.Value;
-import io.stargate.proto.QueryOuterClass.Value.InnerCase;
-import io.stargate.proto.QueryOuterClass.Values;
+import io.stargate.proto.StargateGrpc.StargateBlockingStub;
+import java.util.Arrays;
+import java.util.HashSet;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(CqlSessionExtension.class)
+@CqlSessionSpec(
+    initQueries = {
+      "CREATE TABLE IF NOT EXISTS test (k text, v int, PRIMARY KEY(k, v))",
+    })
 public class QueryTest extends GrpcIntegrationTest {
+
   @Test
-  public void simpleQuery() throws InvalidProtocolBufferException {
+  public void simpleQuery(@TestKeyspace CqlIdentifier keyspace)
+      throws InvalidProtocolBufferException {
+    StargateBlockingStub stub = this.stub.withCallCredentials(new StargateBearerToken(authToken));
+
+    StringValue keyspaceValue = StringValue.of(keyspace.toString());
+
     Result result =
-        stub.withCallCredentials(new StargateBearerToken(authToken))
-            .executeQuery(
-                Query.newBuilder()
-                    .setCql("SELECT release_version FROM system.local WHERE key = ?")
-                    .setParameters(
-                        QueryParameters.newBuilder()
-                            .setPayload(
-                                Payload.newBuilder()
-                                    .setType(Type.TYPE_CQL)
-                                    .setValue(
-                                        Any.pack(
-                                            Values.newBuilder()
-                                                .addValues(
-                                                    Value.newBuilder().setString("local").build())
-                                                .build()))
-                                    .build())
-                            .build())
-                    .build());
+        stub.executeQuery(
+            cqlQuery(
+                "INSERT INTO test (k, v) VALUES ('a', 1)",
+                cqlQueryParameters().setKeyspace(keyspaceValue)));
+    assertThat(result).isNotNull();
+    result =
+        stub.executeQuery(
+            cqlQuery(
+                "INSERT INTO test (k, v) VALUES (?, ?)",
+                cqlQueryParameters(stringValue("b"), intValue(2)).setKeyspace(keyspaceValue)));
+    assertThat(result).isNotNull();
+
+    result =
+        stub.executeQuery(
+            cqlQuery("SELECT * FROM test", cqlQueryParameters().setKeyspace(keyspaceValue)));
     assertThat(result.hasPayload()).isTrue();
     ResultSet rs = result.getPayload().getValue().unpack(ResultSet.class);
-    assertThat(rs.getRowsCount()).isEqualTo(1);
-    assertThat(rs.getRows(0).getValuesCount()).isEqualTo(1);
-    assertThat(rs.getRows(0).getValues(0).getInnerCase()).isEqualTo(InnerCase.STRING);
+    assertThat(new HashSet<>(rs.getRowsList()))
+        .isEqualTo(
+            new HashSet<>(
+                Arrays.asList(
+                    cqlRow(stringValue("a"), intValue(1)), cqlRow(stringValue("b"), intValue(2)))));
   }
 }
