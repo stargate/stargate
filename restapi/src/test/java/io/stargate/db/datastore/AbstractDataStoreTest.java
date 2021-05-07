647da2e01b1e7df0dd1c9ddf2f56cbc3fbc795d4
Dmitri Bourlatchkov <dmitri.bourlatchkov@datastax.com>
2021-05-07 12:07:39 -0400

    Add QueryExecutor for use by Document API

---
+++ restapi/src/test/java/io/stargate/db/datastore/AbstractDataStoreTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright The Stargate Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.stargate.db.datastore;
+
+import io.stargate.db.datastore.ValidatingDataStore.QueryExpectation;
+import io.stargate.db.schema.Schema;
+import io.stargate.db.schema.Table;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public abstract class AbstractDataStoreTest {
+
+  private ValidatingDataStore dataStore;
+
+  protected abstract Schema schema();
+
+  protected DataStore datastore() {
+    return dataStore;
+  }
+
+  @BeforeEach
+  public void createDataStore() {
+    dataStore = new ValidatingDataStore(schema());
+    dataStore.reset();
+  }
+
+  @AfterEach
+  public void checkExpectedExecutions() {
+    dataStore.validate();
+  }
+
+  protected void ignorePreparedExecutions() {
+    dataStore.ignorePrepared();
+  }
+
+  protected QueryExpectation withQuery(Table table, String cql, Object... params) {
+    return dataStore.withQuery(table, cql, params);
+  }
+
+  protected QueryExpectation withAnySelectFrom(Table table) {
+    return dataStore.withAnySelectFrom(table);
+  }
+
+  protected QueryExpectation withAnyUpdateOf(Table table) {
+    return dataStore.withAnyUpdateOf(table);
+  }
+
+  protected QueryExpectation withAnyInsertInfo(Table table) {
+    return dataStore.withAnyInsertInfo(table);
+  }
+}
