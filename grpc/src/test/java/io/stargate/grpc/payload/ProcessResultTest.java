7d052629c36081255b59f1ee22b0720591e02196
Michael Penick <michael.penick@datastax.com>
2021-06-03 09:17:57 -0400

    Fix typo in test class name

--- grpc/src/test/java/io/stargate/grpc/payload/ProcessResultTest.java
+++ grpc/src/test/java/io/stargate/grpc/payload/ProcessResultTest.java
@@ -42,7 +42,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class ProcessResultTests {
+public class ProcessResultTest {
   @ParameterizedTest
   @MethodSource("results")
   public void processResult(
