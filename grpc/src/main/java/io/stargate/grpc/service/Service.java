5530151877305e44e07280dd304311b5043ecbde
Michael Penick <mpenick@users.noreply.github.com>
2021-06-04 11:11:54 -0400

    gRPC errors (#978)
    Handle errors from the persistence layer.

--- grpc/src/main/java/io/stargate/grpc/service/Service.java
+++ grpc/src/main/java/io/stargate/grpc/service/Service.java
@@ -18,9 +18,12 @@ package io.stargate.grpc.service;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import io.grpc.Context;
+import io.grpc.Metadata;
+import io.grpc.Metadata.Key;
 import io.grpc.Status;
 import io.grpc.StatusException;
 import io.grpc.StatusRuntimeException;
+import io.grpc.protobuf.ProtoUtils;
 import io.grpc.stub.StreamObserver;
 import io.stargate.auth.AuthenticationSubject;
 import io.stargate.core.metrics.api.Metrics;
@@ -38,13 +41,21 @@ import io.stargate.db.Result.Rows;
 import io.stargate.db.Statement;
 import io.stargate.grpc.payload.PayloadHandler;
 import io.stargate.grpc.payload.PayloadHandlers;
+import io.stargate.proto.QueryOuterClass.AlreadyExists;
 import io.stargate.proto.QueryOuterClass.Batch;
 import io.stargate.proto.QueryOuterClass.BatchParameters;
 import io.stargate.proto.QueryOuterClass.BatchQuery;
+import io.stargate.proto.QueryOuterClass.CasWriteUnknown;
+import io.stargate.proto.QueryOuterClass.FunctionFailure;
 import io.stargate.proto.QueryOuterClass.Payload;
 import io.stargate.proto.QueryOuterClass.Query;
 import io.stargate.proto.QueryOuterClass.QueryParameters;
+import io.stargate.proto.QueryOuterClass.ReadFailure;
+import io.stargate.proto.QueryOuterClass.ReadTimeout;
 import io.stargate.proto.QueryOuterClass.Response;
+import io.stargate.proto.QueryOuterClass.Unavailable;
+import io.stargate.proto.QueryOuterClass.WriteFailure;
+import io.stargate.proto.QueryOuterClass.WriteTimeout;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
@@ -55,6 +66,15 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.annotation.Nullable;
 import org.apache.cassandra.stargate.db.ConsistencyLevel;
+import org.apache.cassandra.stargate.exceptions.AlreadyExistsException;
+import org.apache.cassandra.stargate.exceptions.CasWriteUnknownResultException;
+import org.apache.cassandra.stargate.exceptions.FunctionExecutionException;
+import org.apache.cassandra.stargate.exceptions.PersistenceException;
+import org.apache.cassandra.stargate.exceptions.ReadFailureException;
+import org.apache.cassandra.stargate.exceptions.ReadTimeoutException;
+import org.apache.cassandra.stargate.exceptions.UnavailableException;
+import org.apache.cassandra.stargate.exceptions.WriteFailureException;
+import org.apache.cassandra.stargate.exceptions.WriteTimeoutException;
 import org.immutables.value.Value;
 
 public class Service extends io.stargate.proto.StargateGrpc.StargateImplBase {
@@ -62,6 +82,23 @@ public class Service extends io.stargate.proto.StargateGrpc.StargateImplBase {
   public static final Context.Key<AuthenticationSubject> AUTHENTICATION_KEY =
       Context.key("authentication");
   public static final Context.Key<SocketAddress> REMOTE_ADDRESS_KEY = Context.key("remoteAddress");
+
+  public static Key<Unavailable> UNAVAILABLE_KEY =
+      ProtoUtils.keyForProto(Unavailable.getDefaultInstance());
+  public static Key<WriteTimeout> WRITE_TIMEOUT_KEY =
+      ProtoUtils.keyForProto(WriteTimeout.getDefaultInstance());
+  public static Key<ReadTimeout> READ_TIMEOUT_KEY =
+      ProtoUtils.keyForProto(ReadTimeout.getDefaultInstance());
+  public static Key<ReadFailure> READ_FAILURE_KEY =
+      ProtoUtils.keyForProto(ReadFailure.getDefaultInstance());
+  public static Key<FunctionFailure> FUNCTION_FAILURE_KEY =
+      ProtoUtils.keyForProto(FunctionFailure.getDefaultInstance());
+  public static Key<WriteFailure> WRITE_FAILURE_KEY =
+      ProtoUtils.keyForProto(WriteFailure.getDefaultInstance());
+  public static Key<AlreadyExists> ALREADY_EXISTS_KEY =
+      ProtoUtils.keyForProto(AlreadyExists.getDefaultInstance());
+  public static Key<CasWriteUnknown> CAS_WRITE_UNKNOWN_KEY =
+      ProtoUtils.keyForProto(CasWriteUnknown.getDefaultInstance());
 
   private static final InetSocketAddress DUMMY_ADDRESS = new InetSocketAddress(9042);
 
@@ -117,13 +154,13 @@ public class Service extends io.stargate.proto.StargateGrpc.StargateImplBase {
           .whenComplete(
               (prepared, t) -> {
                 if (t != null) {
-                  handleError(t, responseObserver);
+                  handleException(t, responseObserver);
                 } else {
                   executePrepared(connection, prepared, query, responseObserver);
                 }
               });
-    } catch (Exception e) {
-      handleError(e, responseObserver);
+    } catch (Throwable t) {
+      handleException(t, responseObserver);
     }
   }
 
@@ -147,20 +184,22 @@ public class Service extends io.stargate.proto.StargateGrpc.StargateImplBase {
           .whenComplete(
               (preparedBatch, t) -> {
                 if (t != null) {
-                  handleError(t, responseObserver);
+                  handleException(t, responseObserver);
                 } else {
                   executeBatch(connection, preparedBatch, batch.getParameters(), responseObserver);
                 }
               });
 
-    } catch (Exception e) {
-      handleError(e, responseObserver);
+    } catch (Throwable t) {
+      handleException(t, responseObserver);
     }
   }
 
-  private void handleError(Throwable throwable, StreamObserver<?> responseObserver) {
+  private void handleException(Throwable throwable, StreamObserver<?> responseObserver) {
     if (throwable instanceof StatusException || throwable instanceof StatusRuntimeException) {
       responseObserver.onError(throwable);
+    } else if (throwable instanceof PersistenceException) {
+      handlePersistenceException((PersistenceException) throwable, responseObserver);
     } else {
       responseObserver.onError(
           Status.UNKNOWN
@@ -168,6 +207,174 @@ public class Service extends io.stargate.proto.StargateGrpc.StargateImplBase {
               .withCause(throwable)
               .asRuntimeException());
     }
+  }
+
+  private void handlePersistenceException(
+      PersistenceException pe, StreamObserver<?> responseObserver) {
+    switch (pe.code()) {
+      case SERVER_ERROR:
+      case PROTOCOL_ERROR: // Fallthrough
+      case UNPREPARED: // Fallthrough
+        onError(responseObserver, Status.INTERNAL, pe);
+        break;
+      case INVALID:
+      case SYNTAX_ERROR: // Fallthrough
+        onError(responseObserver, Status.INVALID_ARGUMENT, pe);
+        break;
+      case TRUNCATE_ERROR:
+      case CDC_WRITE_FAILURE: // Fallthrough
+        onError(responseObserver, Status.ABORTED, pe);
+        break;
+      case BAD_CREDENTIALS:
+        onError(responseObserver, Status.UNAUTHENTICATED, pe);
+        break;
+      case UNAVAILABLE:
+        UnavailableException ue = (UnavailableException) pe;
+        onError(
+            responseObserver,
+            Status.UNAVAILABLE,
+            ue,
+            makeTrailer(
+                UNAVAILABLE_KEY,
+                Unavailable.newBuilder()
+                    .setConsistencyValue(ue.consistency.code)
+                    .setAlive(ue.alive)
+                    .setRequired(ue.required)
+                    .build()));
+        break;
+      case OVERLOADED:
+        onError(responseObserver, Status.RESOURCE_EXHAUSTED, pe);
+        break;
+      case IS_BOOTSTRAPPING:
+        onError(responseObserver, Status.UNAVAILABLE, pe);
+        break;
+      case WRITE_TIMEOUT:
+        WriteTimeoutException wte = (WriteTimeoutException) pe;
+        onError(
+            responseObserver,
+            Status.DEADLINE_EXCEEDED,
+            pe,
+            makeTrailer(
+                WRITE_TIMEOUT_KEY,
+                WriteTimeout.newBuilder()
+                    .setConsistencyValue(wte.consistency.code)
+                    .setBlockFor(wte.blockFor)
+                    .setReceived(wte.received)
+                    .setWriteType(wte.writeType.name())
+                    .build()));
+        break;
+      case READ_TIMEOUT:
+        ReadTimeoutException rte = (ReadTimeoutException) pe;
+        onError(
+            responseObserver,
+            Status.DEADLINE_EXCEEDED,
+            pe,
+            makeTrailer(
+                READ_TIMEOUT_KEY,
+                ReadTimeout.newBuilder()
+                    .setConsistencyValue(rte.consistency.code)
+                    .setBlockFor(rte.blockFor)
+                    .setReceived(rte.received)
+                    .setDataPresent(rte.dataPresent)
+                    .build()));
+        break;
+      case READ_FAILURE:
+        ReadFailureException rfe = (ReadFailureException) pe;
+        onError(
+            responseObserver,
+            Status.ABORTED,
+            pe,
+            makeTrailer(
+                READ_FAILURE_KEY,
+                ReadFailure.newBuilder()
+                    .setConsistencyValue(rfe.consistency.code)
+                    .setNumFailures(rfe.failureReasonByEndpoint.size())
+                    .setBlockFor(rfe.blockFor)
+                    .setReceived(rfe.received)
+                    .setDataPresent(rfe.dataPresent)
+                    .build()));
+        break;
+      case FUNCTION_FAILURE:
+        FunctionExecutionException fee = (FunctionExecutionException) pe;
+        onError(
+            responseObserver,
+            Status.FAILED_PRECONDITION,
+            pe,
+            makeTrailer(
+                FUNCTION_FAILURE_KEY,
+                FunctionFailure.newBuilder()
+                    .setKeyspace(fee.functionName.keyspace)
+                    .setFunction(fee.functionName.name)
+                    .addAllArgTypes(fee.argTypes)
+                    .build()));
+        break;
+      case WRITE_FAILURE:
+        WriteFailureException wfe = (WriteFailureException) pe;
+        onError(
+            responseObserver,
+            Status.ABORTED,
+            pe,
+            makeTrailer(
+                WRITE_FAILURE_KEY,
+                WriteFailure.newBuilder()
+                    .setConsistencyValue(wfe.consistency.code)
+                    .setNumFailures(wfe.failureReasonByEndpoint.size())
+                    .setBlockFor(wfe.blockFor)
+                    .setReceived(wfe.received)
+                    .setWriteType(wfe.writeType.name())
+                    .build()));
+        break;
+      case CAS_WRITE_UNKNOWN:
+        CasWriteUnknownResultException cwe = (CasWriteUnknownResultException) pe;
+        onError(
+            responseObserver,
+            Status.ABORTED,
+            pe,
+            makeTrailer(
+                CAS_WRITE_UNKNOWN_KEY,
+                CasWriteUnknown.newBuilder()
+                    .setConsistencyValue(cwe.consistency.code)
+                    .setBlockFor(cwe.blockFor)
+                    .setReceived(cwe.received)
+                    .build()));
+        break;
+      case UNAUTHORIZED:
+        onError(responseObserver, Status.PERMISSION_DENIED, pe);
+        break;
+      case CONFIG_ERROR:
+        onError(responseObserver, Status.FAILED_PRECONDITION, pe);
+        break;
+      case ALREADY_EXISTS:
+        AlreadyExistsException aee = (AlreadyExistsException) pe;
+        onError(
+            responseObserver,
+            Status.ALREADY_EXISTS,
+            pe,
+            makeTrailer(
+                ALREADY_EXISTS_KEY,
+                AlreadyExists.newBuilder().setKeyspace(aee.ksName).setTable(aee.cfName).build()));
+        break;
+      default:
+        onError(responseObserver, Status.UNKNOWN, pe);
+        break;
+    }
+  }
+
+  private void onError(
+      StreamObserver<?> responseObserver, Status status, Throwable throwable, Metadata trailer) {
+    status = status.withDescription(throwable.getMessage()).withCause(throwable);
+    responseObserver.onError(
+        trailer != null ? status.asRuntimeException(trailer) : status.asRuntimeException());
+  }
+
+  public void onError(StreamObserver<?> responseObserver, Status status, Throwable throwable) {
+    onError(responseObserver, status, throwable, null);
+  }
+
+  private <T> Metadata makeTrailer(Key<T> key, T value) {
+    Metadata trailer = new Metadata();
+    trailer.put(key, value);
+    return trailer;
   }
 
   private CompletableFuture<Prepared> prepareQuery(
@@ -219,7 +426,7 @@ public class Service extends io.stargate.proto.StargateGrpc.StargateImplBase {
           .whenComplete(
               (result, t) -> {
                 if (t != null) {
-                  handleError(t, responseObserver);
+                  handleException(t, responseObserver);
                 } else {
                   try {
                     Response.Builder responseBuilder = makeResponseBuilder(result);
@@ -245,13 +452,13 @@ public class Service extends io.stargate.proto.StargateGrpc.StargateImplBase {
                     }
                     responseObserver.onNext(responseBuilder.build());
                     responseObserver.onCompleted();
-                  } catch (Exception e) {
-                    handleError(e, responseObserver);
+                  } catch (Throwable th) {
+                    handleException(th, responseObserver);
                   }
                 }
               });
-    } catch (Exception e) {
-      handleError(e, responseObserver);
+    } catch (Throwable t) {
+      handleException(t, responseObserver);
     }
   }
 
@@ -268,7 +475,7 @@ public class Service extends io.stargate.proto.StargateGrpc.StargateImplBase {
           .whenComplete(
               (result, t) -> {
                 if (t != null) {
-                  handleError(t, responseObserver);
+                  handleException(t, responseObserver);
                 } else {
                   try {
                     Response.Builder responseBuilder = makeResponseBuilder(result);
@@ -277,13 +484,13 @@ public class Service extends io.stargate.proto.StargateGrpc.StargateImplBase {
                     }
                     responseObserver.onNext(responseBuilder.build());
                     responseObserver.onCompleted();
-                  } catch (Exception e) {
-                    handleError(e, responseObserver);
+                  } catch (Throwable th) {
+                    handleException(th, responseObserver);
                   }
                 }
               });
-    } catch (Exception e) {
-      handleError(e, responseObserver);
+    } catch (Throwable t) {
+      handleException(t, responseObserver);
     }
   }
 
@@ -450,8 +657,8 @@ public class Service extends io.stargate.proto.StargateGrpc.StargateImplBase {
                     PayloadHandler handler = PayloadHandlers.get(query.getValues().getType());
                     statements.add(bindValues(handler, prepared, query.getValues()));
                     next(); // Prepare the next query in the batch
-                  } catch (Exception e) {
-                    future.completeExceptionally(e);
+                  } catch (Throwable th) {
+                    future.completeExceptionally(th);
                   }
                 }
               });
