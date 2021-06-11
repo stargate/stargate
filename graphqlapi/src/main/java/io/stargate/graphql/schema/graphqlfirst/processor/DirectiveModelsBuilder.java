4f9fe83c9ea85b7fc232a6f7658ef8c205208396
tomekl007 <tomasz.lelek@datastax.com>
2021-06-11 11:31:49 +0200

    remove white-space

--- graphqlapi/src/main/java/io/stargate/graphql/schema/graphqlfirst/processor/DirectiveModelsBuilder.java
+++ graphqlapi/src/main/java/io/stargate/graphql/schema/graphqlfirst/processor/DirectiveModelsBuilder.java
@@ -93,7 +93,6 @@ class DirectiveModelsBuilder extends ModelBuilderBase<DirectiveModels> {
       FieldModel field = maybeField.get();
 
       boolean isWhereCondition;
-
       switch (operationType) {
         case SELECT:
           if (hasIfDirective) {
