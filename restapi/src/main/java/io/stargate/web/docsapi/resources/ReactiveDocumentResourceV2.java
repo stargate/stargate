dd1766eea6f71d061ae362801956070ba4a0f792
Dmitri Bourlatchkov <dmitri.bourlatchkov@datastax.com>
2021-07-27 12:29:16 -0400

    Add $and, $or to Swagger descriptions

--- restapi/src/main/java/io/stargate/web/docsapi/resources/ReactiveDocumentResourceV2.java
+++ restapi/src/main/java/io/stargate/web/docsapi/resources/ReactiveDocumentResourceV2.java
@@ -55,7 +55,8 @@ public class ReactiveDocumentResourceV2 {
 
   private static final String WHERE_DESCRIPTION =
       "a JSON blob with search filters;"
-          + " allowed operators: $eq, $ne, $in, $nin, $gt, $lt, $gte, $lte, $exists;"
+          + " allowed predicates: $eq, $ne, $in, $nin, $gt, $lt, $gte, $lte, $exists;"
+          + " allowed boolean operators: $and, $or;"
           + " allowed hints: $selectivity (a number between 0.0 and 1.0, less is better)";
 
   @Inject private Db dbFactory;
