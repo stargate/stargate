f47afc0e641707718f81458e54d06456f2164371
tomekl007 <tomasz.lelek@datastax.com>
2021-09-06 09:48:49 +0200

    fix

--- grpc/src/test/java/io/stargate/grpc/service/RetryTest.java
+++ grpc/src/test/java/io/stargate/grpc/service/RetryTest.java
@@ -55,7 +55,7 @@ import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.mockito.stubbing.Answer;
 
-public class RetryTest extends BaseServiceTest {
+public class RetryTest extends BaseGrpcServiceTest {
 
   @Test
   public void shouldNotRetryOnReadTimeoutWhenDataPresent() {
