4d7f0f5d7b45b69c1b440d26dc4c3f9f7050d8b6
Olivier Michallat <olim7t@users.noreply.github.com>
2021-12-03 10:38:41 -0800

    Simplify ReservedKeywords

--- sgv2-service-common/src/main/java/io/stargate/sgv2/common/cql/ReservedKeywords.java
+++ sgv2-service-common/src/main/java/io/stargate/sgv2/common/cql/ReservedKeywords.java
@@ -16,86 +16,86 @@
 package io.stargate.sgv2.common.cql;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 public final class ReservedKeywords {
-  private static final String[] reservedKeywords =
-      new String[] {
-        "SELECT",
-        "FROM",
-        "WHERE",
-        "AND",
-        "ENTRIES",
-        "FULL",
-        "INSERT",
-        "UPDATE",
-        "WITH",
-        "LIMIT",
-        "USING",
-        "USE",
-        "SET",
-        "BEGIN",
-        "UNLOGGED",
-        "BATCH",
-        "APPLY",
-        "TRUNCATE",
-        "DELETE",
-        "IN",
-        "CREATE",
-        "KEYSPACE",
-        "SCHEMA",
-        "COLUMNFAMILY",
-        "TABLE",
-        "MATERIALIZED",
-        "VIEW",
-        "INDEX",
-        "ON",
-        "TO",
-        "DROP",
-        "PRIMARY",
-        "INTO",
-        "ALTER",
-        "RENAME",
-        "ADD",
-        "ORDER",
-        "BY",
-        "ASC",
-        "DESC",
-        "ALLOW",
-        "IF",
-        "IS",
-        "GRANT",
-        "OF",
-        "REVOKE",
-        "MODIFY",
-        "AUTHORIZE",
-        "DESCRIBE",
-        "EXECUTE",
-        "NORECURSIVE",
-        "TOKEN",
-        "NULL",
-        "NOT",
-        "NAN",
-        "INFINITY",
-        "OR",
-        "REPLACE",
-        "DEFAULT",
-        "UNSET",
-        "MBEAN",
-        "MBEANS",
-        "FOR",
-        "RESTRICT",
-        "UNRESTRICT"
-      };
 
-  private static final Set<String> reservedSet =
-      new CopyOnWriteArraySet<>(Arrays.asList(reservedKeywords));
+  private static final Set<String> KEYWORDS =
+      Collections.unmodifiableSet(
+          new HashSet<>(
+              Arrays.asList(
+                  "SELECT",
+                  "FROM",
+                  "WHERE",
+                  "AND",
+                  "ENTRIES",
+                  "FULL",
+                  "INSERT",
+                  "UPDATE",
+                  "WITH",
+                  "LIMIT",
+                  "USING",
+                  "USE",
+                  "SET",
+                  "BEGIN",
+                  "UNLOGGED",
+                  "BATCH",
+                  "APPLY",
+                  "TRUNCATE",
+                  "DELETE",
+                  "IN",
+                  "CREATE",
+                  "KEYSPACE",
+                  "SCHEMA",
+                  "COLUMNFAMILY",
+                  "TABLE",
+                  "MATERIALIZED",
+                  "VIEW",
+                  "INDEX",
+                  "ON",
+                  "TO",
+                  "DROP",
+                  "PRIMARY",
+                  "INTO",
+                  "ALTER",
+                  "RENAME",
+                  "ADD",
+                  "ORDER",
+                  "BY",
+                  "ASC",
+                  "DESC",
+                  "ALLOW",
+                  "IF",
+                  "IS",
+                  "GRANT",
+                  "OF",
+                  "REVOKE",
+                  "MODIFY",
+                  "AUTHORIZE",
+                  "DESCRIBE",
+                  "EXECUTE",
+                  "NORECURSIVE",
+                  "TOKEN",
+                  "NULL",
+                  "NOT",
+                  "NAN",
+                  "INFINITY",
+                  "OR",
+                  "REPLACE",
+                  "DEFAULT",
+                  "UNSET",
+                  "MBEAN",
+                  "MBEANS",
+                  "FOR",
+                  "RESTRICT",
+                  "UNRESTRICT")));
+
+  public static boolean isReserved(String text) {
+    return KEYWORDS.contains(text.toUpperCase());
+  }
 
   /** This class must not be instantiated as it only contains static methods. */
   private ReservedKeywords() {}
-
-  public static boolean isReserved(String text) {
-    return reservedSet.contains(text.toUpperCase());
-  }
 }
