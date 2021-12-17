9d0bf0af79bab9e2cfdb576804cf72c7e9812d06
tomekl007 <tomasz.lelek@datastax.com>
2021-12-17 12:35:00 +0100

    add more logging

--- grpc/src/main/java/io/grpc/netty/shaded/io/grpc/netty/CustomServerTransportListener.java
+++ grpc/src/main/java/io/grpc/netty/shaded/io/grpc/netty/CustomServerTransportListener.java
@@ -5,6 +5,7 @@ import io.grpc.Metadata;
 import io.grpc.Status;
 import io.grpc.internal.ServerStream;
 import io.grpc.internal.ServerTransportListener;
+import io.grpc.netty.shaded.io.netty.handler.codec.http2.Http2Stream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -80,6 +81,20 @@ public class CustomServerTransportListener implements ServerTransportListener {
                   e.getValue().size(),
                   e.getValue());
 
+              long numberOfCloseStreams =
+                  e.getValue().stream()
+                      .filter(v -> v instanceof NettyServerStream)
+                      .map(v -> (NettyServerStream) v)
+                      .filter(
+                          v -> {
+                            Http2Stream.State state = v.transportState().getHttp2Stream().state();
+                            return state.equals(Http2Stream.State.CLOSED)
+                                || state.equals(Http2Stream.State.HALF_CLOSED_LOCAL)
+                                || state.equals(Http2Stream.State.HALF_CLOSED_REMOTE);
+                          })
+                      .count();
+              System.out.println("Number of closed streams BEFORE: " + numberOfCloseStreams);
+
               e.getValue().stream()
                   .filter(v -> v instanceof NettyServerStream)
                   .map(v -> (NettyServerStream) v)
@@ -87,14 +102,33 @@ public class CustomServerTransportListener implements ServerTransportListener {
                       stream -> {
                         NettyServerStream.TransportState transportState = stream.transportState();
                         logger.info("transportState: " + transportState);
-                        logger.info("Closing Stream: " + stream);
-                        stream.close(
-                            Status.UNAVAILABLE.withDescription("Stream closed"), new Metadata());
-                        // todo
-                        // if(!transportState.getState().equals(CLOSED)){
+                        Http2Stream.State state = transportState.getHttp2Stream().state();
+                        logger.info("Closing Stream: " + stream + " for state: " + state);
 
-                        //                         }
+                        // todo
+                        if (!state.equals(Http2Stream.State.CLOSED)
+                            && !state.equals(Http2Stream.State.HALF_CLOSED_LOCAL)
+                            && !state.equals(Http2Stream.State.HALF_CLOSED_REMOTE)) {
+                          System.out.println(
+                              "Closing stream: " + stream + "  because it is not already closed");
+                          stream.close(
+                              Status.UNAVAILABLE.withDescription("Stream closed"), new Metadata());
+                        }
                       });
+
+              numberOfCloseStreams =
+                  e.getValue().stream()
+                      .filter(v -> v instanceof NettyServerStream)
+                      .map(v -> (NettyServerStream) v)
+                      .filter(
+                          v -> {
+                            Http2Stream.State state = v.transportState().getHttp2Stream().state();
+                            return state.equals(Http2Stream.State.CLOSED)
+                                || state.equals(Http2Stream.State.HALF_CLOSED_LOCAL)
+                                || state.equals(Http2Stream.State.HALF_CLOSED_REMOTE);
+                          })
+                      .count();
+              System.out.println("Number of closed streams AFTER: " + numberOfCloseStreams);
             });
   }
 }
