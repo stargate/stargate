7019581e4f0f54e4c4de09d1f90ebd0e493da0ce
Cai <xiyanc2@fa21-cs527-046.cs.illinois.edu>
2021-12-22 18:44:24 -0600

    typo

--- persistence-api/src/test/java/io/stargate/db/schema/ColumnTest.java
+++ persistence-api/src/test/java/io/stargate/db/schema/ColumnTest.java
@@ -260,7 +260,7 @@ public class ColumnTest {
     String mapString = Type.Map.of(Type.Int, Type.Int).toString(ImmutableMap.of(1, 1, 2, 2));
     assertThat(Type.List.of(Type.Int).toString(list)).isEqualTo("[1,2,3]");
     assertThat(Type.Set.of(Type.Int).toString(set)).isEqualTo("{1,2,3}");
-    assertThat(Type.Map.of(Type.Int, Type.Int).toString(m).isEqualTo("{1:1, 2:2}"));
+    assertThat(Type.Map.of(Type.Int, Type.Int).toString(m)).isEqualTo("{1:1, 2:2}");
   }
 
   @Test
