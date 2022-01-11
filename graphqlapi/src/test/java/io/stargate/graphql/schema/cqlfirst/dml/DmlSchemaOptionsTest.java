1d560b8bfcd89170eb10a2d819bd5630b8691262
Toby Cole <tobyc@yelp.com>
2022-01-11 15:53:45 +0000

    Allows runtime override of query and mutation consistency levels.

---
+++ graphqlapi/src/test/java/io/stargate/graphql/schema/cqlfirst/dml/DmlSchemaOptionsTest.java
@@ -0,0 +1,45 @@
+package io.stargate.graphql.schema.cqlfirst.dml;
+
+import graphql.schema.GraphQLEnumValueDefinition;
+import java.util.List;
+import java.util.stream.Stream;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class DmlSchemaOptionsTest {
+  @ParameterizedTest
+  @MethodSource
+  public void testConsistencyLevelParsing(String csvLevels, List<String> expectedValues) {
+    List<GraphQLEnumValueDefinition> values =
+        DmlSchemaBuilder.getConsistencyEnum(csvLevels, "Name").getValues();
+    // Are all values expected?
+    for (GraphQLEnumValueDefinition v : values) {
+      Assertions.assertTrue(expectedValues.contains(v.getName()));
+    }
+    // Are ALL expected values there, no more no less.
+    Assertions.assertEquals(values.size(), expectedValues.size());
+  }
+
+  static Stream<Arguments> testConsistencyLevelParsing() {
+    return Stream.of(
+        Arguments.of("LOCAL_QUORUM", Lists.newArrayList("LOCAL_QUORUM")),
+        Arguments.of("LOCAL_QUORUM,EACH_QUORUM", Lists.newArrayList("LOCAL_QUORUM", "EACH_QUORUM")),
+        Arguments.of(
+            "LOCAL_QUORUM,EACH_QUORUM,ALL",
+            Lists.newArrayList("LOCAL_QUORUM", "EACH_QUORUM", "ALL")),
+        Arguments.of(
+            "LOCAL_QUORUM,QUORUM,EACH_QUORUM,ALL",
+            Lists.newArrayList("LOCAL_QUORUM", "QUORUM", "EACH_QUORUM", "ALL")),
+        // Local quorum will always be added as it's the default
+        Arguments.of("EACH_QUORUM", Lists.newArrayList("LOCAL_QUORUM", "EACH_QUORUM")),
+        Arguments.of(
+            "monkey,banana,fish,QUORUM,brains", Lists.newArrayList("LOCAL_QUORUM", "QUORUM")),
+        // Invalid quorum levels should be skipped
+        Arguments.of("", Lists.newArrayList("LOCAL_QUORUM")),
+        // Case insensitivity
+        Arguments.of("each_quorum", Lists.newArrayList("LOCAL_QUORUM", "EACH_QUORUM")));
+  }
+}
