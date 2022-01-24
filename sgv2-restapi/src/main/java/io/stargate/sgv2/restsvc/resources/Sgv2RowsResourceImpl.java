c1af76026b9d4b198a3c00beb48f29bde7951024
Tatu Saloranta <tatu.saloranta@datastax.com>
2022-01-24 13:47:11 -0800

    ...

--- sgv2-restapi/src/main/java/io/stargate/sgv2/restsvc/resources/Sgv2RowsResourceImpl.java
+++ sgv2-restapi/src/main/java/io/stargate/sgv2/restsvc/resources/Sgv2RowsResourceImpl.java
@@ -34,6 +34,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.PathSegment;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // note: JAX-RS Class Annotations MUST be in the impl class; only method annotations inherited
 // (but Swagger allows inheritance)
@@ -42,6 +44,9 @@ import javax.ws.rs.core.Response.Status;
 @Singleton
 @CreateGrpcStub
 public class Sgv2RowsResourceImpl extends ResourceBase implements Sgv2RowsResourceApi {
+  // Singleton resource so no need to be static
+  protected final Logger logger = LoggerFactory.getLogger(getClass());
+
   @Inject protected CachedSchemaAccessor schemaAccess;
 
   /*
