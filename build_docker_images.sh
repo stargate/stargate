398cbd2d8b9644b3ecaba5bf9cb78c1c8240e744
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2022-01-27 12:27:36 -0700

    adding options to specify tag, publish

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -5,8 +5,40 @@ set -euo pipefail
 # Assumes that you have done a complete build so that all jars have been created, i.e.:
 #   ./mvnw clean install -P dse -DskipTests=true
 
+#
+# Defaults
+#
+
+# generate for local platform, don't push
+DOCKER_FLAGS=
+
 # extract Stargate version from project pom file
-export SGTAG=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+SGTAG="v$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)"
+
+#
+# overrides via command line
+#
+
+while getopts ":pt:" opt; do
+  case $opt in
+    p)
+      DOCKER_FLAGS="--platform linux/amd64,linux/arm64 --push"
+      ;;
+    t)
+      SGTAG=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo "Building version $SGTAG"
+
+#
+# Persistence images
+#
 
 # Create a temp directory under current directory to use as a staging area for image creation
 # This is a workaround since Docker COPY command provides no way to exclude files.
@@ -17,23 +49,15 @@ mkdir ${LIBDIR}
 cp ./stargate-lib/* $LIBDIR
 rm ${LIBDIR}/persistence*.jar
 
-#ls ${LIBDIR}
-
-docker build --target coordinator-4_0 --build-arg LIBDIR="$LIBDIR" -t stargateio/coordinator-4_0:$SGTAG .
-docker build --target coordinator-3_11 --build-arg LIBDIR="$LIBDIR" -t stargateio/coordinator-3_11:$SGTAG .
-docker build --target coordinator-dse-68 --build-arg LIBDIR="$LIBDIR" -t stargateio/coordinator-dse-68:$SGTAG .
+docker buildx build --target coordinator-4_0 --build-arg LIBDIR="$LIBDIR" -t stargateio/coordinator-4_0:$SGTAG $DOCKER_FLAGS .
+docker buildx build --target coordinator-3_11 --build-arg LIBDIR="$LIBDIR" -t stargateio/coordinator-3_11:$SGTAG $DOCKER_FLAGS .
+docker buildx build --target coordinator-dse-68 --build-arg LIBDIR="$LIBDIR" -t stargateio/coordinator-dse-68:$SGTAG $DOCKER_FLAGS .
 
 rm -rf ${LIBDIR}
 
-docker build --target restapi -t stargateio/restapi:$SGTAG .
+#
+# API Service images
+#
 
+docker buildx build --target restapi -t stargateio/restapi:$SGTAG $DOCKER_FLAGS .
 
-
-echo "Building $DOCKER_IMAGE"
-docker buildx build --push \
---tag ${DOCKER_IMAGE}:${stargate_version} \
---file Dockerfile \
---platform linux/amd64,linux/arm64 .
-
-echo "Inspecting $DOCKER_IMAGE"
-docker buildx imagetools inspect ${DOCKER_IMAGE}:${stargate_version}
