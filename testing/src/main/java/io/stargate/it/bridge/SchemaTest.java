ae80c2a8c583fc36eea5dcccf85ff8c45e5fdf66
Olivier Michallat <olim7t@users.noreply.github.com>
2022-03-16 14:45:19 -0700

    gRPC: Revisit index metadata
    Include all information available from the persistence.

--- testing/src/main/java/io/stargate/it/bridge/SchemaTest.java
+++ testing/src/main/java/io/stargate/it/bridge/SchemaTest.java
@@ -24,6 +24,7 @@ import io.stargate.it.driver.TestKeyspace;
 import io.stargate.proto.QueryOuterClass.ColumnSpec;
 import io.stargate.proto.QueryOuterClass.TypeSpec;
 import io.stargate.proto.Schema;
+import io.stargate.proto.Schema.CqlIndex;
 import io.stargate.proto.Schema.CqlTable;
 import io.stargate.proto.StargateBridgeGrpc.StargateBridgeBlockingStub;
 import org.junit.jupiter.api.Test;
@@ -33,7 +34,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @CqlSessionSpec(
     initQueries = {
       "CREATE TYPE address(street VARCHAR, number int, phone_numbers frozen<map<text,text>>);",
-      "CREATE TABLE users_with_address(id int PRIMARY KEY, address address);"
+      "CREATE TABLE users_with_address(id int PRIMARY KEY, address address);",
+      "CREATE TABLE test_index(k int PRIMARY KEY, v int, m map<int, int>)",
+      "CREATE INDEX v_index ON test_index(v)",
+      "CREATE INDEX m_keys_index ON test_index(KEYS(m))",
     })
 public class SchemaTest extends BridgeIntegrationTest {
 
@@ -47,9 +51,9 @@ public class SchemaTest extends BridgeIntegrationTest {
                 .setKeyspaceName(keyspace.asInternal())
                 .build());
     assertThat(response).isNotNull();
-    assertThat(response.getTablesCount()).isEqualTo(1);
-    CqlTable usersTable = response.getTables(0);
-    assertThat(usersTable.getName()).isEqualTo("users_with_address");
+    assertThat(response.getTablesCount()).isEqualTo(2);
+
+    CqlTable usersTable = findTable(response, "users_with_address");
     assertThat(usersTable.getPartitionKeyColumnsCount()).isEqualTo(1);
     ColumnSpec idColumn = usersTable.getPartitionKeyColumns(0);
     assertThat(idColumn.getName()).isEqualTo("id");
@@ -72,5 +76,40 @@ public class SchemaTest extends BridgeIntegrationTest {
               assertThat(phonesType.getKey().getBasic()).isEqualTo(TypeSpec.Basic.VARCHAR);
               assertThat(phonesType.getValue().getBasic()).isEqualTo(TypeSpec.Basic.VARCHAR);
             });
+
+    CqlTable indexTable = findTable(response, "test_index");
+    assertThat(indexTable.getIndexesCount()).isEqualTo(2);
+
+    CqlIndex vIndex = findIndex(indexTable, "v_index");
+    assertThat(vIndex.getColumnName()).isEqualTo("v");
+    assertThat(vIndex.getIndexingType()).isEqualTo(Schema.IndexingType.DEFAULT);
+    assertThat(vIndex.getCustom()).isFalse();
+    assertThat(vIndex.hasIndexingClass()).isFalse();
+    assertThat(vIndex.getOptionsMap()).isEmpty();
+
+    CqlIndex mKeysIndex = findIndex(indexTable, "m_keys_index");
+    assertThat(mKeysIndex.getColumnName()).isEqualTo("m");
+    assertThat(mKeysIndex.getIndexingType()).isEqualTo(Schema.IndexingType.KEYS);
+    assertThat(mKeysIndex.getCustom()).isFalse();
+    assertThat(mKeysIndex.hasIndexingClass()).isFalse();
+    assertThat(mKeysIndex.getOptionsMap()).isEmpty();
+  }
+
+  private CqlTable findTable(Schema.CqlKeyspaceDescribe response, String tableName) {
+    for (CqlTable table : response.getTablesList()) {
+      if (tableName.equals(table.getName())) {
+        return table;
+      }
+    }
+    throw new AssertionError("Expected to find a table named " + tableName);
+  }
+
+  private CqlIndex findIndex(CqlTable table, String indexName) {
+    for (CqlIndex index : table.getIndexesList()) {
+      if (indexName.equals(index.getName())) {
+        return index;
+      }
+    }
+    throw new AssertionError("Expected to find an index named " + indexName);
   }
 }
