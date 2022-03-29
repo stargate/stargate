0c198ced3a8ade69b4811e3c819dcfe8bb782d48
Ivan Senic <ivan.senic@datastax.com>
2022-03-29 16:02:02 +0200

    initial project creation with quarkus cli

---
+++ sgv2-docsapi/src/test/java/io/stargate/sgv2/docsapi/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package io.stargate.sgv2.docsapi;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
