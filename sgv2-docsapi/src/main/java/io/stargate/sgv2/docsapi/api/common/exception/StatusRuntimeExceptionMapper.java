8a3ff4b7b97d06cf219fee513e9b2f25d766d9fe
Eric Borczuk <eric.borczuk@datastax.com>
2022-04-19 17:18:14 -0400

    Add exception mappers for grpc status runtime exception and ErrorCodes (#1779)

--- sgv2-docsapi/src/main/java/io/stargate/sgv2/docsapi/api/common/exception/StatusRuntimeExceptionMapper.java
+++ sgv2-docsapi/src/main/java/io/stargate/sgv2/docsapi/api/common/exception/StatusRuntimeExceptionMapper.java
@@ -22,22 +22,81 @@ import io.grpc.StatusRuntimeException;
 import io.stargate.sgv2.docsapi.api.common.exception.model.dto.ApiError;
 import javax.ws.rs.core.Response;
 import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 /** Simple exception mapper for the {@link StatusRuntimeException}. */
 public class StatusRuntimeExceptionMapper {
 
   @ServerExceptionMapper
-  public RestResponse<ApiError> statusRuntimeException(StatusRuntimeException exception) {
+  public RestResponse<ApiError> statusRuntimeException(StatusRuntimeException grpcE) {
     // TODO finalize mapper, messages, logs, etc.
 
-    Status status = exception.getStatus();
-    ApiError body = new ApiError(status.getDescription());
+    Status.Code sc = grpcE.getStatus().getCode();
 
-    return switch (status.getCode()) {
-      case UNAVAILABLE -> RestResponse.status(Response.Status.BAD_GATEWAY, body);
-      case UNAUTHENTICATED -> RestResponse.status(Response.Status.UNAUTHORIZED, body);
-      default -> RestResponse.status(Response.Status.INTERNAL_SERVER_ERROR, body);
-    };
+    // Handling partly based on information from:
+    // https://developers.google.com/maps-booking/reference/grpc-api/status_codes
+    switch (sc) {
+        // Start with codes we know how to handle
+      case FAILED_PRECONDITION:
+        return grpcResponse(
+            sc,
+            Response.Status.BAD_REQUEST,
+            "Invalid state or argument(s) for gRPC operation",
+            grpcE.getMessage());
+      case INVALID_ARGUMENT:
+        return grpcResponse(
+            sc,
+            Response.Status.BAD_REQUEST,
+            "Invalid argument for gRPC operation",
+            grpcE.getMessage());
+      case NOT_FOUND:
+        return grpcResponse(
+            sc, Response.Status.NOT_FOUND, "Unknown gRPC operation", grpcE.getMessage());
+      case PERMISSION_DENIED:
+        return grpcResponse(
+            sc, Response.Status.UNAUTHORIZED, "Unauthorized gRPC operation", grpcE.getMessage());
+      case UNAUTHENTICATED:
+        return grpcResponse(
+            sc, Response.Status.UNAUTHORIZED, "Unauthenticated gRPC operation", grpcE.getMessage());
+      case UNAVAILABLE:
+        // Should this be SERVICE_UNAVAILABLE (503) or BAD_GATEWAY (502)?
+        return grpcResponse(
+            sc,
+            Response.Status.SERVICE_UNAVAILABLE,
+            "gRPC service unavailable",
+            grpcE.getMessage());
+      case UNIMPLEMENTED:
+        return grpcResponse(
+            sc,
+            Response.Status.NOT_IMPLEMENTED,
+            "Unimplemented gRPC operation",
+            grpcE.getMessage());
+
+        // And then codes we ... don't know how to handle
+      case OK: // huh?
+
+      case CANCELLED:
+      case UNKNOWN:
+      case DEADLINE_EXCEEDED:
+      case ALREADY_EXISTS:
+      case RESOURCE_EXHAUSTED:
+      case ABORTED:
+      case OUT_OF_RANGE:
+      case INTERNAL:
+      case DATA_LOSS:
+        break;
+    }
+
+    return grpcResponse(
+        sc, Response.Status.INTERNAL_SERVER_ERROR, "Unhandled gRPC failure", grpcE.getMessage());
+  }
+
+  private static RestResponse<ApiError> grpcResponse(
+      Status.Code grpcCode, Response.Status httpStatus, String prefix, String suffix) {
+    final String msg = String.format("%s (%s->%s): %s", prefix, grpcCode, httpStatus, suffix);
+    return ResponseBuilder.create(
+            httpStatus, new ApiError(msg, httpStatus.getStatusCode(), grpcCode.toString()))
+        .build();
   }
 }
