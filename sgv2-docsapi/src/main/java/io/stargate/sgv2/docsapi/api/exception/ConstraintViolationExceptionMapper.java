20111dad9ced0d4ee5200a1b2e5335463867ec95
Eric Borczuk <eric.borczuk@datastax.com>
2022-04-19 13:11:34 -0400

    Review changes

--- sgv2-docsapi/src/main/java/io/stargate/sgv2/docsapi/api/exception/ConstraintViolationExceptionMapper.java
+++ sgv2-docsapi/src/main/java/io/stargate/sgv2/docsapi/api/exception/ConstraintViolationExceptionMapper.java
@@ -1,18 +1,18 @@
 package io.stargate.sgv2.docsapi.api.exception;
 
-import io.quarkus.hibernate.validator.runtime.jaxrs.ResteasyReactiveViolationException;
 import io.stargate.sgv2.docsapi.api.common.exception.model.dto.ApiError;
+import javax.validation.ConstraintViolationException;
 import javax.ws.rs.core.Response;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
-/** Simple exception mapper for the {@link ResteasyReactiveViolationException}. */
-public class ResteasyReactiveViolationExceptionMapper {
+/** Simple exception mapper for the {@link ConstraintViolationException}. */
+public class ConstraintViolationExceptionMapper {
 
   @ServerExceptionMapper
-  public RestResponse<ApiError> resteasyReactiveViolationException(
-      ResteasyReactiveViolationException exception) {
+  public RestResponse<ApiError> constraintViolationException(
+      ConstraintViolationException exception) {
     return ResponseBuilder.create(
             Response.Status.BAD_REQUEST, new ApiError(exception.getMessage(), 400))
         .build();
--- sgv2-docsapi/src/test/java/io/stargate/sgv2/docsapi/api/exception/ConstraintViolationExceptionMapperTest.java
+++ sgv2-docsapi/src/test/java/io/stargate/sgv2/docsapi/api/exception/ConstraintViolationExceptionMapperTest.java
@@ -2,20 +2,19 @@ package io.stargate.sgv2.docsapi.api.exception;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.quarkus.hibernate.validator.runtime.jaxrs.ResteasyReactiveViolationException;
 import io.stargate.sgv2.docsapi.api.common.exception.model.dto.ApiError;
 import java.util.HashSet;
+import javax.validation.ConstraintViolationException;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.junit.jupiter.api.Test;
 
-public class ResteasyReactiveViolationExceptionMapperTest {
+public class ConstraintViolationExceptionMapperTest {
   @Test
   public void testErrorCodeMappings() {
-    ResteasyReactiveViolationExceptionMapper mapper =
-        new ResteasyReactiveViolationExceptionMapper();
-    ResteasyReactiveViolationException error =
-        new ResteasyReactiveViolationException("Email must not be empty", new HashSet<>());
-    RestResponse<ApiError> response = mapper.resteasyReactiveViolationException(error);
+    ConstraintViolationExceptionMapper mapper = new ConstraintViolationExceptionMapper();
+    ConstraintViolationException error =
+        new ConstraintViolationException("Email must not be empty", new HashSet<>());
+    RestResponse<ApiError> response = mapper.constraintViolationException(error);
     assertThat(response.getStatus()).isEqualTo(400);
     assertThat(response.getEntity().description()).isEqualTo("Email must not be empty");
   }
