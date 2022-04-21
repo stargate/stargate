493e39ed6c010aaa28700960422a29979c144071
Ivan Senic <ivan.senic@datastax.com>
2022-04-21 17:40:17 +0200

    format fix

--- sgv2-docsapi/src/main/java/io/stargate/sgv2/docsapi/api/common/properties/document/DocumentTableColumns.java
+++ sgv2-docsapi/src/main/java/io/stargate/sgv2/docsapi/api/common/properties/document/DocumentTableColumns.java
@@ -1,22 +1,14 @@
 package io.stargate.sgv2.docsapi.api.common.properties.document;
 
-/**
- * Helper for understanding the available document table columns.
- */
+/** Helper for understanding the available document table columns. */
 public interface DocumentTableColumns {
 
-  /**
-   * @return Value columns, including the leaf.
-   */
+  /** @return Value columns, including the leaf. */
   String[] valueColumnNames();
 
-  /**
-   * @return All the JSON path columns based on the max depth.
-   */
+  /** @return All the JSON path columns based on the max depth. */
   String[] pathColumnNames();
 
-  /**
-   * @return  All the columns of the document table.
-   */
+  /** @return All the columns of the document table. */
   String[] allColumnNames();
 }
