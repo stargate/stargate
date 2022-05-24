bcb977db3b61761cdc2a7372a4c031ca4d1eadf6
Tatu Saloranta <tatu.saloranta@datastax.com>
2022-05-24 11:48:53 -0700

    Fix tests wrt new sampler descs

--- sgv2-service-common/src/test/java/io/stargate/sgv2/common/metrics/ApiTimingDiagnosticsTest.java
+++ sgv2-service-common/src/test/java/io/stargate/sgv2/common/metrics/ApiTimingDiagnosticsTest.java
@@ -8,30 +8,30 @@ public class ApiTimingDiagnosticsTest {
   @Test
   public void testSamplerDefParsingForInterval() {
     assertThat(ApiTimingDiagnosticsSampler.fromString("5s").toString())
-        .isEqualTo("at most every 5 seconds");
+        .isEqualTo("for event at most every 5 seconds");
     assertThat(ApiTimingDiagnosticsSampler.fromString("12s").toString())
-        .isEqualTo("at most every 12 seconds");
+        .isEqualTo("for event at most every 12 seconds");
     assertThat(ApiTimingDiagnosticsSampler.fromString("12.0s").toString())
-        .isEqualTo("at most every 12 seconds");
+        .isEqualTo("for event at most every 12 seconds");
     assertThat(ApiTimingDiagnosticsSampler.fromString("12.75s").toString())
-        .isEqualTo("at most every 12.75 seconds");
+        .isEqualTo("for event at most every 12.75 seconds");
     assertThat(ApiTimingDiagnosticsSampler.fromString("12.750s").toString())
-        .isEqualTo("at most every 12.75 seconds");
+        .isEqualTo("for event at most every 12.75 seconds");
     assertThat(ApiTimingDiagnosticsSampler.fromString("0.5s").toString())
-        .isEqualTo("at most every 0.5 seconds");
+        .isEqualTo("for event at most every 0.5 seconds");
   }
 
   @Test
   public void testSamplerDefParsingForProbability() {
-    assertThat(ApiTimingDiagnosticsSampler.fromString("50%").toString()).isEqualTo("50%");
-    assertThat(ApiTimingDiagnosticsSampler.fromString("15%").toString()).isEqualTo("15%");
-    assertThat(ApiTimingDiagnosticsSampler.fromString("15.00%").toString()).isEqualTo("15%");
-    assertThat(ApiTimingDiagnosticsSampler.fromString("0.25%").toString()).isEqualTo("0.25%");
+    assertThat(ApiTimingDiagnosticsSampler.fromString("50%").toString()).isEqualTo("for 50% of events");
+    assertThat(ApiTimingDiagnosticsSampler.fromString("15%").toString()).isEqualTo("for 15% of events");
+    assertThat(ApiTimingDiagnosticsSampler.fromString("15.00%").toString()).isEqualTo("for 15% of events");
+    assertThat(ApiTimingDiagnosticsSampler.fromString("0.25%").toString()).isEqualTo("for 0.25% of events");
   }
 
   @Test
   public void testSamplerDefParsingForNone() {
-    assertThat(ApiTimingDiagnosticsSampler.fromString("").toString()).isEqualTo("none");
-    assertThat(ApiTimingDiagnosticsSampler.fromString("none").toString()).isEqualTo("none");
+    assertThat(ApiTimingDiagnosticsSampler.fromString("").toString()).isEqualTo("for none of events");
+    assertThat(ApiTimingDiagnosticsSampler.fromString("none").toString()).isEqualTo("for none of events");
   }
 }
