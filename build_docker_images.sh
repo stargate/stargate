680da9114f182330c5a4f1d3ff3d865e2bac06d4
Michael Penick <michael.penick@datastax.com>
2022-06-24 12:53:02 -0400

    Make docker repo a flag

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -9,9 +9,12 @@ set -euo pipefail
 # -p - causes the images to be built for all supported platform architectures and pushed to
 #   Docker Hub (assumes you are logged in to Stargate Docker Hub account).
 #   This is intended to be used as part of automated builds.
+# -a - only build/push the API images; skip building the persistence images
 # -t <version> - overrides the default tag that will be applied to the image with the one
 #   you provide. By default the tag consists of the version is obtained from the parent
 #   pom.xml file, prepended with v, i.e. v2.0.0.
+# -r <repository> - overrides the default repository used to tag image. By
+#   default the repository is "stargateio".
 
 #
 # Defaults
@@ -23,17 +26,25 @@ DOCKER_FLAGS="--load"
 # extract Stargate version from project pom file
 SGTAG="v$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)"
 
+REPO="stargateio"
+
 #
 # overrides via command line
 #
 
-while getopts ":pt:" opt; do
+while getopts ":pat:r:" opt; do
   case $opt in
     p)
       DOCKER_FLAGS="--platform linux/amd64,linux/arm64 --push"
       ;;
     t)
       SGTAG=$OPTARG
+      ;;
+    r)
+      REPO=$OPTARG
+      ;;
+    a)
+      API_ONLY=true
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -58,9 +69,11 @@ cp ./stargate-lib/*.jar $LIBDIR
 cp ./stargate-lib/logback.xml $LIBDIR
 rm ${LIBDIR}/persistence*.jar
 
-docker buildx build --target coordinator-4_0 --build-arg LIBDIR="$LIBDIR" -t stargateio/coordinator-4_0:$SGTAG $DOCKER_FLAGS .
-docker buildx build --target coordinator-3_11 --build-arg LIBDIR="$LIBDIR" -t stargateio/coordinator-3_11:$SGTAG $DOCKER_FLAGS .
-docker buildx build --target coordinator-dse-68 --build-arg LIBDIR="$LIBDIR" -t stargateio/coordinator-dse-68:$SGTAG $DOCKER_FLAGS .
+if [ -z $API_ONLY ]; then
+  docker buildx build --target coordinator-4_0 --build-arg LIBDIR="$LIBDIR" -t $REPO/coordinator-4_0:$SGTAG $DOCKER_FLAGS .
+  docker buildx build --target coordinator-3_11 --build-arg LIBDIR="$LIBDIR" -t $REPO/coordinator-3_11:$SGTAG $DOCKER_FLAGS .
+  docker buildx build --target coordinator-dse-68 --build-arg LIBDIR="$LIBDIR" -t $REPO/coordinator-dse-68:$SGTAG $DOCKER_FLAGS .
+fi
 
 rm -rf ${LIBDIR}
 
@@ -68,6 +81,5 @@ rm -rf ${LIBDIR}
 # API Service images
 #
 
-docker buildx build --target restapi -t stargateio/restapi:$SGTAG $DOCKER_FLAGS .
-docker buildx build --target graphqlapi -t stargateio/graphqlapi:$SGTAG $DOCKER_FLAGS .
-
+docker buildx build --target restapi -t $REPO/restapi:$SGTAG $DOCKER_FLAGS .
+docker buildx build --target graphqlapi -t $REPO/graphqlapi:$SGTAG $DOCKER_FLAGS .
