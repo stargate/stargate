47e838d7027e04bb625f1bdb6a1560816a105e90
Olivier Michallat <olim7t@users.noreply.github.com>
2022-07-07 14:54:20 -0700

    Adapt GCB CI

--- ci/test.sh
+++ ci/test.sh
@@ -43,7 +43,7 @@ esac
 echoinfo "Using backend $PERSISTENCE_BACKEND"
 
 export CCM_CLUSTER_START_TIMEOUT_OVERRIDE=600
-./mvnw -B verify --file pom.xml \
+./mvnw -B verify --file coordinator/pom.xml \
 -P \${C3}it-cassandra-3.11 \
 -P \${C4}it-cassandra-4.0 \
 -P \${DSE}dse -P \${DSE}it-dse-6.8 \
