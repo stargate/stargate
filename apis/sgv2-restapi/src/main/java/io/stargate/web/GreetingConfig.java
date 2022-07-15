f9cb0a3c0ce5e16ebf8479e1a112a042b915a47a
Tatu Saloranta <tatu.saloranta@datastax.com>
2022-07-15 15:00:35 -0700

    Start work on #1892 by adding auto-generated skeleton

---
+++ apis/sgv2-restapi/src/main/java/io/stargate/web/GreetingConfig.java
@@ -0,0 +1,12 @@
+package io.stargate.web;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
---
+++ apis/sgv2-restapi/src/test/java/io/stargate/web/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package io.stargate.web;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
