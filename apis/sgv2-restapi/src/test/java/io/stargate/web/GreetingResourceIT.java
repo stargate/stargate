7ba968ab1b5025f4d8ed52ef33df9afe1ae5fd51
Tatu Saloranta <tatu.saloranta@datastax.com>
2022-07-20 14:57:08 -0700

    Add needed deps, remove "hello world" end point

--- apis/sgv2-restapi/src/test/java/io/stargate/web/GreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.stargate.web;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-
-  // Execute the same tests but in native mode.
-}
