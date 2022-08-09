89adde707a8db846b52d70bd3270c8efb59a8bc6
Tatu Saloranta <tatu.saloranta@datastax.com>
2022-08-09 13:56:16 -0700

    Some test refactoring

---
+++ apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/RestApiV2QIntegrationTestBase.java
@@ -0,0 +1,109 @@
+package io.stargate.sgv2.it;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.google.protobuf.StringValue;
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.restassured.RestAssured;
+import io.stargate.bridge.proto.QueryOuterClass;
+import io.stargate.bridge.proto.StargateBridge;
+import io.stargate.sgv2.api.common.StargateRequestInfo;
+import io.stargate.sgv2.restapi.service.models.Sgv2RESTResponse;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import javax.inject.Inject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+
+public class RestApiV2QIntegrationTestBase {
+  protected static final ObjectMapper objectMapper = JsonMapper.builder().build();
+
+  @Inject protected StargateRequestInfo stargateRequestInfo;
+
+  protected StargateBridge bridge;
+
+  @TestHTTPResource protected String baseUrl;
+
+  private String testKeyspaceName;
+
+  /*
+  /////////////////////////////////////////////////////////////////////////
+  // Initialization, Dependency Injection
+  /////////////////////////////////////////////////////////////////////////
+   */
+
+  @BeforeAll
+  public void init() {
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+  }
+
+  @BeforeEach
+  public void initPerTest(TestInfo testInfo) {
+    bridge = stargateRequestInfo.getStargateBridge();
+    String testName = testInfo.getTestMethod().map(ti -> ti.getName()).get();
+    testKeyspaceName = "ks_" + testName + "_" + System.currentTimeMillis();
+
+    createKeyspace(testKeyspaceName);
+  }
+
+  /*
+  /////////////////////////////////////////////////////////////////////////
+  // Schema initialization
+  /////////////////////////////////////////////////////////////////////////
+   */
+
+  protected void createKeyspace(String keyspaceName) {
+    String cql =
+        "CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}"
+            .formatted(keyspaceName);
+    QueryOuterClass.Query.Builder query =
+        QueryOuterClass.Query.newBuilder()
+            .setCql(cql)
+            .setParameters(QueryOuterClass.QueryParameters.getDefaultInstance());
+    bridge.executeQuery(query.build()).await().atMost(Duration.ofSeconds(10));
+  }
+
+  protected QueryOuterClass.Response executeCql(String cql, QueryOuterClass.Value... values) {
+    return executeCql(cql, testKeyspaceName, values);
+  }
+
+  protected QueryOuterClass.Response executeCql(
+      String cql, String ks, QueryOuterClass.Value... values) {
+    QueryOuterClass.QueryParameters parameters =
+        QueryOuterClass.QueryParameters.newBuilder().setKeyspace(StringValue.of(ks)).build();
+    QueryOuterClass.Query.Builder query =
+        QueryOuterClass.Query.newBuilder().setCql(cql).setParameters(parameters);
+    if (values.length > 0) {
+      query.setValues(QueryOuterClass.Values.newBuilder().addAllValues(Arrays.asList(values)));
+    }
+    return bridge.executeQuery(query.build()).await().atMost(Duration.ofSeconds(10));
+  }
+
+  /*
+  /////////////////////////////////////////////////////////////////////////
+  // JSON handling
+  /////////////////////////////////////////////////////////////////////////
+   */
+
+  protected <T> T readWrappedRESTResponse(String body, Class<T> wrappedType) {
+    JavaType wrapperType =
+        objectMapper.getTypeFactory().constructParametricType(Sgv2RESTResponse.class, wrappedType);
+    try {
+      Sgv2RESTResponse<T> wrapped = objectMapper.readValue(body, wrapperType);
+      return wrapped.getData();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  protected <T> T readJsonAs(String body, Class<T> asType) {
+    try {
+      return objectMapper.readValue(body, asType);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
--- apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/RestApiV2QSchemaTest.java
+++ apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/RestApiV2QSchemaTest.java
@@ -1,31 +1,19 @@
 package io.stargate.sgv2.it;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import com.google.protobuf.StringValue;
-import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
-import io.restassured.RestAssured;
-import io.stargate.bridge.proto.QueryOuterClass;
-import io.stargate.bridge.proto.QueryOuterClass.Query;
-import io.stargate.bridge.proto.QueryOuterClass.QueryParameters;
-import io.stargate.bridge.proto.StargateBridge;
-import io.stargate.sgv2.api.common.StargateRequestInfo;
 import io.stargate.sgv2.api.common.config.constants.HttpConstants;
 import io.stargate.sgv2.common.testprofiles.IntegrationTestProfile;
+import io.stargate.sgv2.restapi.service.models.Sgv2Keyspace;
 import java.io.IOException;
-import java.time.Duration;
-import java.util.Arrays;
 import javax.enterprise.context.control.ActivateRequestContext;
-import javax.inject.Inject;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestClassOrder;
-import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestInstance;
 
 @QuarkusTest
@@ -33,60 +21,46 @@ import org.junit.jupiter.api.TestInstance;
 @ActivateRequestContext
 @TestClassOrder(ClassOrderer.ClassName.class) // prefer stable even if arbitrary ordering
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class RestApiV2QSchemaTest {
-  @Inject protected StargateRequestInfo stargateRequestInfo;
-
-  protected StargateBridge bridge;
-
-  @TestHTTPResource protected String baseUrl;
-
-  private String testKeyspaceName;
-
-  @BeforeAll
-  public void init() {
-    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-  }
-
-  @BeforeEach
-  public void initPerTest(TestInfo testInfo) {
-    bridge = stargateRequestInfo.getStargateBridge();
-    String testName = testInfo.getTestMethod().map(ti -> ti.getName()).get();
-    testKeyspaceName = "ks_" + testName + "_" + System.currentTimeMillis();
-
-    createKeyspace(testKeyspaceName);
-  }
+public class RestApiV2QSchemaTest extends RestApiV2QIntegrationTestBase {
 
   @Test
   public void keyspacesGetAll() throws IOException {
-    given()
-        .header(HttpConstants.AUTHENTICATION_TOKEN_HEADER_NAME, "")
-        .when()
-        .get("/v2/schemas/keyspaces")
-        .then()
-        .statusCode(HttpStatus.SC_OK);
+    String response =
+        given()
+            .header(HttpConstants.AUTHENTICATION_TOKEN_HEADER_NAME, "")
+            .when()
+            .get("/v2/schemas/keyspaces")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .extract()
+            .asString();
+    assertSystemKeyspaces(readWrappedRESTResponse(response, Sgv2Keyspace[].class));
   }
 
-  protected void createKeyspace(String keyspaceName) {
-    String cql =
-        "CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}"
-            .formatted(keyspaceName);
-    Query.Builder query =
-        Query.newBuilder().setCql(cql).setParameters(QueryParameters.getDefaultInstance());
-    bridge.executeQuery(query.build()).await().atMost(Duration.ofSeconds(10));
+  @Test
+  public void keyspacesGetAllRaw() throws IOException {
+    String response =
+        given()
+            .header(HttpConstants.AUTHENTICATION_TOKEN_HEADER_NAME, "")
+            .queryParam("raw", "true")
+            .when()
+            .get("/v2/schemas/keyspaces")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .extract()
+            .asString();
+    assertSystemKeyspaces(readJsonAs(response, Sgv2Keyspace[].class));
   }
 
-  protected QueryOuterClass.Response executeCql(String cql, QueryOuterClass.Value... values) {
-    return executeCql(cql, testKeyspaceName, values);
+  private void assertSystemKeyspaces(Sgv2Keyspace[] keyspaces) {
+    assertSimpleKeyspaces(keyspaces, "system", "system_auth", "system_schema");
   }
 
-  protected QueryOuterClass.Response executeCql(
-      String cql, String ks, QueryOuterClass.Value... values) {
-    QueryParameters parameters =
-        QueryParameters.newBuilder().setKeyspace(StringValue.of(ks)).build();
-    Query.Builder query = Query.newBuilder().setCql(cql).setParameters(parameters);
-    if (values.length > 0) {
-      query.setValues(QueryOuterClass.Values.newBuilder().addAllValues(Arrays.asList(values)));
+  private void assertSimpleKeyspaces(Sgv2Keyspace[] keyspaces, String... expectedKsNames) {
+    for (String ksName : expectedKsNames) {
+      assertThat(keyspaces)
+          .anySatisfy(
+              v -> assertThat(v).usingRecursiveComparison().isEqualTo(new Sgv2Keyspace(ksName)));
     }
-    return bridge.executeQuery(query.build()).await().atMost(Duration.ofSeconds(10));
   }
 }
