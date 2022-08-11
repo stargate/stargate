50c2c33318b980ed3eaf054eed8ca2af2f034fa3
Tatu Saloranta <tatu.saloranta@datastax.com>
2022-08-11 16:07:37 -0700

    Add skeletal IT classes

---
+++ apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/RestApiV2QRowAddIT.java
@@ -0,0 +1,21 @@
+package io.stargate.sgv2.it;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+import io.stargate.sgv2.common.testprofiles.IntegrationTestProfile;
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.TestClassOrder;
+import org.junit.jupiter.api.TestInstance;
+
+import javax.enterprise.context.control.ActivateRequestContext;
+
+@QuarkusTest
+@TestProfile(IntegrationTestProfile.class)
+@ActivateRequestContext
+@TestClassOrder(ClassOrderer.DisplayName.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RestApiV2QRowAddIT extends RestApiV2QIntegrationTestBase {
+    public RestApiV2QRowAddIT() {
+        super("rowadd_ks_", "rowadd_t_");
+    }
+}
