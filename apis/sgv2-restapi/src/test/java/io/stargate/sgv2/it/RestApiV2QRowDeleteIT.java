b4f02a34d4c7ff216e4784f13019d4db67a90c35
Tatu Saloranta <tatu.saloranta@datastax.com>
2022-08-15 14:19:01 -0700

    Initial for work SGv2/REST/Q/DELETE ITs

--- apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/RestApiV2QRowDeleteIT.java
+++ apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/RestApiV2QRowDeleteIT.java
@@ -2,11 +2,23 @@ package io.stargate.sgv2.it;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
+import io.restassured.http.ContentType;
+import io.stargate.sgv2.api.common.config.constants.HttpConstants;
 import io.stargate.sgv2.common.testprofiles.IntegrationTestProfile;
 import javax.enterprise.context.control.ActivateRequestContext;
+
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.TestInstance;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
 
 @QuarkusTest
 @TestProfile(IntegrationTestProfile.class)
@@ -17,4 +29,64 @@ public class RestApiV2QRowDeleteIT extends RestApiV2QIntegrationTestBase {
   public RestApiV2QRowDeleteIT() {
     super("rowdel_ks_", "rowdel_t_");
   }
+
+  @Test
+  public void deleteRow() {
+    final String tableName = testTableName();
+    createSimpleTestTable(testKeyspaceName(), tableName);
+
+    final String rowIdentifier = UUID.randomUUID().toString();
+    Map<String, String> row = new HashMap<>();
+    row.put("id", rowIdentifier);
+    row.put("firstName", "John");
+    insertRow(testKeyspaceName(), tableName, row);
+
+    // Successful deletion
+    deleteRow(endpointPathForRowByPK(testKeyspaceName(), tableName, rowIdentifier));
+  }
+
+
+  @Test
+  public void deleteRowByPartitionKey() { }
+
+  @Test
+  public void deleteRowClustering() { }
+
+  @Test
+  public void deleteRowsMixedClusteringAndCK() { }
+
+  @Test
+  public void deleteRowsWithMixedClustering() { }
+
+  @Test
+  public void deleteRowNoSuchKey() {
+    final String tableName = testTableName();
+    createSimpleTestTable(testKeyspaceName(), tableName);
+
+    final String rowIdentifier = UUID.randomUUID().toString();
+
+    // To keep DELETE idempotent, it always succeeds even if no rows match, so:
+    deleteRow(endpointPathForRowByPK(testKeyspaceName(), tableName, rowIdentifier),
+            HttpStatus.SC_NO_CONTENT);
+  }
+
+  /*
+  /////////////////////////////////////////////////////////////////////////
+  // Helper methods
+  /////////////////////////////////////////////////////////////////////////
+   */
+
+  private void deleteRow(String deletePath) {
+    deleteRow(deletePath, HttpStatus.SC_NO_CONTENT);
+  }
+
+  private void deleteRow(String deletePath, int expectedStatus) {
+    given()
+            .header(HttpConstants.AUTHENTICATION_TOKEN_HEADER_NAME, "")
+            .when()
+            .delete(deletePath)
+            .then()
+            .statusCode(expectedStatus);
+  }
+
 }
