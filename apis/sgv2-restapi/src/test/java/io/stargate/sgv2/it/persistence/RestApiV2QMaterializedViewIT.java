195a7a8dc30f2b675b6b1fea27bd6535c9760ba0
Tatu Saloranta <tatu.saloranta@datastax.com>
2022-10-14 10:09:35 -0700

    Change a Thread.sleep() to Awaitility as per Ivan's suggestion

--- apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/persistence/RestApiV2QMaterializedViewIT.java
+++ apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/persistence/RestApiV2QMaterializedViewIT.java
@@ -9,10 +9,13 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.stargate.sgv2.common.IntegrationTestUtils;
 import io.stargate.sgv2.common.testresource.StargateTestResource;
 import io.stargate.sgv2.it.RestApiV2QCqlEnabledTestBase;
+
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -80,18 +83,22 @@ public class RestApiV2QMaterializedViewIT extends RestApiV2QCqlEnabledTestBase {
 
     // 14-Sep-2022, tatu: Not sure why but there is a transient issue here wrt timing.
     //   Locally test does not appear to ever fail, but in CI it does, with error suggesting
-    //   MV has not been created or Schema metadata not (yet) updated. In absence of
-    //   better solution, let's try simple wait to give it time; 3 seconds should do it.
+    //   MV has not been created or Schema metadata not (yet) updated.
+    //  Awaitility needs to be used to retry a few times.
 
-    Thread.sleep(3000L);
+    Awaitility.await()
+            .atMost(Duration.ofSeconds(5))
+            .atLeast(Duration.ofMillis(100L))
+            .pollInterval(Duration.ofMillis(500L))
+            .untilAsserted(() -> {
+              List<Map<String, Object>> rows = findAllRowsAsList(keyspaceName, materializedViewName);
 
-    List<Map<String, Object>> rows = findAllRowsAsList(keyspaceName, materializedViewName);
+              // Alas, due to "id" as partition key, ordering is arbitrary; so need to
+              // convert from List to something like Set
+              expRows.remove(2); // the MV should only return the rows with a lastName
 
-    // Alas, due to "id" as partition key, ordering is arbitrary; so need to
-    // convert from List to something like Set
-    expRows.remove(2); // the MV should only return the rows with a lastName
-
-    assertThat(rows.size()).isEqualTo(2);
-    assertThat(new LinkedHashSet<>(rows)).isEqualTo(new LinkedHashSet<>(expRows));
+              assertThat(rows.size()).isEqualTo(2);
+              assertThat(new LinkedHashSet<>(rows)).isEqualTo(new LinkedHashSet<>(expRows));
+            });
   }
 }
