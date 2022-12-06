558427bab4aaf328edeb8cc21944255661dfdd19
Tatu Saloranta <87213665+tatu-at-datastax@users.noreply.github.com>
2022-12-06 12:07:43 -0800

    Merge revert #2296 of early-auth-check (#619) into main (v2) (#2297)

--- persistence-cassandra-4.0/src/main/java/io/stargate/db/cassandra/impl/StargateQueryHandler.java
+++ persistence-cassandra-4.0/src/main/java/io/stargate/db/cassandra/impl/StargateQueryHandler.java
@@ -141,15 +141,15 @@ public class StargateQueryHandler implements QueryHandler {
       Map<String, ByteBuffer> customPayload,
       long queryStartNanoTime)
       throws RequestExecutionException, RequestValidationException {
-    if (customPayload != null && customPayload.containsKey("stargate.auth.subject.token")) {
-      authorizeByToken(customPayload, statement);
-    }
-
     ResultMessage result =
         maybeIntercept(statement, queryState, options, customPayload, queryStartNanoTime);
 
     if (result != null) {
       return result;
+    }
+
+    if (customPayload != null && customPayload.containsKey("stargate.auth.subject.token")) {
+      authorizeByToken(customPayload, statement);
     }
 
     return QueryProcessor.instance.process(statement, queryState, options, queryStartNanoTime);
@@ -195,15 +195,16 @@ public class StargateQueryHandler implements QueryHandler {
       Map<String, ByteBuffer> customPayload,
       long queryStartNanoTime)
       throws RequestExecutionException, RequestValidationException {
-    if (customPayload != null && customPayload.containsKey("stargate.auth.subject.token")) {
-      authorizeByToken(customPayload, statement);
-    }
 
     ResultMessage result =
         maybeIntercept(statement, queryState, options, customPayload, queryStartNanoTime);
 
     if (result != null) {
       return result;
+    }
+
+    if (customPayload != null && customPayload.containsKey("stargate.auth.subject.token")) {
+      authorizeByToken(customPayload, statement);
     }
 
     return QueryProcessor.instance.processPrepared(
