1d4af926ffb6bcaa8fb2becf3562ec3647878c7d
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-01-23 13:55:22 -0800

    Clean up test a bit, use dedicated keyspace to avoid conflicts with other tests

--- testing/src/main/java/io/stargate/it/cql/DynamicCompositeTypeTest.java
+++ testing/src/main/java/io/stargate/it/cql/DynamicCompositeTypeTest.java
@@ -7,37 +7,50 @@ import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 import io.stargate.it.BaseIntegrationTest;
 import io.stargate.it.driver.CqlSessionExtension;
+import io.stargate.it.driver.CqlSessionSpec;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(CqlSessionExtension.class)
+@CqlSessionSpec(
+    initQueries = {
+      "CREATE KEYSPACE IF NOT EXISTS dynamic_comp_ks WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':'1'}"
+    })
 public class DynamicCompositeTypeTest extends BaseIntegrationTest {
-  @Test
-  @DisplayName("Try to create Table with DynamicCompositeType")
-  public void createDynamicCompositeTypeTable(CqlSession session) {
-    // Cannot use "CreateTable" since we seem not to have support for building dynamic
-    // composite type. Instead, straight CQL:
-    List<Row> rows =
-        session
-            .execute(
-                "CREATE TABLE dynamic_composite_table (\n"
-                    + "       k int PRIMARY KEY,\n"
-                    + "       dct 'DynamicCompositeType(s => UTF8Type, i => Int32Type)',\n"
-                    + "       stuff Text)")
-            .all();
-    assertThat(rows).isEmpty();
+  @BeforeEach
+  public void initTable(CqlSession session) {
+    session.execute(
+        "CREATE TABLE dynamic_comp_ks.dynamic_composite_table (\n"
+            + "       k int PRIMARY KEY,\n"
+            + "       dct 'DynamicCompositeType(s => UTF8Type, i => Int32Type)',\n"
+            + "       stuff Text)");
+  }
 
+  @AfterEach
+  public void cleanupTable(CqlSession session) {
+    session.execute("DROP TABLE dynamic_comp_ks.dynamic_composite_table IF EXISTS");
+  }
+
+  @Test
+  @DisplayName("Try to create Table with DynamicCompositeType, insert and read")
+  public void createDynamicCompositeTypeTable(CqlSession session) {
     // Ok let's see if we can actually insert rows (without trying to set DCT)
     session.execute(
-        SimpleStatement.builder("INSERT into dynamic_composite_table (k, stuff) values (:k, :v)")
+        SimpleStatement.builder(
+                "INSERT into dynamic_comp_ks.dynamic_composite_table (k, stuff) values (:k, :v)")
             .addNamedValue("k", 123)
             .addNamedValue("v", "text value")
             .build());
 
     // And then fetch row inserted as well, first without accessing DCT
-    rows = session.execute("select k,stuff from dynamic_composite_table where k=123").all();
+    List<Row> rows =
+        session
+            .execute("select k,stuff from dynamic_comp_ks.dynamic_composite_table where k=123")
+            .all();
     assertThat(rows).isNotNull().hasSize(1);
     Row row = rows.get(0);
     assertThat(row.getInt("k")).isEqualTo(123);
@@ -45,7 +58,10 @@ public class DynamicCompositeTypeTest extends BaseIntegrationTest {
 
     // and then getting DCT too (missing/null)
     // !!! Fails here at this point
-    row = session.execute("select k,stuff,dct from dynamic_composite_table where k=123").one();
+    row =
+        session
+            .execute("select k,stuff,dct from dynamic_comp_ks.dynamic_composite_table where k=123")
+            .one();
     assertThat(row.getInt("k")).isEqualTo(123);
     assertThat(row.getString("stuff")).isEqualTo("text value");
     assertThat(row.isNull("dct")).isTrue();
