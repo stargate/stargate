71013c670c75486d0921da0f025f8c00d61c6f85
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-02-01 14:28:53 -0800

    Change cql-disabled test to rely on defaults since we want default to be disabled

--- apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/RestApiV2QCqlDisabledIT.java
+++ apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/RestApiV2QCqlDisabledIT.java
@@ -1,41 +1,28 @@
 package io.stargate.sgv2.it;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import io.quarkus.test.junit.QuarkusTestProfile;
-import io.quarkus.test.junit.TestProfile;
 import io.restassured.http.ContentType;
 import io.stargate.sgv2.common.testresource.StargateTestResource;
-import java.util.Map;
 import org.junit.jupiter.api.Test;
 
-/** Integration tests that verify it is possible to disable CQL endpoint (at {@code /v2/cql}). */
+/** Integration tests that verify that CQL endpoint (at {@code /v2/cql}) is disabled by default. */
 @QuarkusIntegrationTest
 @QuarkusTestResource(StargateTestResource.class)
-@TestProfile(RestApiV2QCqlDisabledIT.Profile.class)
 public class RestApiV2QCqlDisabledIT extends RestApiV2QIntegrationTestBase {
-  // Although /cql endpoint may be disabled by default let's make sure
-  public static class Profile implements QuarkusTestProfile {
-    @Override
-    public Map<String, String> getConfigOverrides() {
-      return Map.of("stargate.rest.cql.disabled", "true");
-    }
-  }
-
   public RestApiV2QCqlDisabledIT() {
     super("cqld_ks_", "cqld_t_", KeyspaceCreation.NONE);
   }
 
   @Test
   public void testCqlQueryWhenEndpointDisabled() {
-        givenWithAuth()
-            .contentType(ContentType.TEXT)
-            .body("SELECT key FROM system.local")
-            .when()
-            .post(endpointPathForCQL())
-            .then()
-            .statusCode(404);
+    givenWithAuth()
+        .contentType(ContentType.TEXT)
+        .body("SELECT key FROM system.local")
+        .when()
+        .post(endpointPathForCQL())
+        .then()
+        .statusCode(404);
   }
 }
