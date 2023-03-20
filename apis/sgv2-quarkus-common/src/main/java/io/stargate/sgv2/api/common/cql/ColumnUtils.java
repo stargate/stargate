37970e062e010ba42cd15f60dcdb956585248de1
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2023-03-20 11:25:05 -0400

    Added review comment

--- apis/sgv2-quarkus-common/src/main/java/io/stargate/sgv2/api/common/cql/ColumnUtils.java
+++ apis/sgv2-quarkus-common/src/main/java/io/stargate/sgv2/api/common/cql/ColumnUtils.java
@@ -22,6 +22,10 @@ public class ColumnUtils {
 
   private static final Pattern PATTERN_DOUBLE_QUOTE = Pattern.compile("\"", Pattern.LITERAL);
   private static final String ESCAPED_DOUBLE_QUOTE = Matcher.quoteReplacement("\"\"");
+  /**
+   * Updated regex pattern to support selecting collection entry lime map_column['entry_key'],
+   * set_column['set_value']
+   */
   private static final Pattern UNQUOTED_IDENTIFIER =
       Pattern.compile("[a-z][a-z0-9_]*(\\['.*'\\])?");
 
