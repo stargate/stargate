b37ae19568df3a983100b867c30814519b4975a1
Jeffrey Carpenter <jeffrey.carpenter@datastax.com>
2023-06-21 15:06:17 -0700

    formatting error

--- coordinator/testing/src/main/java/io/stargate/it/http/MetricsTest.java
+++ coordinator/testing/src/main/java/io/stargate/it/http/MetricsTest.java
@@ -45,9 +45,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-/**
- * Tests against HTTP modules in the Coordinator.
- */
+/** Tests against HTTP modules in the Coordinator. */
 @NotThreadSafe
 @StargateSpec(parametersCustomizer = "buildParameters")
 @Order(TestOrder.LAST)
