52977c6afe5e3f412956a40ad5d2419eca242ef2
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-07-05 17:51:19 -0700

    Fix GET access wrt new tests

--- apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/RestApiV2QPrimaryKeyIT.java
+++ apis/sgv2-restapi/src/test/java/io/stargate/sgv2/it/RestApiV2QPrimaryKeyIT.java
@@ -23,12 +23,12 @@ public class RestApiV2QPrimaryKeyIT extends RestApiV2QIntegrationTestBase {
     super("rowpk_ks_", "rowpk_t_", KeyspaceCreation.PER_CLASS);
   }
 
-  private static final String TEST_KEY1 = "key11/ab&cd/xy";
-  private static final String TEST_KEY2 = "key2/ef&gh/yz";
-
   @Test
   public void getWithSpecialCharsInPK() {
     final String tableName = testTableName();
+    final String testKey1 = "get1/ab&cd/xy";
+    final String testKey2 = "get2/ef&gh/yz";
+
     createPKTestTable(tableName);
 
     // To try to ensure we actually find the right entry, create one other entry first
@@ -41,13 +41,13 @@ public class RestApiV2QPrimaryKeyIT extends RestApiV2QIntegrationTestBase {
     insertRow(
         testKeyspaceName(),
         tableName,
-        Map.of("id1", TEST_KEY1, "id2", TEST_KEY2, "description", "Desc"));
+        Map.of("id1", testKey1, "id2", testKey2, "description", "Desc"));
 
     String response =
         givenWithAuth()
             .queryParam("raw", "false")
             .when()
-            .get(endpointTemplateForRowByPK(testKeyspaceName(), tableName, 2), TEST_KEY1, TEST_KEY2)
+            .get(endpointTemplateForRowByPK(testKeyspaceName(), tableName, 2), testKey1, testKey2)
             .then()
             .statusCode(HttpStatus.SC_OK)
             .extract()
@@ -59,20 +59,23 @@ public class RestApiV2QPrimaryKeyIT extends RestApiV2QIntegrationTestBase {
     List<Map<String, Object>> data = wrapper.data();
     assertThat(data.size()).isEqualTo(1);
     // and that its contents match
-    assertThat(data.get(0).get("id1")).isEqualTo(TEST_KEY1);
-    assertThat(data.get(0).get("id2")).isEqualTo(TEST_KEY2);
+    assertThat(data.get(0).get("id1")).isEqualTo(testKey1);
+    assertThat(data.get(0).get("id2")).isEqualTo(testKey2);
     assertThat(data.get(0).get("description")).isEqualTo("Desc");
   }
 
   @Test
   public void updateWithSpecialCharsInPK() {
     final String tableName = testTableName();
+    final String testKey1 = "put1/ab&cd/xy";
+    final String testKey2 = "put2/ef&gh/yz";
+
     createPKTestTable(tableName);
 
     insertRow(
         testKeyspaceName(),
         tableName,
-        Map.of("id1", TEST_KEY1, "id2", TEST_KEY2, "description", "Desc"));
+        Map.of("id1", testKey1, "id2", testKey2, "description", "Desc"));
 
     Map<String, String> rowUpdate = Map.of("description", "Updated Desc");
     final String payloadJSON = asJsonString(rowUpdate);
@@ -81,27 +84,28 @@ public class RestApiV2QPrimaryKeyIT extends RestApiV2QIntegrationTestBase {
         .contentType(ContentType.JSON)
         .body(payloadJSON)
         .when()
-        .put(endpointTemplateForRowByPK(testKeyspaceName(), tableName, 2), TEST_KEY1, TEST_KEY2)
+        .put(endpointTemplateForRowByPK(testKeyspaceName(), tableName, 2), testKey1, testKey2)
         .then()
         .statusCode(HttpStatus.SC_OK);
 
-    List<Map<String, Object>> rows =
-        findRowsAsList(testKeyspaceName(), tableName, TEST_KEY1, TEST_KEY2);
+    List<Map<String, Object>> rows = findRows(testKeyspaceName(), tableName, testKey1, testKey2);
     assertThat(rows).hasSize(1);
-    assertThat(rows.get(0).get("id1")).isEqualTo(TEST_KEY1);
-    assertThat(rows.get(0).get("id2")).isEqualTo(TEST_KEY2);
+    assertThat(rows.get(0).get("id1")).isEqualTo(testKey1);
+    assertThat(rows.get(0).get("id2")).isEqualTo(testKey2);
     assertThat(rows.get(0).get("description")).isEqualTo("Updated Desc");
   }
 
   @Test
   public void patchWithSpecialCharsInPK() {
     final String tableName = testTableName();
+    final String testKey1 = "patch1/ab&cd/xy";
+    final String testKey2 = "patch2/ef&gh/yz";
     createPKTestTable(tableName);
 
     insertRow(
         testKeyspaceName(),
         tableName,
-        Map.of("id1", TEST_KEY1, "id2", TEST_KEY2, "description", "Desc"));
+        Map.of("id1", testKey1, "id2", testKey2, "description", "Desc"));
 
     Map<String, String> rowUpdate = Map.of("description", "Patched Desc");
     final String payloadJSON = asJsonString(rowUpdate);
@@ -110,35 +114,35 @@ public class RestApiV2QPrimaryKeyIT extends RestApiV2QIntegrationTestBase {
         .contentType(ContentType.JSON)
         .body(payloadJSON)
         .when()
-        .patch(endpointTemplateForRowByPK(testKeyspaceName(), tableName, 2), TEST_KEY1, TEST_KEY2)
+        .patch(endpointTemplateForRowByPK(testKeyspaceName(), tableName, 2), testKey1, testKey2)
         .then()
         .statusCode(HttpStatus.SC_OK);
 
-    List<Map<String, Object>> rows =
-        findRowsAsList(testKeyspaceName(), tableName, TEST_KEY1, TEST_KEY2);
+    List<Map<String, Object>> rows = findRows(testKeyspaceName(), tableName, testKey1, testKey2);
     assertThat(rows).hasSize(1);
-    assertThat(rows.get(0).get("id1")).isEqualTo(TEST_KEY1);
-    assertThat(rows.get(0).get("id2")).isEqualTo(TEST_KEY2);
+    assertThat(rows.get(0).get("id1")).isEqualTo(testKey1);
+    assertThat(rows.get(0).get("id2")).isEqualTo(testKey2);
     assertThat(rows.get(0).get("description")).isEqualTo("Patched Desc");
   }
 
   @Test
   public void deleteWithSpecialCharsInPK() {
     final String tableName = testTableName();
+    final String testKey1 = "delete1/ab&cd/xy";
+    final String testKey2 = "delete2/ef&gh/yz";
     createPKTestTable(tableName);
 
     insertRow(
         testKeyspaceName(),
         tableName,
-        Map.of("id1", TEST_KEY1, "id2", TEST_KEY2, "description", "Desc"));
+        Map.of("id1", testKey1, "id2", testKey2, "description", "Desc"));
 
-    assertThat(findRowsAsList(testKeyspaceName(), tableName, TEST_KEY1, TEST_KEY2)).hasSize(1);
     givenWithAuth()
         .when()
-        .delete(endpointTemplateForRowByPK(testKeyspaceName(), tableName, 2), TEST_KEY1, TEST_KEY2)
+        .delete(endpointTemplateForRowByPK(testKeyspaceName(), tableName, 2), testKey1, testKey2)
         .then()
         .statusCode(HttpStatus.SC_NO_CONTENT);
-    assertThat(findRowsAsList(testKeyspaceName(), tableName, TEST_KEY1, TEST_KEY2)).hasSize(0);
+    assertThat(findRows(testKeyspaceName(), tableName, testKey1, testKey2)).hasSize(0);
   }
 
   /*
@@ -154,5 +158,19 @@ public class RestApiV2QPrimaryKeyIT extends RestApiV2QIntegrationTestBase {
         Arrays.asList("id1 text", "id2 text", "description text"),
         Arrays.asList("id1"),
         Arrays.asList("id2"));
+  }
+
+  protected List<Map<String, Object>> findRows(
+      String keyspaceName, String tableName, String key1, String key2) {
+    String response =
+        givenWithAuth()
+            .queryParam("raw", "true")
+            .when()
+            .get(endpointTemplateForRowByPK(keyspaceName, tableName, 2), key1, key2)
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .extract()
+            .asString();
+    return readJsonAs(response, LIST_OF_MAPS_TYPE);
   }
 }
