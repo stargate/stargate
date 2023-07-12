8d4b9de26b65ffcc8dab44c63d513ed238f066f1
Jeffrey Carpenter <jeffrey.carpenter@datastax.com>
2023-07-12 16:06:09 -0700

    another attempt

--- coordinator/download_ds_cassandra.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Script to download Datastax Cassandra distribution used for testing persistence-cassandra-5.0 module
-git clone https://github.com/datastax/cassandra.git ds-cassandra
-cd ds-cassandra
-ant -Duse.jdk11=true
-
