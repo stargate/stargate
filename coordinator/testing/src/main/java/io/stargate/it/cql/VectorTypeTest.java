eba73b34d36d67e065c705d8cc88b721aad44fe9
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-07-27 16:17:11 -0700

    Initial skeletal check-in to see how CI work

---
+++ coordinator/testing/src/main/java/io/stargate/it/cql/VectorTypeTest.java
@@ -0,0 +1,27 @@
+package io.stargate.it.cql;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import io.stargate.it.BaseIntegrationTest;
+import io.stargate.it.driver.CqlSessionExtension;
+import io.stargate.it.driver.CqlSessionSpec;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(CqlSessionExtension.class)
+@CqlSessionSpec
+public class VectorTypeTest extends BaseIntegrationTest {
+  private static final String CREATE_VECTOR_TABLE =
+      "CREATE TABLE vector_table ("
+          + "id int PRIMARY KEY, "
+          + "embedding vector<float, 100> "
+          + ")";
+  private static final String DROP_VECTOR_TABLE = "DROP TABLE IF EXISTS vector_table";
+
+  @Test
+  @DisplayName("Should be able to create a table with Vector column")
+  public void createTableInsertAndFetch(CqlSession session) {
+    session.execute(DROP_VECTOR_TABLE);
+    session.execute(CREATE_VECTOR_TABLE);
+  }
+}
