c78a1414b99284eaeceb1ffa620c5729fe96ece6
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-08-01 13:06:02 -0700

    Add actual vector search case too

--- coordinator/testing/src/main/java/io/stargate/it/cql/VectorTypeTest.java
+++ coordinator/testing/src/main/java/io/stargate/it/cql/VectorTypeTest.java
@@ -24,6 +24,10 @@ public class VectorTypeTest extends BaseIntegrationTest {
       "CREATE TABLE IF NOT EXISTS vector_table ("
           + "id int PRIMARY KEY, embedding vector<float, 5> "
           + ")";
+  private static final String CREATE_VECTOR_INDEX =
+      "CREATE CUSTOM INDEX embedding_index "
+          + "  ON vector_table(embedding) USING 'StorageAttachedIndex'";
+
   private static final String DROP_VECTOR_TABLE = "DROP TABLE IF EXISTS vector_table";
 
   @BeforeAll
@@ -34,38 +38,41 @@ public class VectorTypeTest extends BaseIntegrationTest {
   }
 
   @Test
-  @DisplayName("Should be able to create a table with Vector column")
+  @DisplayName("Should be able to insert and fetch a vector value")
   @Order(1)
-  public void createVectorTable(CqlSession session) {
+  public void insertAndGet(CqlSession session) {
     session.execute(DROP_VECTOR_TABLE);
     session.execute(CREATE_VECTOR_TABLE);
-  }
-
-  @Test
-  @DisplayName("Should be able to create an index for Vector column")
-  @Order(2)
-  public void createVectorIndex(CqlSession session) {
-    session.execute(CREATE_VECTOR_TABLE);
     session.execute(
-        "CREATE CUSTOM INDEX IF NOT EXISTS ann_index "
-            + "  ON vector_table(embedding) USING 'StorageAttachedIndex'");
-  }
-
-  @Test
-  @DisplayName("Should be able to insert and fetch a vector value")
-  @Order(3)
-  public void insertAndFetch(CqlSession session) {
-    session.execute(CREATE_VECTOR_TABLE);
-    session.execute(
-        "INSERT into vector_table (id, embedding) values (1, [1.0, 0.5, 0.0, 0.125, 0.25])");
+        "INSERT into vector_table (id, embedding) values (1, [1.0, 0.5, 0.75, 0.125, 0.25])");
     List<Row> rows =
-        session.execute("select id, embedding from vector_table where id = ?", 1).all();
+        session.execute("SELECT id, embedding from vector_table where id = ?", 1).all();
     assertThat(rows).hasSize(1);
     Row row = rows.get(0);
     assertThat(row).isNotNull();
     assertThat(row.getInt(0)).isEqualTo(1);
     CqlVector<Float> v = row.getVector(1, Float.class);
     assertThat(v).hasSize(5);
-    assertThat(v).containsExactly(1.0f, 0.5f, 0.0f, 0.125f, 0.25f);
+    assertThat(v).containsExactly(1.0f, 0.5f, 0.75f, 0.125f, 0.25f);
+  }
+
+  @Test
+  @DisplayName("Should be able to insert and fetch rows by vector similarity")
+  @Order(2)
+  public void insertAndSearch(CqlSession session) {
+    session.execute(DROP_VECTOR_TABLE);
+    session.execute(CREATE_VECTOR_TABLE);
+    session.execute(CREATE_VECTOR_INDEX);
+    session.execute(
+        "INSERT into vector_table (id, embedding) values (123, [0.25, 0.25, 0.25, 0.25, 0.125])");
+    session.execute(
+        "INSERT into vector_table (id, embedding) values (456, [1.0, 1.0, 1.0, 1.0, 1.0])");
+    List<Row> rows =
+        session
+            .execute("SELECT id FROM vector_table ORDER BY embedding ANN OF [1,1,1,1,1] LIMIT 10")
+            .all();
+    assertThat(rows).hasSize(2);
+    assertThat(rows.get(0).getInt(0)).isEqualTo(456);
+    assertThat(rows.get(1).getInt(0)).isEqualTo(123);
   }
 }
