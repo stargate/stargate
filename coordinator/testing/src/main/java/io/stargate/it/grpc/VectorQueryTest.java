4e1dc517512f97d36d2646d7e32cdca751b15af0
Hazel-Datastax <139801967+Hazel-Datastax@users.noreply.github.com>
2023-08-17 11:03:10 -0400

    Reformat file

--- coordinator/testing/src/main/java/io/stargate/it/grpc/VectorQueryTest.java
+++ coordinator/testing/src/main/java/io/stargate/it/grpc/VectorQueryTest.java
@@ -15,6 +15,9 @@
  */
 package io.stargate.it.grpc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
 import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.CqlSession;
 import io.stargate.it.driver.CqlSessionExtension;
@@ -26,75 +29,81 @@ import io.stargate.proto.StargateGrpc;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
-
 @ExtendWith(CqlSessionExtension.class)
 @CqlSessionSpec
 public class VectorQueryTest extends GrpcIntegrationTest {
 
-    @BeforeAll
-    public static void validateRunningOnVSearchEnabled(ClusterConnectionInfo backend) {
-        assumeTrue(
-                backend.supportsVSearch(),
-                "Test disabled if backend does not support Vector Search (vsearch)");
-    }
+  @BeforeAll
+  public static void validateRunningOnVSearchEnabled(ClusterConnectionInfo backend) {
+    assumeTrue(
+        backend.supportsVSearch(),
+        "Test disabled if backend does not support Vector Search (vsearch)");
+  }
 
-    @BeforeEach
-    public void init(CqlSession session) {
-        session.execute("DROP TABLE IF EXISTS vector_table");
-        session.execute("CREATE TABLE IF NOT EXISTS vector_table (id int PRIMARY KEY, embedding vector<float, 5>)");
-        session.execute("CREATE CUSTOM INDEX embedding_index ON vector_table(embedding) USING 'StorageAttachedIndex'");
-    }
+  @BeforeEach
+  public void init(CqlSession session) {
+    session.execute("DROP TABLE IF EXISTS vector_table");
+    session.execute(
+        "CREATE TABLE IF NOT EXISTS vector_table (id int PRIMARY KEY, embedding vector<float, 5>)");
+    session.execute(
+        "CREATE CUSTOM INDEX embedding_index ON vector_table(embedding) USING 'StorageAttachedIndex'");
+  }
 
-    @Test
-    @DisplayName("Should be able to insert and fetch a vector value")
-    @Order(1)
-    public void insertAndGet(@TestKeyspace CqlIdentifier keyspace) {
-        // Insert rows into table
-        StargateGrpc.StargateBlockingStub stub = stubWithCallCredentials();
-        QueryOuterClass.Response response =
-                stub.executeQuery(
-                        cqlQuery("INSERT into vector_table (id, embedding) values (1, [1.0, 0.5, 0.75, 0.125, 0.25])", queryParameters(keyspace)));
-        assertThat(response).isNotNull();
+  @Test
+  @DisplayName("Should be able to insert and fetch a vector value")
+  @Order(1)
+  public void insertAndGet(@TestKeyspace CqlIdentifier keyspace) {
+    // Insert rows into table
+    StargateGrpc.StargateBlockingStub stub = stubWithCallCredentials();
+    QueryOuterClass.Response response =
+        stub.executeQuery(
+            cqlQuery(
+                "INSERT into vector_table (id, embedding) values (1, [1.0, 0.5, 0.75, 0.125, 0.25])",
+                queryParameters(keyspace)));
+    assertThat(response).isNotNull();
 
-        response =
-                stub.executeQuery(
-                        cqlQuery(
-                                "INSERT into vector_table (id, embedding) values (2, [1.0, 1.0, 1.0, 1.0, 1.0])",
-                                queryParameters(keyspace)));
-        assertThat(response).isNotNull();
+    response =
+        stub.executeQuery(
+            cqlQuery(
+                "INSERT into vector_table (id, embedding) values (2, [1.0, 1.0, 1.0, 1.0, 1.0])",
+                queryParameters(keyspace)));
+    assertThat(response).isNotNull();
 
-        // Get rows with Vector values
-        response = stub.executeQuery(cqlQuery("SELECT * FROM vector_table", queryParameters(keyspace)));
-        assertThat(response.hasResultSet()).isTrue();
-        assertThat(response.getResultSet().getRowsCount()).isEqualTo(2);
-    }
+    // Get rows with Vector values
+    response = stub.executeQuery(cqlQuery("SELECT * FROM vector_table", queryParameters(keyspace)));
+    assertThat(response.hasResultSet()).isTrue();
+    assertThat(response.getResultSet().getRowsCount()).isEqualTo(2);
+  }
 
-    @Test
-    @DisplayName("Should be able to insert and fetch rows by vector similarity")
-    @Order(2)
-    public void insertAndSearch(@TestKeyspace CqlIdentifier keyspace) {
-        // Insert rows into table
-        StargateGrpc.StargateBlockingStub stub = stubWithCallCredentials();
-        QueryOuterClass.Response response =
-                stub.executeQuery(
-                        cqlQuery("INSERT into vector_table (id, embedding) values (1, [1.0, 0.5, 0.75, 0.125, 0.25])", queryParameters(keyspace)));
-        assertThat(response).isNotNull();
+  @Test
+  @DisplayName("Should be able to insert and fetch rows by vector similarity")
+  @Order(2)
+  public void insertAndSearch(@TestKeyspace CqlIdentifier keyspace) {
+    // Insert rows into table
+    StargateGrpc.StargateBlockingStub stub = stubWithCallCredentials();
+    QueryOuterClass.Response response =
+        stub.executeQuery(
+            cqlQuery(
+                "INSERT into vector_table (id, embedding) values (1, [1.0, 0.5, 0.75, 0.125, 0.25])",
+                queryParameters(keyspace)));
+    assertThat(response).isNotNull();
 
-        response =
-                stub.executeQuery(
-                        cqlQuery(
-                                "INSERT into vector_table (id, embedding) values (2, [1.0, 1.0, 1.0, 1.0, 1.0])",
-                                queryParameters(keyspace)));
-        assertThat(response).isNotNull();
+    response =
+        stub.executeQuery(
+            cqlQuery(
+                "INSERT into vector_table (id, embedding) values (2, [1.0, 1.0, 1.0, 1.0, 1.0])",
+                queryParameters(keyspace)));
+    assertThat(response).isNotNull();
 
-        // vector search
-        response = stub.executeQuery(cqlQuery("SELECT id FROM vector_table ORDER BY embedding ANN OF [1,1,1,1,1] LIMIT 10", queryParameters(keyspace)));
-        assertThat(response.hasResultSet()).isTrue();
-        assertThat(response.getResultSet().getRowsCount()).isEqualTo(2);
-        assertThat(response.getResultSet().getRows(0).getValues(0).getInt()).isEqualTo(2);
-        assertThat(response.getResultSet().getRows(1).getValues(0).getInt()).isEqualTo(1);
-    }
-
+    // vector search
+    response =
+        stub.executeQuery(
+            cqlQuery(
+                "SELECT id FROM vector_table ORDER BY embedding ANN OF [1,1,1,1,1] LIMIT 10",
+                queryParameters(keyspace)));
+    assertThat(response.hasResultSet()).isTrue();
+    assertThat(response.getResultSet().getRowsCount()).isEqualTo(2);
+    assertThat(response.getResultSet().getRows(0).getValues(0).getInt()).isEqualTo(2);
+    assertThat(response.getResultSet().getRows(1).getValues(0).getInt()).isEqualTo(1);
+  }
 }
