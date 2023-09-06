a6f3c93312f2733605878bfa09f403f8098609d8
Jeffrey Carpenter <jeffrey.carpenter@datastax.com>
2023-09-06 14:47:22 -0700

    create separate changelog for v2.1 (#2758)

--- update_changelog.sh
+++ update_changelog.sh
@@ -5,7 +5,7 @@ which docker > /dev/null || (echoerr "Please ensure that docker is installed" &&
 
 cd -P -- "$(dirname -- "$0")" # switch to this dir
 
-CHANGELOG_FILE=CHANGELOG.md
+CHANGELOG_FILE=CHANGELOG_V21.md
 previous_version=$(head -5 $CHANGELOG_FILE | grep "##" | awk -F']' '{print $1}' | cut -c 5-)
 
 if [[ -z ${GITHUB_TOKEN-} ]]; then
@@ -20,7 +20,7 @@ INTERACTIVE=""
 if [[ -t 1 ]]; then
   INTERACTIVE="-it"
 fi
-docker run $INTERACTIVE --rm -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator -u stargate -p stargate -t $GITHUB_TOKEN --since-tag $previous_version --base $CHANGELOG_FILE --output $CHANGELOG_FILE --release-branch 'main' --exclude-tags-regex 'v1.*' --exclude-labels 'stargate-v1,duplicate,question,invalid,wontfix'
+docker run $INTERACTIVE --rm -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator -u stargate -p stargate -t $GITHUB_TOKEN --since-tag $previous_version --base $CHANGELOG_FILE --output $CHANGELOG_FILE --release-branch 'v2.1' --exclude-tags-regex 'v1.*' --exclude-labels 'stargate-v1,duplicate,question,invalid,wontfix'
 
 # Remove the additional footer added
 head -n $(( $(wc -l < $CHANGELOG_FILE) - 3 )) $CHANGELOG_FILE > "$CHANGELOG_FILE.tmp" && mv "$CHANGELOG_FILE.tmp" "$CHANGELOG_FILE"
