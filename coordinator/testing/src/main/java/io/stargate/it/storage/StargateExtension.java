db0ed8131f5a47788477999c9f85033bb267a367
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-09-25 12:39:20 -0700

    Fix java.base -> java.rmi for RMI registration change

--- coordinator/testing/src/main/java/io/stargate/it/storage/StargateExtension.java
+++ coordinator/testing/src/main/java/io/stargate/it/storage/StargateExtension.java
@@ -419,7 +419,7 @@ public class StargateExtension extends ExternalResource<StargateSpec, StargateEx
       cmd.addArgument("--add-opens");
       cmd.addArgument("java.base/sun.nio.ch=ALL-UNNAMED");
       cmd.addArgument("--add-opens");
-      cmd.addArgument("java.base/sun.rmi.registry=ALL-UNNAMED");
+      cmd.addArgument("java.rmi/sun.rmi.registry=ALL-UNNAMED");
 
       if (backend.isDse()) {
         cmd.addArgument("-Dstargate.request_timeout_in_ms=60000");
