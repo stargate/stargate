69ce59197aa64df57a03b09f8ab14ff4e4402d03
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-12-05 10:55:40 -0800

    minor comment addition

--- coordinator/persistence-cassandra-4.0/src/main/java/org/apache/cassandra/transport/Cassandra40TracingIdAccessor.java
+++ coordinator/persistence-cassandra-4.0/src/main/java/org/apache/cassandra/transport/Cassandra40TracingIdAccessor.java
@@ -6,6 +6,7 @@ import java.util.UUID;
  * Helper class needed to access tracing id from Message.Response to copy it to converted response.
  */
 public class Cassandra40TracingIdAccessor {
+  // Needed because C-4.0 does not expose `getTracingId()` as public unlike 3.11
   public static UUID getTracingId(Message.Response response) {
     return response.getTracingId();
   }
