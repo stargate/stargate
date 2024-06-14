62f1f5ac2d332332d2f2a1bf81766bf1f4c5a2a9
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-06-14 12:02:55 -0400

    Fix for protocol version in value codec

--- coordinator/grpc/src/main/java/io/stargate/grpc/codec/ValueCodec.java
+++ coordinator/grpc/src/main/java/io/stargate/grpc/codec/ValueCodec.java
@@ -15,7 +15,6 @@
  */
 package io.stargate.grpc.codec;
 
-import com.datastax.oss.driver.api.core.DefaultProtocolVersion;
 import com.datastax.oss.driver.api.core.ProtocolVersion;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import io.stargate.db.schema.Column.ColumnType;
